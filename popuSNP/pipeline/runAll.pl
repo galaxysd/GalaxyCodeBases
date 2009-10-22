b3fa170d2c9d1f99814c7f51c6b6f38ee443db75
Simba Galaxy <galaxy001@gmail.com>
2009-10-22 10:32:45 +0800

    add some existanse check

--- popuSNP/pipeline/runAll.pl
+++ popuSNP/pipeline/runAll.pl
@@ -8,15 +8,24 @@ use Galaxy::ShowHelp;
 $main::VERSION=0.0.1;
 my $SCRIPTS='/panfs/GAG/huxuesong/scripts';
 
-our $opts='i:o:s:l:c:bv';
-our($opt_i, $opt_s, $opt_o, $opt_v, $opt_b, $opt_c);
+=pod
+Note:
+If a PE Lib come with only one .adapter.list file, all will use that one. If more than 2, only (sort)[0,-1] is used as _1 and _2.
+
+./0rawfq , the path to fq file cannot contain more than 1 Lib name, since Lib is searched directly on fullpath.
+
+=cut
+our $opts='i:o:s:l:c:r:bvq';
+our($opt_i, $opt_s, $opt_o, $opt_v, $opt_b, $opt_c, $opt_q, $opt_r);
 
 our $desc='1.filter fq, 2.soap, 3.rmdup';
 our $help=<<EOH;
 \t-i FASTAQ file path (./0rawfq) with *.adapter.list
 \t-s Sample list (sample.lst) in format: /^Sample\\tLib\$/
 \t-c Chromosome length list (chr.len) in format: /^ChrName\\s+ChrLen\\s?.*\$/
+\t-r Reference Genome for Soap2 (./Ref) with *.index.bwt
 \t-o Project output path (.), will mkdir if not exist
+\t-q run qsub automatically
 \t-v show verbose info to STDOUT
 \t-b No pause for batch runs
 EOH
@@ -27,11 +36,17 @@ $opt_i='./0rawfq' if ! $opt_i;
 $opt_s='sample.lst' if ! $opt_s;
 $opt_o='.' if ! $opt_o;
 $opt_c='chr.len' if ! $opt_c;
+$opt_r='./Ref' if ! $opt_r;
 
 $opt_i=`readlink -nf $opt_i`;
 $opt_o=`readlink -nf $opt_o`;
+$opt_r=`readlink -nf $opt_r`;
 
-print STDERR "From [$opt_i] to [$opt_o] refer to [$opt_s][$opt_c]\n";
+my @t=`find $opt_r -name '*.index.bwt'`;
+$t[0] =~ /(.+\.index)\.\w+$/;
+$opt_r = $1;
+
+print STDERR "From [$opt_i] to [$opt_o] refer to [$opt_s][$opt_c]\nRef:[$opt_r]\n";
 unless ($opt_b) {print STDERR 'press [Enter] to continue...'; <>;}
 
 my $start_time = [gettimeofday];
@@ -77,38 +92,45 @@ if ($opt_v) {
 }
 system('mkdir','-p',$opt_o);
 my $opath;
-# 1.filter fq
+### 1.filter fq
 $opath=$opt_o.'/1fqfilted';
 system('mkdir','-p',$opath);
 my @fq = `find $opt_i -name '*.fq'`;
 chomp @fq;
 my %fqbylib;
 for my $k (keys %LibSample) {
-	$fqbylib{$k}=[grep /$k/,@fq];
+	my @t=grep /$k/,@fq;
+	$fqbylib{$k}=\@t if $#t > -1;
+	#$fqbylib{$k}=[grep /$k/,@fq];
+	#delete $fqbylib{$k} if $#$fqbylib{$k}==-1;
 }
 if ($opt_v) {
 	for my $k (sort keys %fqbylib) {
-		print "[$k]\n[",join("]\n[",@{$fqbylib{$k}}),"]\n"
+		print "\n[$k]\n[",join("]\n[",@{$fqbylib{$k}}),"]\n"
 	}
 }
 #my ($skip,$count,$copy,$lstcount,$adapter,%copy)=(0,0,0,0);
-my ($sample,$cmd);
+my ($sample,$cmd,@sh);
+my %fqfiltedbylib;
 for my $k (keys %fqbylib) {
 	$sample=$LibSample{$k}->[0];
 	my $dir = $opath."/$sample/$k";
 	system('mkdir','-p',$dir);
-	open OUT,'>',$dir.'.lst' || die "$!\n";
-	open SH,'>',$dir.'.sh' || die "$!\n";
-	open NFO,'>',$dir.'.nfo' || die "$!\n";
+	open OUT,'>',$dir.'.cmd' || die "$!\n";
+	open LST,'>',$dir.'.lst' || die "$!\n";
+	#open SH,'>',$dir.'_filte.sh' || die "$!\n";
+	#open LOG,'>',$dir.'.log' || die "$!\n";
 	my ($skip,$count,$copy,$lstcount,$adapter,%copy)=(0,0,0,0);
 	for my $fq (@{$fqbylib{$k}}) {
 		++$count;
 		my ($file, $path, $ext) = fileparse($fq, qr/\.fq/);
 		my @adapter = `find $path -name '*.list'`;
+		print LST "${dir}/${file}.fq\n";
+		push @{$fqfiltedbylib{$k}},"${dir}/${file}.fq";
 # existance check, old version
 		if (-s "${dir}/${file}.nfo" or -s "${dir}/${file}.fq.bz2") {	# We may package the data and mix *.nfo then?
 			++$skip;
-			system("mv -f ${dir}/${file}.sh ${dir}/${file}.oldsh") if ($#adapter<0 and -e "${dir}/${file}.sh");
+			system("mv -f ${dir}/${file}_filte.sh ${dir}/${file}_filte.oldsh") if ($#adapter<0 and -e "${dir}/${file}_filte.sh");
 			next;
 		}
 # existance check, old version
@@ -116,11 +138,11 @@ for my $k (keys %fqbylib) {
 			++$copy{$path};
 			++$copy;
 			#system('mkdir','-p',$dir);
-			system('cp','-a',$fq,"${dir}/${file}.fq");
+			system('cp','-s',$fq,"${dir}/${file}.fq");
 			#warn $fq,"${dir}/${file}.fq";
-			open STAT,'>',"${dir}/${file}.sh" || die "$!\n";
+			open STAT,'>',"${dir}/${file}_filte.sh" || die "$!\n";
 			print STAT "#!/bin/sh
-#\$ -N \"s$k\"
+#\$ -N \"s_$k\"
 #\$ -v PERL5LIB,PATH,PYTHONPATH,LD_LIBRARY_PATH
 #\$ -cwd -r y -l vf=9M
 #\$ -o /dev/null -e /dev/null
@@ -141,34 +163,89 @@ perl $SCRIPTS/fqstat.pl $fq 2>${dir}/${file}.nfo
 		print OUT $cmd;
 		++$lstcount;
 	}
+	close LST;
 	close OUT;
+	open SH,'>',$dir.'_filte.sh' || die "$!\n";
 	print SH "#!/bin/sh
-#\$ -N \"f$k\"
+#\$ -N \"f_$k\"
 #\$ -v PERL5LIB,PATH,PYTHONPATH,LD_LIBRARY_PATH
 #\$ -cwd -r y -l vf=276M
 #\$ -o /dev/null -e /dev/null
 #\$ -S /bin/bash -t 1-$lstcount
-SEEDFILE=${dir}.lst
+SEEDFILE=${dir}.cmd
 SEED=\$(sed -n -e \"\$SGE_TASK_ID p\" \$SEEDFILE)
 eval perl $SCRIPTS/fqfilter.pl \$SEED
 " if $lstcount > 0;
 	close SH;
 	if ($lstcount == 0) {
-		unlink $dir.'.sh';
-		unlink $dir.'.lst';
+		unlink $dir.'_filte.sh';
+		unlink $dir.'.cmd';
 	}
-	print NFO "Of $count file(s),$skip skipped, $copy with no adapter.list.\n";
+	open LOG,'>',$dir.'.log' || die "$!\n";
+	print LOG "Of $count file(s),$skip skipped, $copy with no adapter.list.\n";
 	if ($copy) {
-		print NFO "\nThe following path with no adapter.list\n";
+		print LOG "\nThe following path with no adapter.list\n";
 		for (sort keys %copy) {
-			print NFO $_,"\t",$copy{$_}," time(s)\n";
+			print LOG $_,"\t",$copy{$_}," time(s)\n";
 		}
 	}
-	close NFO;
+	close LOG;
+## maxReadLen
+	if (-s "${dir}.maxReadLen") {
+		system("mv -f ${dir}_redlen.sh ${dir}_redlen.oldsh") if (-e "${dir}_redlen.sh");
+	} else {
+		open SH,'>',$dir.'_redlen.sh' || die "$!\n";
+		print SH "#!/bin/sh
+#\$ -N \"len_$k\"
+#\$ -v PERL5LIB,PATH,PYTHONPATH,LD_LIBRARY_PATH
+#\$ -cwd -r y -l vf=1M
+#\$ -hold_jid f_$k,s_$k
+#\$ -o /dev/null -e /dev/null
+#\$ -S /bin/bash
+grep '# MaxReadLen' $dir/*.nfo | perl -F'\\t' -lane 'END { print \$b }\$b=\$F[-1] if \$b<\$F[-1]' > $dir.maxReadLen
+";
+		close SH;
+	}
+## InsertSizing
+	if (-s "${dir}.insize") {
+		system("mv -f ${dir}_insize.sh ${dir}_insize.oldsh") if (-e "${dir}_insize.sh");
+	} else {
+		open SH,'>',$dir.'_insize.sh' || die "$!\n";
+		print SH "#!/bin/sh
+#\$ -N \"size_$k\"
+#\$ -v PERL5LIB,PATH,PYTHONPATH,LD_LIBRARY_PATH
+#\$ -cwd -r y -l vf=1M
+#\$ -hold_jid len_$k,f_$k,s_$k
+#\$ -o /dev/null -e /dev/null
+#\$ -S /bin/bash
+
+";
+		close SH;
+	}
+
+}	# End for my $k (keys %fqbylib)
+## Qsub
+if ($opt_q) {
+	@sh = `find $opath -name '*_filte.sh'`;
+	chomp @sh;
+	for (@sh) {
+		print STDERR "[$_]\n" if $opt_v;
+		system("qsub $_");
+	}
+	@sh = `find $opath -name '*_redlen.sh'`;
+	chomp @sh;
+	for (@sh) {
+		print STDERR "[$_]\n" if $opt_v;
+		system("qsub $_");
+	}
 }
+## Stat
 
 
 
+### 2.soap
+# %fqfiltedbylib
+# /share/raid010/resequencing/resequencing/tmp/bin/pipeline/SNPcalling/subBin/soap2.20 -p 4 -a 090811_I58_FC42C7AAAXX_L8_PANwkgRBMDXAAPEI_1.fq -b 090811_I58_FC42C7AAAXX_L8_PANwkgRBMDXAAPEI_2.fq -D Panda.merge.fa.index -o 090811_I58_FC42C7AAAXX_L8_PANwkgRBMDXAAPEI_1.fq.soap -2 090811_I58_FC42C7AAAXX_L8_PANwkgRBMDXAAPEI_1.fq.single -m 100 -x 400  -t -s 40 -l 32 -v 3
 
 
 
