9313115aa1f5c24c6579e8102b8ca5ee80e7ed90
Simba Galaxy <galaxy001@gmail.com>
2009-12-13 01:31:44 +0800

    new file:   funny/gp.pl

---
+++ funny/gp.pl
@@ -0,0 +1,142 @@
+﻿#!/bin/env perl
+use strict;
+use warnings;
+
+my $path='E:\BGI\gaosj\dat\\';
+
+unless (@ARGV){
+	print "perl $0 <begin date(\"10/22/2009\")> [days for UD (2)]\n";
+	exit;
+}
+
+my $date = shift;
+my $dayUD = shift;
+
+my $Days=5;
+my $UpRatio=.6;
+my $UpRate=.02;
+my $BorderRatioU=4;	# 1/4
+my $BorderRatioD=5;	# 1/5
+
+$dayUD=2 unless $dayUD;
+$dayUD=2 unless $dayUD == 1;
+$date='10/22/2009' unless $date=~/^\d+\/\d+\/\d{4}$/;
+print "Caltulating from [$date] to $Days days later with [$dayUD]...\n";
+my (%Names,%Max,%Min,%Files,%Values,%P);
+
+opendir INDIR,$path or warn "[!]Error opening [$path]: $!\n";
+for my $file (readdir INDIR) {
+	if ($file =~ /([^.]+)\.txt$/i) { $Files{$1}=$file;}
+}
+closedir INDIR;
+
+sub Cal($$$) {
+	my ($inref,$max,$min)=@_;
+	my ($cur,$p,$ud)=@$inref;
+	my ($av,$ap,$aud,$bv,$bp,$bud);
+	if ($cur*(1+$BorderRatioU*$UpRate) > $max) {
+		$aud=$bud=-1;
+		$ap=$bp=$p*0.5;
+	} elsif ($cur*(1-$BorderRatioD*$UpRate) < $min) {
+		$aud=$bud=1;
+		$ap=$bp=$p*0.5;
+	} else {
+		$aud=$ud;
+		$bud=-$ud;
+		$ap=$p*$UpRatio;
+		$bp=$p*(1-$UpRatio);
+	}
+
+	$av=$cur*(1+$aud*$UpRate);
+	$bv=$cur*(1+$bud*$UpRate);
+	#my @t=([$av,$ap,$aud],[$bv,$bp,$bud]);
+	return [[$av,$ap,$aud],[$bv,$bp,$bud]];
+}
+
+my $skip=0;
+MAIN: for my $id (keys %Files) {
+	open IN,'<',$path.$Files{$id} or die "[!]Error opening [$path$Files{$id}]: $!\n";
+	my $line=<IN>;
+	$line =~ /(\d+) (.+) \S+$/;
+	my $ID=$1;
+	$Names{$ID}=$2;
+#print "$id\t$1\t",$Names{$ID},"\n";
+	<IN>;
+	my ($max0,$min0,$end0,$endpre,$endpt)=(-999999,999999,-1,-1);
+	while (<IN>) {
+		$endpt=$end0;
+#      日期	    开盘	    最高	    最低	    收盘	    成交量	    成交额
+		my ($dateitem,$begin,$max,$min,$end)=split /\t/;
+		$max0=$max if $max0<$max;
+		$min0=$min if $min0>$min;
+		if ($date eq $dateitem) {
+			$end0=$end;
+			$endpre=($dayUD==2)?$endpt:$begin;
+		}
+	}
+	close IN;
+	$Max{$id}=$max0;
+	$Min{$id}=$min0;
+#print "$Max{$id},$Min{$id}\t$end0\n";
+	unless ($end0) {
+		#warn "[!]$date not found in $id !\n";
+		++$skip;
+		next MAIN;
+		#$end0=0;
+	}
+#print "$Max{$id},$Min{$id}\t$end0\t";
+	#my $Result=&Cal($max0,$min0,$end0);
+	my $UDbegin=($end0>=$endpre)?1:-1;
+	my @Days=([[$end0,1,$UDbegin]]);	# ($cur,$p,$ud)
+	for (my $i=1;$i<$Days;$i++) {	# 0..4
+		$Days[$i]=[];
+		push @{$Days[$i]},@{&Cal($_,$max0,$min0)} for @{$Days[$i-1]};
+	}
+	my ($ups,$upp,$downs,$downp)=(0,0,0,0);
+	for (@{$Days[-1]}) {
+		my ($cur,$p,$ud)=@$_;
+#warn "[$cur,$p,$ud]\n";
+		if ($cur >= $end0) {
+			$ups += $cur*$p;
+			$upp += $p;
+		} else {
+			$downs += $cur*$p;
+			$downp += $p;
+		}
+	}
+	$ups /= $upp unless $upp==0;
+	$downs /= $downp unless $downp==0;
+	my ($resv,$resp);
+	if ($upp >= $downp) {
+		$resv = $ups/$end0;
+		$resp = $upp;
+	} else {
+		$resv = $downs/$end0;
+		$resp = $downp;
+	}
+	$Values{$ID}=$resv;
+	$P{$ID}=$resp;
+#print "$resv\t$resp\n";# if $resv > 1;
+}
+
+warn "[!]$skip skipped !\n" if $skip;
+
+my @KEYs=sort {$Values{$b} <=> $Values{$a}} keys %Values;
+
+for my $i (0..9) {
+	my $key=$KEYs[$i];
+	print "$key\t$Names{$key}:\t$Values{$key}\t$P{$key}\n";
+}
+print "\n";
+for my $i (-5..-1) {
+	my $key=$KEYs[$i];
+	print "$key\t$Names{$key}:\t$Values{$key}\t$P{$key}\n";
+}
+
+my %t;
+for (values %Values) {
+	++$t{$_};
+}
+for (sort {$b <=> $a} keys %t) {
+	print "$_\t$t{$_}\n";
+}
