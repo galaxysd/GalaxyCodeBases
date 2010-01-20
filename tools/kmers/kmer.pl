225a3d2d12806a91eae0bdc378559b7ccc1b235b
Simba Galaxy <galaxy001@gmail.com>
2010-01-20 22:24:15 +0800

    modified:   tools/kmers/kmer.pl

--- tools/kmers/kmer.pl
+++ tools/kmers/kmer.pl
@@ -30,12 +30,12 @@ $opt_i='human.fa' if ! defined $opt_i;
 $opt_o='stat.txt' if ! $opt_o;
 $opt_t='./._tmp_' if ! $opt_t;
 $opt_c=4 if ! $opt_c;
-$opt_c=0.00001 if $opt_c<0.00001;
+$opt_c=0.002 if $opt_c<0.002;
 $opt_l=33 if ! $opt_l;
 $opt_l=int($opt_l);
 $opt_l=1 if $opt_l<1;
 
-print STDERR "From [$opt_i] with [$opt_l][$opt_t][$opt_c] to [$opt_o]\n";
+print STDERR "From [$opt_i] with [$opt_l][$opt_t][$opt_c]G to [$opt_o]\n";
 if (! $opt_b) {print STDERR 'press [Enter] to continue...'; <>;}
 
 open( T,'>',$opt_t) or die "[x]Error creating swap file: $!\n";
@@ -45,8 +45,8 @@ my $start_time = [gettimeofday];
 
 my %attr = (
     RaiseError => 0,
-    PrintError => 1,	# 1 for debug only (way1)
-    AutoCommit => 0,	# OFF on ON ?
+    PrintError => 1,
+    AutoCommit => 0,
 );
 my $dbh = DBI->connect('dbi:SQLite:dbname='.$opt_t,undef,undef,\%attr) or die $DBI::errstr;
 my $sql=q/
@@ -54,23 +54,15 @@ PRAGMA synchronous = OFF;
 PRAGMA journal_mode = OFF;
 PRAGMA temp_store = MEMORY;
 PRAGMA auto_vacuum = NONE;
-PRAGMA cache_size = /.1000000*$opt_c.q/;
+PRAGMA cache_size = /.int(1000000*$opt_c).	# 2000 for 2MB. Bigger, better ?
+q/;
 CREATE TABLE kmers ( seq TEXT );
 /;
-=pod
-PRAGMA journal_mode = OFF;	# Should OFF or MEMORY?
-PRAGMA cache_size = 4000000;	# 2000 for 2MB. Bigger, better ?
-CREATE TABLE kmers0
-(  seq TEXT PRIMARY KEY,
-   count INTEGER DEFAULT 1);
-=cut
 for (split /;/,$sql) {
 	next if /^\s*$/;
 	$dbh->do($_) or die $dbh->errstr,": $_";
 }
 my $sth = $dbh->prepare( 'INSERT INTO kmers ( seq ) VALUES ( ? );' );
-#my $gth = $dbh->prepare( 'UPDATE kmers SET count = count + 1 WHERE seq = ?;' );
-#my $gth = $dbh->prepare( 'SELECT seq,COUNT(seq) FROM kmers GROUP BY seq;' );
 
 open( IN,'<',$opt_i) or die "[x]Error: $!\n";
 my ($seq,$len);
@@ -86,7 +78,7 @@ while (<IN>) {
 	$/="\n";
 	# Begin Kmers
 	$len=length $seq;
-	uc $seq;
+	$seq=uc $seq;
 #$len=10000000+$opt_l-1;
 	print STDERR $len,' - ';
 	#next if $len < $opt_l;	# We already while it
@@ -95,7 +87,7 @@ while (<IN>) {
 	for ($i=0;$i<=$t;$i++) {
 		my $str=substr $seq,$i,$opt_l;
 #die if length $str != $opt_l;	# just test
-		$sth->execute($str);# or $gth->execute($str);# or die $sth->errstr;
+		$sth->execute($str);# or warn $sth->errstr;
 	}
 	print STDERR $i,"\n";
 	#$dbh->commit;
@@ -103,12 +95,11 @@ while (<IN>) {
 }
 close IN;
 my $read_time = [gettimeofday];
+$seq=undef;
 warn "[!]Kmers Inserted !\n";
-#$dbh->do('CREATE INDEX s ON kmers(seq);') or warn $dbh->errstr;
-#warn;
 
-if ($opt_s) { $sql='SELECT seq,COUNT(seq) AS count FROM kmers GROUP BY seq ORDER BY count DESC;';}
- else {$sql='SELECT seq,COUNT(seq) AS count FROM kmers GROUP BY seq;';}
+if ($opt_s) { $sql='SELECT seq,COUNT(seq) AS count FROM kmers GROUP BY seq ORDER BY count DESC;'; }
+ else { $sql='SELECT seq,COUNT(seq) AS count FROM kmers GROUP BY seq;'; }
 my $gth = $dbh->prepare( $sql );
 $gth->execute;
 my $work_time = [gettimeofday];
@@ -136,14 +127,7 @@ print STDERR "\nTime Elapsed:\t",tv_interval( $start_time, $stop_time ),
 	" second(s).\n   Clean used:\t",tv_interval( $end_time, $stop_time )," second(s).\n";
 __END__
 
-10000000*2
-
-without sort:
-Time Elapsed:   544.888772 second(s).
-   INSERT used: 189.922403 second(s).
-   Count used:  199.940926 second(s).
-   Output used: 154.487606 second(s).
-   Clean used:  0.537837 second(s).
+10000000*2 = 20 M
 
 with sort:
 Time Elapsed:   653.591479 second(s).
@@ -152,9 +136,38 @@ Time Elapsed:   653.591479 second(s).
    Output used: 137.574869 second(s).
    Clean used:  0.535277 second(s).
 
+without sort:
+Time Elapsed:   544.888772 second(s).
+   INSERT used: 189.922403 second(s).
+   Count used:  199.940926 second(s).
+   Output used: 154.487606 second(s).
+   Clean used:  0.537837 second(s).
+
 without sort, PRAGMA journal_mode = MEMORY;:
 Time Elapsed:   547.098508 second(s).
    Insert used: 190.366605 second(s).
    Count used:  200.026188 second(s).
    Output used: 156.246296 second(s).
    Clean used:  0.459419 second(s).
+
+without sort, cache=1 GB:
+Time Elapsed:   547.102521 second(s).
+   Insert used: 189.94249 second(s).
+   Count used:  200.629807 second(s).
+   Output used: 155.977248 second(s).
+   Clean used:  0.552976 second(s).
+
+2 M:
+without sort, cache=10 MB:
+Time Elapsed:   56.378994 second(s).
+   Insert used: 23.819828 second(s).
+   Count used:  16.546491 second(s).
+   Output used: 15.898938 second(s).
+   Clean used:  0.113737 second(s).
+
+without sort, cache=100 MB:
+Time Elapsed:   162.321573 second(s).
+   Insert used: 23.709979 second(s).
+   Count used:  102.493035 second(s).
+   Output used: 36.062866 second(s).
+   Clean used:  0.055693 second(s).
