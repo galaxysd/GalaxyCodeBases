d0a27625f983d05a10b11a397e39f267a062d971
Simba Galaxy <galaxy001@gmail.com>
2010-02-08 21:27:16 +0800

    Merge Sort is done. 	modified:   popuSNP/pipeline/scripts/merge.pl

--- popuSNP/pipeline/scripts/merge.pl
+++ popuSNP/pipeline/scripts/merge.pl
@@ -12,9 +12,35 @@ open LST,'<',$listf or die "[x]Error opening $listf: $!\n";
 my @files=<LST>;
 chomp @files;
 close LST;
-if ($#files == 0) {
+my $n=$#files;
+print '[!]',$n+1," file(s) to be megred.\n";
+if ($n == 0) {
 	link $files[0],"$outf.sp" or symlink $files[0],"$outf.sp" or exit 1;
-} else {}
+} else {
+	open O,'>',"$outf.sp" or die "[x]Error opening $outf.sp: $!\n";
+	my @FHL;
+	for (0..$n) {
+		open $FHL[$_][0],'<',$files[$_] or die "[x]Error opening $files[$_]: $!\n";
+	}
+	#chomp ($FHL[$_][1]=readline $FHL[$_][0]) for (0..$n);
+	for (@FHL) {
+		$$_[1]=readline $$_[0];	# no need to chomp
+		$$_[2]=(split /\t/,$$_[1])[8];
+	}
+#print '[',$FHL[$_][1],"]\t[",$FHL[$_][2],"]\n" for (0..$n);
+	while ($n>-1) {
+		@FHL = sort {unless (defined $$a[2]) {return 1} elsif (! defined $$b[2]) {return -1} $$a[2]<=>$$b[2]} @FHL;
+		print O $FHL[0][1];
+		$FHL[0][1]=readline $FHL[0][0];
+		if (defined $FHL[0][1]) {$FHL[0][2]=(split /\t/,$FHL[0][1])[8];}
+		 else {--$n; $FHL[0][2]=undef;}
+	}
+	#print O $FHL[0][1];
+#print '[',$FHL[$_][1],"]\t[",$FHL[$_][2],"]\n" for (0..$n);
+	close O;
+	close $$_[0] for @FHL;
+}
+$n=@files;
 open LOG,'>',"$outf.log";
-print LOG "done !\n";
+print LOG "#$n files megred.\ndone !\n";
 close LOG;
