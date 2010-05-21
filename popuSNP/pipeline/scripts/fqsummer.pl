598692b3be294d8652a2bb443f8c7600ab9296e5
alpha <alpha@galaxy.pg>
2010-05-21 11:19:48 +0800

    modified:   fqsummer.pl

--- popuSNP/pipeline/scripts/fqsummer.pl
+++ popuSNP/pipeline/scripts/fqsummer.pl
@@ -15,7 +15,9 @@ while (<LST>) {
 	my ($sample,$lib,$FL,$min,$max,$fq1,$fq2,$ext,$path)=split /\t/;
 	$Librmx{$sample}{$lib}{$FL}=[0,$min,$max];	# readlen,minIS,maxIS
 	$FQnfo{$sample}{$lib}{$FL}=[$ext,$path,$fq1,$fq2];
-	$DATrbrf{$sample}{$lib}=[0,0,0,0];	# rawReads,rawBP,filteredReads,filteredBP
+	$DATrbrf{Lib}{$sample}{$lib}=[0,0,0,0];	# rawReads,rawBP,filteredReads,filteredBP
+	$DATrbrf{Sample}{$sample}=[0,0,0,0];
+	$DATrbrf{Lane}{$sample}{$lib}{$FL}=[0,0,0,0];
 }
 open LST,'<',$selst or warn "Error opening $pelst: $!\n";
 while (<LST>) {
@@ -23,16 +25,49 @@ while (<LST>) {
 	my ($sample,$lib,$FL,$fq,$ext,$path)=split /\t/;
 	$Librmx{$sample}{$lib}{$FL}=[0];	# readlen,minIS,maxIS
 	$FQnfo{$sample}{$lib}{$FL}=[$ext,$path,$fq];
-	$DATrbrf{$sample}{$lib}=[0,0,0,0];	# rawReads,rawBP,filteredReads,filteredBP
+	$DATrbrf{Lib}{$sample}{$lib}=[0,0,0,0];	# rawReads,rawBP,filteredReads,filteredBP
+	$DATrbrf{Sample}{$sample}=[0,0,0,0];
+	$DATrbrf{Lane}{$sample}{$lib}{$FL}=[0,0,0,0];
 }
 
+sub sumup ($$) {
+	my ($arrayr,$hashr)=@_;
+	$$arrayr[0] += $$hashr{InReads};
+	$$arrayr[1] += $$hashr{InBPs};
+	$$arrayr[2] += $$hashr{OutReads};
+	$$arrayr[3] += $$hashr{OutBP};
+}
+
+my (@NFO,%NFO,$arrayp);
+open OP,'>',$pelst.'.n' or die "Error opening $pelst.n: $!\n";
+open OS,'>',$selst.'.n' or die "Error opening $selst.n: $!\n";
 for my $sample (keys %FQnfo) {
 	for my $lib (keys %{$FQnfo{$sample}}) {
 		for my $FL (keys %{$FQnfo{$sample}{$lib}}) {
 			my ($ext,$path,@fq)=@{$FQnfo{$sample}{$lib}{$FL}};
 			for (@fq) {
-				;
+				#print "[$_]\n";
+				open NFO,'<',"$path$_.nfo" or die "Error opening $path$_.nfo: $!\n";
+				@NFO=<NFO>;
+				chomp @NFO;
+				%NFO = map {split /\t/} grep /\t/,@NFO;
+				#print "$_ => $NFO{$_}\n" for keys %NFO;
+				close NFO;
+				$Librmx{$sample}{$lib}{$FL}->[0]=$NFO{MaxReadLen} if $Librmx{$sample}{$lib}{$FL}->[0] < $NFO{MaxReadLen};
+				&sumup($DATrbrf{Sample}{$sample},\%NFO);
+				&sumup($DATrbrf{Lib}{$sample}{$lib},\%NFO);
+				&sumup($DATrbrf{Lane}{$sample}{$lib}{$FL},\%NFO);
+			}
+			if ($#fq == 1) {	# PE
+				print OP join("\t",$sample,$lib,$FL,@{$Librmx{$sample}{$lib}{$FL}},@{$FQnfo{$sample}{$lib}{$FL}}),"\n";
+			} else {
+				print OS join("\t",$sample,$lib,$FL,@{$Librmx{$sample}{$lib}{$FL}},@{$FQnfo{$sample}{$lib}{$FL}}),"\n";
 			}
 		}
 	}
 }
+close OP; close OS;
+
+open O,'>',$statout or die "Error opening $statout: $!\n";
+
+close O;
