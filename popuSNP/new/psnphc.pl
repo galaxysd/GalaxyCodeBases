2d7df4551e5839a1966d78d53a493fbd657d5e11
Simba Galaxy <galaxy001@gmail.com>
2010-08-30 20:10:44 +0800

    modified:   new/psnphc.pl

--- popuSNP/new/psnphc.pl
+++ popuSNP/new/psnphc.pl
@@ -50,3 +50,19 @@ warn "\n[!] $In -> $Out, ",int(10000*$Out/$In)/100,"%\n";
 __END__
 ./psnphc.pl psnp/Chr02.add_ref Chr02 0.95,0.99999
 cat 9311/chrorder |while read a; do echo ./psnphc.pl ./psnp/$a.add_ref $a 0.95,0.999;done > ff.sh
+
+perl -lane 'next if $F[8]<70;next if $F[8]>170;print join("\t",@F[0,1,2,7,8],$F[8]/($F[7]+$F[8]))' psnp/Chr01_final.snp
+cat 9311/chrorder |while read a;do perl -lane 'next if $F[8]<70;next if $F[8]>170;print join("\t",@F[0,1,2,7,8],$F[8]/($F[7]+$F[8]))' psnp/${a}_final.snp > ff$a.snp;done
+
+cat 9311/chrorder |while read a;do perl -e "my %Dat;open IN,'<','ff$a.snp';
+while (<IN>) {\$x=(split /\t/)[1]; ++\$Dat{\$x};}
+close IN;
+open T,'<','psnp/$a.add_ref';
+while (<T>) {
+	@F=split /\t/;
+	next unless \$Dat{\$F[1]};
+	print \$_;
+}
+close T;
+" > aa$a.snp;
+done
