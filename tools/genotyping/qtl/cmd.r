c7f44f78797809dba5b8ac098e9e2ad7f3dde6a8
alpha <alpha@galaxy.pg>
2010-10-19 23:27:09 +0800

    modified:   cmd.r

--- tools/genotyping/qtl/cmd.r
+++ tools/genotyping/qtl/cmd.r
@@ -54,3 +54,58 @@ dev.off()
 
 out.mr <- scanone(dat, method="mr")
 out.2p <- scanone(dat, pheno.col=1, method="mr")
+
+
+
+
+
+library('qtl')
+file='av1'
+dat=read.cross('csvsr','.',paste(sep='',file,'_rot.csv'),'2nd_phe.csv')
+
+out <- scanone(dat, pheno.col=1)
+write.table(out,paste(sep='',file,'_chlorophyl_content.txt'))
+png(paste(sep='',file,'_chlorophyl_content.png'),16000,1200)
+plot(out, ylab="LOD score",main='Raw SNP Markers')
+dev.off()
+
+dat0 <- dat
+dat <- calc.genoprob(dat, step=1, error.prob=0.01)
+out.np <- scanone(dat, model="np", pheno.col=1)
+write.table(out.np,paste(sep='',file,'_chlorophyl_content.np.txt'))
+png(paste(sep='',file,'_chlorophyl_content.np.png'),16000,1200)
+plot(out.np, ylab="LOD score", alternate.chrid=TRUE,main='HMM filtered')
+dev.off()
+
+out <- scanone(dat0, pheno.col=2)
+write.table(out,paste(sep='',file,'_heading_days.txt'))
+png(paste(sep='',file,'_heading_days.png'),16000,1200)
+plot(out, ylab="LOD score",main='Raw SNP Markers')
+dev.off()
+out.np <- scanone(dat, model="np", pheno.col=2)
+write.table(out.np,paste(sep='',file,'_heading_days.np.txt'))
+png(paste(sep='',file,'_heading_days.np.png'),16000,1200)
+plot(out.np, ylab="LOD score", alternate.chrid=TRUE,main='HMM filtered')
+dev.off()
+
+out <- scanone(dat0, pheno.col=3)
+write.table(out,paste(sep='',file,'_plant_height2.txt'))
+png(paste(sep='',file,'_plant_height2.png'),16000,1200)
+plot(out, ylab="LOD score",main='Raw SNP Markers')
+dev.off()
+out.np <- scanone(dat, model="np", pheno.col=3)
+write.table(out.np,paste(sep='',file,'_plant_height2.np.txt'))
+png(paste(sep='',file,'_plant_height2.np.png'),16000,1200)
+plot(out.np, ylab="LOD score", alternate.chrid=TRUE,main='HMM filtered')
+dev.off()
+
+out <- scanone(dat0, pheno.col=4)
+write.table(out,paste(sep='',file,'_tiller_number2.txt'))
+png(paste(sep='',file,'_tiller_number2.png'),16000,1200)
+plot(out, ylab="LOD score",main='Raw SNP Markers')
+dev.off()
+out.np <- scanone(dat, model="np", pheno.col=4)
+write.table(out.np,paste(sep='',file,'_tiller_number2.np.txt'))
+png(paste(sep='',file,'_tiller_number2.np.png'),16000,1200)
+plot(out.np, ylab="LOD score", alternate.chrid=TRUE,main='HMM filtered')
+dev.off()
