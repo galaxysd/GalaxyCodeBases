a662b30f5212f6bb494a2f103d1cd9143cbf96cb
Simba Galaxy <galaxy001@gmail.com>
2010-12-16 17:42:27 +0800

    modified:   clinkage.pl, add Kosambi & r is of per meiosis now

--- tools/linkage/clinkage.pl
+++ tools/linkage/clinkage.pl
@@ -7,12 +7,22 @@ unless (@ARGV > 0) {
     exit 0;
 }
 
+# choose Kosambi, which is slower than Haldane, for density markers
+sub corRIL($) {
+	my ($bigRth)=@_;
+	my $smallr;
+	$smallr=0.5*$bigRth/(1-$bigRth);
+	# applies the distance function
+	my $RILdist = 25 * log((1 + 2 * $smallr) / (1 - 2 * $smallr));
+	return $RILdist;
+}
+
 open I,'<',$ARGV[0] or die $!;
 open O,'>',$ARGV[1] or die $!;
 my $t=<I>;
 $t=~s/\tPos\t/\tPos\tcM\tΔcM\tkB\/cM\t/;
 #print O $t;
-print O "#ChrID\tPos\tcM\tΔcM\tkB/cM\n";
+print O "#ChrID\tPos\tcM\tΔcM\tkB/cM\tR_RIL\n";
 my (@Pos,%Dat);
 my ($chr,$pos,$dat);
 while (<I>) {
@@ -24,7 +34,7 @@ while (<I>) {
 }
 my $LastcM=0;
 my $ratio;
-print O "$chr\t$Pos[0]\t$LastcM\t0\tNA\n";	#,join(' ',@{$Dat{$Pos[0]}}),"\n";
+print O "$chr\t$Pos[0]\t$LastcM\t0\tNA\t0\n";	#,join(' ',@{$Dat{$Pos[0]}}),"\n";
 for my $i (1..$#Pos) {
 	my $ary_a=$Dat{$Pos[$i-1]};
 	my $ary_b=$Dat{$Pos[$i]};
@@ -34,11 +44,12 @@ for my $i (1..$#Pos) {
 		++$cross if $ary_a->[$p] ne $ary_b->[$p];
 		++$cnt;
 	}
-	$cross = 100*$cross/$cnt;
+	my $cross0 = $cross/$cnt;
+	$cross = &corRIL($cross0);
 	$LastcM += $cross;
 	$ratio=$cross? int(0.5+100*($Pos[$i]-$Pos[$i-1])/(1000*$cross)) : 'NA';
 	print O "$chr\t$Pos[$i]\t",int(0.5+100*$LastcM)/100,"\t",int(0.5+100*$cross)/100,"\t"
-		,$ratio,"\n";	#,join(' ',@{$Dat{$Pos[$i]}}),"\n";
+		,$ratio,"\t",100*$cross0,"\n";	#,join(' ',@{$Dat{$Pos[$i]}}),"\n";
 }
 
 close O;
