9d6ee8e3e374a04c50880b4440483001226cc279
Simba Galaxy <galaxy001@gmail.com>
2011-02-21 13:12:58 +0800

    new file:   color.pl 	ANSI color test

---
+++ funny/color.pl
@@ -0,0 +1,21 @@
+#!/bin/env perl
+use strict;
+use warnings;
+
+for my $bg (0..7) {
+	for my $fg (0..7) {
+		print "\033[",$fg+30,";",$bg+40,"m","[t$fg,${bg}t]","\033[0m";
+	}
+	print "\n";
+}
+
+for my $attr (0..9) {
+	print "\nAttr:$attr\n";
+	for my $bg (0..7) {
+		print "before ";
+		for my $fg (0..7) {
+			print "\033[",$attr,";",$fg+30,";",$bg+40,"m","[t$fg,${bg}t]","\033[0m";
+		}
+		print " after\n";
+	}
+}
