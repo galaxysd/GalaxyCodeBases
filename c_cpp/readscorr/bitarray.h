467aa8c3a0173554a12f5622f145611980ff9418
HXs <HXs@alpha>
2011-04-28 14:05:21 +0800

    modified:   bitarray.h

--- c_cpp/readscorr/bitarray.h
+++ c_cpp/readscorr/bitarray.h
@@ -1,12 +1,36 @@
 // From http://c-faq.com/misc/bitsets.html
+// See also /usr/include/X11/extensions/xtrapbits.h
+// Added BitToggle and BitCopy with ByteInArray from xtrapbits.h. Not tested yet.
+// by Hu Xuesong
+
 #include <limits.h>		/* for CHAR_BIT */
 
-#define BITMASK(b) (1 << ((b) % CHAR_BIT))
-#define BITSLOT(b) ((b) / CHAR_BIT)
-#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
-#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
-#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
-#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
+typedef unsigned char * UByteP;  /* Pointer to an unsigned byte array */
+
+#define BITMASK(b) /* Returns the bit mask of a byte */ \
+    (1 << ((b) % CHAR_BIT))
+
+#define ByteInArray(array,bit) /* Returns the byte offset to get to a bit */ \
+    (((UByteP)(array))[(bit) / CHAR_BIT)])
+
+#define BITSLOT(b) ((b) / CHAR_BIT) // Just the index of char[]
+
+#define BITSET(a, b) /* Set a specific bit to be True */ \
+    ((a)[BITSLOT(b)] |= BITMASK(b))
+
+#define BITCLEAR(a, b) /* Set a specific bit to be False */ \
+    ((a)[BITSLOT(b)] &= ~BITMASK(b))
+
+#define BitToggle(array,bit)    /* Toggle a specific bit */ \
+    (ByteInArray(array,bit) ^= BITMASK(bit))
+
+#define BITTEST(a, b) /* Test to see if a specific bit is True */ \
+    ((a)[BITSLOT(b)] & BITMASK(b))
+
+#define BitCopy(dest,src,bit)   /* Copy a specific bit */ \
+    BITTEST((src),(bit)) ? BITSET((dest),(bit)) : BITCLEAR((dest),(bit))
+
+#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT) // caltulate the length of char[]
 
 /*
 (If you don't have <limits.h>, try using 8 for CHAR_BIT.)
