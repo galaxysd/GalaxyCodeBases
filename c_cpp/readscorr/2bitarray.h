91c9dfe298d82ba349d0994b7766578c96503b00
HXs <HXs@alpha>
2011-04-29 19:55:15 +0800

    GCC will not inline functions in other c files, thus move into header. 	deleted:    2bitarray.c 	modified:   2bitarray.h 	modified:   Makefile

--- c_cpp/readscorr/2bitarray.h
+++ c_cpp/readscorr/2bitarray.h
@@ -23,13 +23,83 @@
 
 #define TBITNSLOTS(nb) ((2*(nb) + CHAR_BIT - 1) / CHAR_BIT) // caltulate the length of char[]. x*2 can overflow while x<<1 cannot.
 
+/*
+__inline void TBITSetValue ( char arr[], size_t index, uint_fast8_t value );
+__inline uint_fast8_t TBITSaturatedADD ( char arr[], size_t index, int_fast8_t value );
+__inline uint_fast8_t TBITSaturatedINC ( char arr[], size_t index );
+__inline uint_fast8_t TBITSaturatedDEC ( char arr[], size_t index );
+*/
 
-void TBITSetValue ( char arr[], size_t index, uint_fast8_t value );
-uint_fast8_t TBITSaturatedADD ( char arr[], size_t index, int_fast8_t value );
-uint_fast8_t TBITSaturatedINC ( char arr[], size_t index );
-uint_fast8_t TBITSaturatedDEC ( char arr[], size_t index );
+// I find gcc will not inline functions in other c files, thus move them here.
+
+#if defined(_MSC_VER)
+
+#define FORCE_INLINE	__forceinline
+
+#else	// defined(_MSC_VER)
+
+#define	FORCE_INLINE	__attribute__((always_inline))
+
+#endif // !defined(_MSC_VER)
+
+FORCE_INLINE void TBITSetValue ( char arr[], size_t index, uint_fast8_t value ) {
+    if (value>3) value=3;
+    unsigned char theByte=(arr[TBITSLOT(index)]) & ~TBITMASK(index);
+    theByte |= value << TBITSHIFT(index);
+    arr[TBITSLOT(index)] = theByte;
+}
+
+FORCE_INLINE uint_fast8_t TBITSaturatedADD ( char arr[], size_t index, int_fast8_t value ) {
+    unsigned char theByte = (arr[TBITSLOT(index)]);
+    int_fast8_t theValue = (theByte >> TBITSHIFT(index)) & 3 ;
+    if (value==0) {
+        return (uint_fast8_t) theValue;
+    } else if (value > 0) {
+        if (theValue > 3-value) theValue=3;
+          else theValue += value;
+    } else {
+        if (theValue < -value) theValue =0;
+          else theValue += value;
+    }
+    theByte &= ~TBITMASK(index);
+    theByte |= theValue << TBITSHIFT(index);
+    arr[TBITSLOT(index)] = theByte;
+    return (uint_fast8_t) theValue;
+}
+
+FORCE_INLINE uint_fast8_t TBITSaturatedINC ( char arr[], size_t index ) {
+    unsigned char theByte = (arr[TBITSLOT(index)]);
+    uint_fast8_t theValue = (theByte >> TBITSHIFT(index)) & 3 ;
+    if (theValue==3) {
+        return 3;
+    } else {
+        ++theValue;
+        theByte &= ~TBITMASK(index);
+        theByte |= theValue << TBITSHIFT(index);
+        arr[TBITSLOT(index)] = theByte;
+        return theValue;
+    }
+}
+
+FORCE_INLINE uint_fast8_t TBITSaturatedDEC ( char arr[], size_t index ) {
+    unsigned char theByte = (arr[TBITSLOT(index)]);
+    uint_fast8_t theValue = (theByte >> TBITSHIFT(index)) & 3 ;
+    if (theValue==0) {
+        return 0;
+    } else {
+        --theValue;
+        theByte &= ~TBITMASK(index);
+        theByte |= theValue << TBITSHIFT(index);
+        arr[TBITSLOT(index)] = theByte;
+        return theValue;
+    }
+}
+
+
 
 #endif /* 2bitarray.h */
+
+// Original comments for bitarray: 
 /*
 (If you don't have <limits.h>, try using 8 for CHAR_BIT.)
 
