53813c17fc092c29345d32887ddeeee45461303a
HXs <HXs@alpha>
2011-05-27 10:23:37 +0800

    snapshot 01-20110527, a much better way for TF. See also sf#6146971. 	modified:   sdleftTF.c

--- c_cpp/readscorr/sdleftTF.c
+++ c_cpp/readscorr/sdleftTF.c
@@ -2,24 +2,31 @@
 #include "sdleft.h"
 #include "sdleftTF.h"
 
-uint16_t *dleft_statu16(SDLeftArray_t *dleftobj);
-uint32_t *dleft_statu32(SDLeftArray_t *dleftobj);
-uint64_t *dleft_statu64(SDLeftArray_t *dleftobj);
+#define TOKENPASTE(x, y) x ## y
+#define TOKENPASTE2(x, y) TOKENPASTE(x, y)
 
-#ifndef PUBLIC
-    #ifdef USEUINT16
-    uint16_t  *dleft_statu16
-    #elif defined USEUINT32
-    uint32_t  *dleft_statu32
-    #elif defined USEUINT64
-    uint64_t  *dleft_statu64
-    #else
-        #error Must define USEUINT{16,32,64} or PUBLIC before compilering !
-    #endif
-     (SDLeftArray_t *dleftobj) {
-        return 0;
-    }
+#ifdef USEUINT16
+    #define THETYPE uint16_t
+#elif defined USEUINT32
+    #define THETYPE uint32_t
+#elif defined USEUINT64
+    #define THETYPE uint64_t
+#elif defined PUBLIC
+    // NA
 #else
+    #error Must define USEUINT{16,32,64} or PUBLIC before compilering !
+#endif
 
-G_SDLeftArray_IN pf = (G_SDLeftArray_IN) dleft_statu16;
+#define ADDSUFFIX(x) TOKENPASTE2(x ## _,THETYPE)
+
+uint16_t *dleft_stat_uint16_t(SDLeftArray_t *dleftobj);
+uint32_t *dleft_stat_uint32_t(SDLeftArray_t *dleftobj);
+uint64_t *dleft_stat_uint64_t(SDLeftArray_t *dleftobj);
+
+#ifndef PUBLIC  // USEUINT{16,32,64}
+THETYPE * ADDSUFFIX(dleft_stat) (SDLeftArray_t *dleftobj) {
+        return 0;
+}
+#else   // PUBLIC
+G_SDLeftArray_IN pf = (G_SDLeftArray_IN) dleft_stat_uint16_t;
 #endif
