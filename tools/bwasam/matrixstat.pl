1c34d5a8845cb0f0a576c3798bb31d102a8000ba
HXs <HXs@alpha>
2011-07-28 20:07:14 +0800

    deleted:    matrixsoap.pl 	renamed:    matrixsam.pl -> matrixstat.pl

--- tools/bwasam/matrixstat.pl
+++ tools/bwasam/matrixstat.pl
@@ -6,26 +6,28 @@ use Time::HiRes qw ( gettimeofday tv_interval );
 use Galaxy::ShowHelp;
 
 $main::VERSION=0.0.1;
-our $opts='r:o:l:b';
-our($opt_o, $opt_r, $opt_l, $opt_b);
+our $opts='r:o:l:p:b';
+our($opt_o, $opt_r, $opt_l, $opt_p, $opt_b);
 
 #our $desc='';
 our $help=<<EOH;
+\t-p type of input files {(auto),sam,soap}
 \t-r ref fasta file (./ref/human.fa)
 \t-l read length of reads (100)
 \t-o output prefix (./matrix).{mcount,mratio}
 \t-b No pause for batch runs
 EOH
-our $ARG_DESC='sampe_files';
+our $ARG_DESC='{sam,soap}pe_files';
 
 ShowHelp();
 $opt_r='./ref/human.fa' if ! $opt_r;
+$opt_p='auto' if ! $opt_p;
 $opt_o='./matrix' if ! $opt_o;
 $opt_l=100 if ! $opt_l;
 die "[x]-r $opt_r not exists !\n" unless -f $opt_r;
 
-print STDERR "From [@ARGV] of [$opt_l] with [$opt_r] to [$opt_o]\n";
-unless ($opt_b) {print STDERR 'press [Enter] to continue...'; <STDIN>;}
+print STDERR "From [@ARGV]($opt_p) of [$opt_l] with [$opt_r] to [$opt_o]\n";
+unless ($opt_b) {print STDERR "Wait 3 seconds to continue...\n"; sleep 3;}
 
 my $start_time = [gettimeofday];
 #BEGIN
@@ -58,6 +60,8 @@ my $MaxQ=40;
 my $MisBase=0;
 
 my ($TotalBase,$TotalReads,%BaseCountTypeRef);
+my ($mapBase,$mapReads)=(0,0);
+my $Qascii=33;  # Sam 33, Soap 64.
 my %Stat;   # $Stat{Ref}{Cycle}{Read}{Quality}
 sub statRead($$$$$) {
     my ($ref,$isReverse,$read,$Qstr,$cyclestart)=@_;
@@ -72,7 +76,7 @@ sub statRead($$$$$) {
         my $readBase=substr $read,$i,1;
         next if $readBase eq 'N';
         my $QstrSingle=substr $Qstr,$i,1;
-        my $Qval=ord($QstrSingle)-33;
+        my $Qval=ord($QstrSingle)-$Qascii;
         if ($MaxQ<$Qval) {
             $MaxQ=$Qval;
             warn "[!] Qval=$Qval($QstrSingle) > 40 found. Remember to add -I at bwa aln for Illumina reads !\n";
@@ -91,32 +95,94 @@ sub statRead($$$$$) {
     ++$TotalReads unless $PEpos==-1;
 }
 
-while (<>) {
-    next if /^@\w\w\t\w\w:/;
-    chomp;
-    my @read1=split /\t/;
-    chomp($_=<>) or last;
-    my @read2=split /\t/;
-#print join("\t",@read1),"\n-",join("\t",@read2),"\n";
-    die "[x]Not PE sam file.\n" if $read1[0] ne $read2[0];
-    next unless $read1[1] & 3;  # paired + mapped in a proper pair
-    next if $read1[1] >= 256;   # not primary || QC failure || optical or PCR duplicate
-    next unless $read2[1] & 3;
-    next if $read2[1] >= 256;
-    next unless $read1[5] =~ /^(\d+)M$/;
-    next unless $1 == $READLEN;
-    next unless $read2[5] =~ /^(\d+)M$/;
-    next unless $1 == $READLEN;
-    next unless $read1[6] eq '=';   # same Reference sequence NAME
-    next unless $read2[6] eq '=';
-    next if $read1[11] eq 'XT:A:R'; # Type: Unique/Repeat/N/Mate-sw, N not found.
-    next if $read2[11] eq 'XT:A:R';
-    my $ref1=uc getBases($read1[2],$read1[3],$READLEN) or print join("\t",@read1),"\n";
-    my $ref2=uc getBases($read2[2],$read2[3],$READLEN) or print join("\t",@read2),"\n";
-    #my ($QNAME,$FLAG,$RNAME,$POS,$MAPQ,$CIAGR,$MRNM,$MPOS,$ISIZE,$SEQ,$QUAL,$OPT)=@read1;
-    #       0      1    2       3   4       5   6       7     8     9    10    11
-    statRead($ref1,$read1[1] & 16,$read1[9],$read1[10],1);
-    statRead($ref2,$read2[1] & 16,$read2[9],$read2[10],1+$READLEN);
+my $type;
+if ($opt_p eq 'sam') {
+    $type = 'sam';
+} elsif ($opt_p eq 'soap') {
+    $type = 'soap';
+    $Qascii = 64;
+} else {
+    chomp($_=<>) or die "[x]Empty input !\n";
+    if (/^@/) {
+        $type = 'sam';
+    } else {
+        $type = 'soap';
+        $Qascii = 64;
+        my @read1=split /\t/;
+        chomp($_=<>) or die "[x]Input too short !\n";
+        my @read2=split /\t/;
+        die "[x]Not PE soap file.\n" if $read1[0] ne $read2[0];
+        $mapBase += $read1[5]+$read2[5];
+        $mapReads +=2;
+        goto LABEL unless $read1[3] == 1 and $read2[3] == 1;  # Hit==1
+        goto LABEL if $read1[9] > 100 or $read2[9] > 100; # No Indel
+        goto LABEL unless $read1[5] == $READLEN;
+        goto LABEL unless $read2[5] == $READLEN;
+        goto LABEL unless $read1[7] eq $read2[7];   # same Reference sequence NAME
+        my $ref1=uc getBases($read1[7],$read1[8],$READLEN) or print join("\t",@read1),"\n";
+        my $ref2=uc getBases($read2[7],$read2[8],$READLEN) or print join("\t",@read2),"\n";
+        #my ($QNAME,$Seq,$Qual,$Hit,$a/b,$Len,$Strand,$Chr,$Pos,$Type,$SMID,$CIAGR,$Etc)=@read1;
+        #       0     1    2     3    4    5     6      7    8    9     10     11   12
+        statRead($ref1,$read1[6] eq '-',$read1[1],$read1[2],1);
+        statRead($ref2,$read2[6] eq '-',$read2[1],$read2[2],1+$READLEN);
+    }
+}
+LABEL:
+print "[!]Input file type is [$type].\n";
+
+if ($type eq 'sam') {
+    while (<>) {
+        next if /^@\w\w\t\w\w:/;
+        chomp;
+        my @read1=split /\t/;
+        chomp($_=<>) or last;
+        my @read2=split /\t/;
+    #print join("\t",@read1),"\n-",join("\t",@read2),"\n";
+        die "[x]Not PE sam file.\n" if $read1[0] ne $read2[0];
+        ++$mapReads if $read1[2] ne '*';
+        ++$mapReads if $read2[2] ne '*';
+        next unless $read1[1] & 3;  # paired + mapped in a proper pair
+        next if $read1[1] >= 256;   # not primary || QC failure || optical or PCR duplicate
+        next unless $read2[1] & 3;
+        next if $read2[1] >= 256;
+        next unless $read1[5] =~ /^(\d+)M$/;
+        next unless $1 == $READLEN;
+        next unless $read2[5] =~ /^(\d+)M$/;
+        next unless $1 == $READLEN;
+        next unless $read1[6] eq '=';   # same Reference sequence NAME
+        next unless $read2[6] eq '=';
+        next if $read1[11] eq 'XT:A:R'; # Type: Unique/Repeat/N/Mate-sw, N not found.
+        next if $read2[11] eq 'XT:A:R';
+        my $ref1=uc getBases($read1[2],$read1[3],$READLEN) or print join("\t",@read1),"\n";
+        my $ref2=uc getBases($read2[2],$read2[3],$READLEN) or print join("\t",@read2),"\n";
+        #my ($QNAME,$FLAG,$RNAME,$POS,$MAPQ,$CIAGR,$MRNM,$MPOS,$ISIZE,$SEQ,$QUAL,$OPT)=@read1;
+        #       0      1    2       3   4       5   6       7     8     9    10    11
+        statRead($ref1,$read1[1] & 16,$read1[9],$read1[10],1);
+        statRead($ref2,$read2[1] & 16,$read2[9],$read2[10],1+$READLEN);
+    }
+} else {
+    while (<>) {
+        #next if /^@\w\w\t\w\w:/;
+        chomp;
+        my @read1=split /\t/;
+        chomp($_=<>) or last;
+        my @read2=split /\t/;
+    #print join("\t",@read1),"\n-",join("\t",@read2),"\n";
+        die "[x]Not PE soap file.\n" if $read1[0] ne $read2[0];
+        $mapBase += $read1[5]+$read2[5];
+        $mapReads +=2;
+        next unless $read1[3] == 1 and $read2[3] == 1;  # Hit==1
+        next if $read1[9] > 100 or $read2[9] > 100; # No Indel
+        next unless $read1[5] == $READLEN;
+        next unless $read2[5] == $READLEN;
+        next unless $read1[7] eq $read2[7];   # same Reference sequence NAME
+        my $ref1=uc getBases($read1[7],$read1[8],$READLEN) or print join("\t",@read1),"\n";
+        my $ref2=uc getBases($read2[7],$read2[8],$READLEN) or print join("\t",@read2),"\n";
+        #my ($QNAME,$Seq,$Qual,$Hit,$a/b,$Len,$Strand,$Chr,$Pos,$Type,$SMID,$CIAGR,$Etc)=@read1;
+        #       0     1    2     3    4    5     6      7    8    9     10     11   12
+        statRead($ref1,$read1[6] eq '-',$read1[1],$read1[2],1);
+        statRead($ref2,$read2[6] eq '-',$read2[1],$read2[2],1+$READLEN);
+    }
 }
 
 open OA,'>',$opt_o.'.mcount' or die "Error: $!\n";
@@ -133,8 +199,10 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
 my $date=sprintf "%02d:%02d:%02d,%4d-%02d-%02d",$hour,$min,$sec,$year+1900,$mon+1,$mday;
 my $Cycle=2*$READLEN;
 my $Qcount=$MaxQ+1;
+$TotalBase=-1 unless $TotalBase;
 my $MisRate=100*$MisBase/$TotalBase;
 $tmp="#Generate @ $date by ${user}$mail
+#Input [$type] file of Reads: $mapReads , Bases $mapBase (no base stat for sam files)
 #Total statistical Bases: $TotalBase , Reads: $TotalReads of ReadLength $READLEN
 #Dimensions: Ref_base_number 4, Cycle_number $Cycle, Seq_base_number 4, Quality_number $Qcount
 #Mismatch_base: $MisBase, Mismatch_rate: $MisRate %
