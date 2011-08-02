e086e1c9e86886f2fdd8480bcc9bf21065535a40
HXs <HXs@alpha>
2011-08-02 19:35:10 +0800

    modified:   alistat.pl

--- tools/bwasam/alistat.pl
+++ tools/bwasam/alistat.pl
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Time::HiRes qw ( gettimeofday tv_interval );
 use Galaxy::ShowHelp;
-#use Data::Dump qw(dump);
+use Data::Dump qw(dump);
 
 $main::VERSION=0.0.1;
 our $opts='o:b';
@@ -290,7 +290,7 @@ sub sumintohash($$) {
     my ($inhref,$intohref)=@_;
     for my $key (keys %{$inhref}) {
         if (ref($$inhref{$key}) eq 'HASH') {
-            &mergehash($$inhref{$key},$$intohref{$key});
+            &sumintohash($$inhref{$key},$$intohref{$key});
         } else {
             $$intohref{$key} += $$inhref{$key};
         }
@@ -313,8 +313,8 @@ while($_=shift @ARGV) {
         $mapPair += $ret->[1]->[1];
         $mapSingle += $ret->[1]->[2];
         if ($ret->[0] eq 'PE') {
-            &sumintohash(\%DatSum,$ret->[2]) if $ret->[2];
-            &sumintohash(\%InsD,$ret->[3]) if $ret->[3];
+            &sumintohash($ret->[2],\%DatSum) if $ret->[2];
+            &sumintohash($ret->[3],\%InsD) if $ret->[3];
         }
         #print "\n[";dump($ret);print "]\n";
     } else {
@@ -325,176 +325,7 @@ while($_=shift @ARGV) {
 }
 warn "[!]Files Read: $files\n";
 
+print "\n[";dump([$InReads,$mapPair,$mapSingle,\%DatSum,\%InsD]);print "]\n";
 
 __END__
 
-my ($PESE,$rlins,$G,$opath,$Ref,$fqextpath,$fqname,$u) = @ARGV;
-#print '[',join('] [',@ARGV),"]\n";
-#print '[',join('] [',$PESE,$ins,$G,$Ref,$fqextpath,@fqnames),"]\n";
-my ($readlen,$ins)=split ',',$rlins;
-my ($ext,$path)=split ',',$fqextpath;
-my @fqnames=split ',',$fqname;
-
-}
-my ($n,$fqcmd,$tmpcmd,$avg,$std,$Lsd,$Rsd,$max_y,$max_x)=(0);
-$ext .= '.gz' unless -s "$path$fqnames[0]$ext";	# Well, sometime we gzip those .fqs later on and ...
-if ($PESE eq 'PE') {	# PE
-###
-	#open TP,'<',"$opath$fqnames[0].tp";
-	open( TP,"-|","gzip -dc $opath$fqnames[0].tp.gz") or die "Error: $!\n";
-	my ($pairs,$lastpos,$line1,$line2,$pp,$pn,$calins,%insD)=(0);
-	while ($line1=<TP>) {
-		last if eof TP;
-		$lastpos=tell TP;
-		my ($id1, $n1, $len1, $f1, $chr1, $x1, $m1) = (split "\t", $line1)[0,3,5,6,7,8,-2];
-		$line2=<TP>;
-		my ($id2, $n2, $len2, $f2, $chr2, $x2, $m2) = (split "\t", $line2)[0,3,5,6,7,8,-2];
-			#($soapid,$hit,$len,$strand,$chr,$pos,$trim) = (split(/\t/))[0,3,5,6,7,8,-2];
-		$id1 =~ s/\/[12]$//;
-		$id2 =~ s/\/[12]$//;
-		if ($id1 ne $id2){	# single
-			seek (TP, $lastpos, 0);
-			next;
-		}
-		next if $n1+$n2>2 or $chr1 ne $chr2;
-		if ($f1 eq '+') {
-			($pp,$pn)=($x1,$x2);
-		} else {
-			($pp,$pn)=($x2,$x1);
-		}
-		if ($ins > 1500) {	# FR => +.pos < -.pos; RF => -.pos < +.pos
-			next if $pp < $pn;
-		} else { next if $pp > $pn; }
-		++$pairs;
-		$line1=&getRealpos($len1, $f1, $x1, $m1);	# $len,$strand,$realpos,$trim
-		$line2=&getRealpos($len2, $f2, $x2, $m2);	# Well, $line{1,2} is recycled.
-		$calins=abs($line1-$line2);	# -.starting=0
-		++$insD{$calins};
-	}
-	close TP;
-	my ($sum,$sum2,$v)=(0,0);
-	open O,'>',"$opath$fqnames[0].insD";
-	for my $k (sort {$a <=> $b} keys %insD) {
-		$v=$insD{$k};
-		print O "$k\t$v\n";
-		$sum += $k * $v;
-		$n += $v;
-		$sum2 += $k*$k * $v;
-	}
-	$avg = $sum/$n;
-	$std = sqrt($sum2/$n-$avg*$avg);
-	print O "# $avg ± $std\n";
-	($max_y,$max_x)=(-1,0);
-	for my $k ($avg-$std .. $avg+$std) {	# 68.27%
-		next unless $v=$insD{$k};
-		if ($max_y < $v) {
-			$max_x = $k;
-			$max_y = $v;
-		}
-	}
-	my $cutoff = $max_y / 1000;
-	$cutoff = 3 if $cutoff<3;
-	my ($diff, $Lc, $Rc);
-	for my $k (keys %insD) {
-		$v=$insD{$k};
-		next if $v < $cutoff;
-		$diff = $k - $max_x;
-		if ($diff < 0) {
-			$Lsd += $v * $diff * $diff;
-			$Lc += $v;
-		}
-		elsif ($diff > 0) {
-			$Rsd += $v * $diff * $diff;
-			$Rc += $v;
-		}
-	}
-	$Lsd = sqrt($Lsd/$Lc);
-	$Rsd = sqrt($Rsd/$Rc);
-	print O "# +$Lsd -$Rsd\n";
-	close O;
-	($min,$max)=(int($avg-$Lsd*2.576-$soap2patch),int($avg+$Rsd*2.576+$soap2patch+.5));	# 99%
-}
-my ($Pairs,$Paired,$Singled,$Reads,$Alignment);
-
-open NFO,'>',"$opath$fqnames[0].nfo" or die "[x]Error opening $opath$fqnames[0].nfo: $!\n";
-if ($max==0) {
-	print NFO "#fmtS\tTotal_Reads\tAlignment\n";
-	print NFO "Summary\t",join("\t",$Reads,$Alignment),"\n";
-	@ARGV=("$opath$fqnames[0].se");
-} else {
-	my $p=sprintf "%.2f",100*$max_y/$n;
-	$avg=int($avg*10+.5)/10;
-	$Lsd=int($Lsd*100+.5)/100;
-	$Rsd=int($Rsd*100+.5)/100;
-	print NFO "#fmtS\tTotal_Pairs\tPaired\tSingled\tMode(p%),Lsd,Rsd,InsAvg,STD\tInsMin,InsMax\n";
-	print NFO "Summary\t",join("\t",$Pairs,$Paired,$Singled,"$max_x($p %),$Lsd,$Rsd,$avg,$std","$min,$max"),"\n";
-	#@ARGV=("$opath$fqnames[0].soap", "$opath$fqnames[0].single");
-	open(OUTS,"-|","gzip -dc $opath$fqnames[0].soap.gz $opath$fqnames[0].single.gz") or die "Error: $!\n";
-}
-my ($BadLines,$BPOut,$ReadsOut,$MisSum,$TrimedBP,$TrimedReads,%Hit9r,%Hit9bp,%misMatch,%Indel)=(0,0,0,0,0,0);
-my (%chrBPOut,%chrReadsOut,%chrMisSum,%chrTrimedBP,%chrTrimedReads,%chrHit9r,%chrHit9bp,%chrmisMatch,%chrIndel);
-# for 46999 ChrIDs, one hash took 12m RES for {}=$ and 125m VIRT for {}{10}=$, thus fine to work with scaffolds. ( 1 gb VIRT max ? )
-my (@lines,$hit,$len,$chr,$types,$trim,$mistr,$missed);
-while (<OUTS>) {
-	@lines = split /\t/;
-	if (@lines > 10) {	# soap2 output always more than 10 columes.
-		($hit,$len,$chr,$types,$trim,$mistr) = @lines[3,5,7,9,-2,-1];
-		$BPOut += $len;	$chrBPOut{$chr} += $len;
-		++$ReadsOut;	++$chrReadsOut{$chr};
-		$missed=$mistr=~tr/ATCGatcg//;
-		++$misMatch{$missed};
-		++$chrmisMatch{$chr}{$missed};
-		$MisSum += $missed;
-		$chrMisSum{$chr} += $missed;
-
-		$hit=4 if $hit>4;	# max to count 3, then >=4. Ancient Chinese wisdom, and a bit more ...
-		++$Hit9r{$hit};
-		++$chrHit9r{$chr}{$hit};
-		$Hit9bp{$hit} += $len;
-		$chrHit9bp{$chr}{$hit} += $len;
-=pod
-		if ($types < 100) {
-			#++$misMatch{$types};
-			#++$chrmisMatch{$chr}{$types};
-		} elsif ($types < 200) {	# '3S33M9D39M', '32M1D14M29S' exists ?
-			++$Indel{$types-100};
-			++$chrIndel{$chr}{$types-100};
-		} else {
-			++$Indel{200-$types};
-			++$chrIndel{$chr}{200-$types};
-		}
-=cut
-		if ($types > 200) {
-			++$Indel{200-$types};
-			++$chrIndel{$chr}{200-$types};
-		} elsif ($types > 100) {
-			++$Indel{$types-100};
-			++$chrIndel{$chr}{$types-100};
-		}
-
-		@lines = $trim =~ /(\d+)S/;
-		if (@lines) {
-			++$TrimedReads;
-			++$chrTrimedReads{$chr};
-			for ( @lines ) {
-				$TrimedBP += $_;
-				$chrTrimedBP{$chr} += $_;
-			}
-		}
-	} else {	# rare event ...
-		++$BadLines;
-		next;
-	}
-}
-close OUTS;
-
-print NFO "\n#fmtC\tReadsOut\tBPOut\tMisSum\tTrimedReads\tTrimedBP\tmisMatchReads\tReads\@Hit\tBP\@Hit\tIndelReads\tBadLines\n";
-print NFO join("\t",'ALL',$ReadsOut,$BPOut,$MisSum,$TrimedReads,$TrimedBP,
-	${&combineC(\%misMatch)},${&combineJ(\%Hit9r)},${&combineJ(\%Hit9bp)},${&combineJ(\%Indel)},$BadLines),"\n\n";
-print NFO "#fmtP\tReadsOut\tBPOut\tMisSum\tTrimedReads\tTrimedBP\tmisMatchReads\tReads\@Hit\tBP\@Hit\tIndelReads\n";
-print NFO join("\t",";$_",$chrReadsOut{$_},$chrBPOut{$_},$chrMisSum{$_}||0,$chrTrimedReads{$_}||0,$chrTrimedBP{$_}||0,
-	${&combineC(\%{$chrmisMatch{$_}})},${&combineJ(\%{$chrHit9r{$_}})},${&combineJ(\%{$chrHit9bp{$_}})},${&combineJ(\%{$chrIndel{$_}})}),"\n" for sort keys %chrReadsOut;
-close NFO;
-
-END:
-#system("bzip2 -9 $opath$fqnames[0].unmap");
