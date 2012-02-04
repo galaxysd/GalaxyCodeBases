bf5bc78feb7b15c360605ec7a042916c612f46f1
Yuuki Galaxy <galaxy001@gmail.com>
2012-02-05 03:44:30 +0800

    modified:   statdep.pl, faster now.

--- perl/etc/coveragestackplot/statdep.pl
+++ perl/etc/coveragestackplot/statdep.pl
@@ -4,20 +4,9 @@ use warnings;
 use Inline 'Info', 'Force', 'Noclean';
 use Inline 'C';
 
-use Data::Dump 'ddx';
+#use Data::Dump 'ddx';
 #use Inline 'C' => 'Config' => 'OPTIMIZE' => '-march=core2 -mtune=generic -O2 -pipe' ;
-=pod
-my $text = join '', <>;           # slurp input file
-my $vp = vowel_scan($text);       # call our function
-$vp = sprintf("%03.1f", $vp * 100);  # format for printing
-print "The letters in @ARGV are $vp% vowels.\n";
-#=cut
-my $a=testm();
-print testp($a);
-print testf($a),"\n";
-my $tt;
-testundef($tt);
-=cut
+
 die "Usage: $0 <chr.info> <list> [output]\n" if @ARGV <2;
 my ($nfo,$in,$out)=@ARGV;
 $out=$in.'.depstat' unless $out;
@@ -103,7 +92,7 @@ while (%IFH) {
 			for ( split /\s+/,$tmp ) {	# no need to chomp for /\s+/
 				push @dat,$_ if /^\d+$/;
 			}
-			next unless @dat;
+			#next unless @dat;
 			$CVGPOS{$id} = chrdat_push($CVGDAT{$id},@dat);
 =pod
 			for my $v (@dat) {
@@ -115,8 +104,8 @@ while (%IFH) {
 =cut
 #print STDERR "\n${Filechrid}[$id]$CVGPOS{$id}<$tmp>\n";
 		}
-my @t=chrdat_readzone($CVGDAT{$id},1,$ChrLen{$Filechrid});
-print STDERR "Read:",scalar @t,"[",join(',',@t),"]\n";
+#my @t=chrdat_readzone($CVGDAT{$id},1,$ChrLen{$Filechrid});
+#print STDERR "Read:",scalar @t,"[",join(',',@t),"]\n";
 		die "[x]Depth file format error in Chr:$chrid for $CVGPOS{$id} != $ChrLen{$Filechrid}." if $CVGPOS{$id} != $ChrLen{$Filechrid};
 		unless (defined $tmp) {	# file end
 			close $fh;
@@ -129,7 +118,7 @@ print STDERR "Read:",scalar @t,"[",join(',',@t),"]\n";
 		die "\n[x]ChrID order not match for [$chrid] in [$IFname{$id}] & [$CurrentChrID] !" if $CurrentChrID ne $chrid;
 		++$cnt;
 	}
-	warn "\b\b\b\bdone !\n";
+	warn "\b\b\b\bdone of $ChrLen{$Filechrid} !\n";
 }
 
 for my $id (keys %CVGDAT) {
@@ -177,22 +166,22 @@ void chrdat_resize(void * ptr, size_t size) {
 	}
 	pt->dat = newpt;
 	pt->len = size;
-	pt->position = 0;
+	pt->position = 0;	// rewind here.
 }
 void chrdat_rewind(void * ptr) {
 	ChrDat_t* pt = ptr;
 	pt->position = 0;
 }
-SV* chrdat_push(void * ptr, SV* name1, ...) {
+SV* chrdat_push(void * ptr, ...) {
 	ChrDat_t* pt = ptr;
 	uint16_t * pDat = pt->dat + pt->position;
 	Inline_Stack_Vars;
 	int i;
-fputs("-[",stderr);
-	for (i = 1; i < Inline_Stack_Items; i++) {
+//fputs("-[",stderr);
+	for (i = 1; i < Inline_Stack_Items; i++) {	// ST(0) is ptr
 		*pDat++ = (uint16_t) SvIV(Inline_Stack_Item(i));
 		++pt->position;
-fprintf(stderr, "%zd:%d\t", pt->position, *(pDat-1) );
+//fprintf(stderr, "%zd:%d\t", pt->position, *(pDat-1) );
 		if (pt->position > pt->len) {
 			fputs("[x]Array too long !\n",stderr);
 			exit(2); // Is it safe to exit here ?
@@ -200,7 +189,7 @@ fprintf(stderr, "%zd:%d\t", pt->position, *(pDat-1) );
 	}
 	//Inline_Stack_Void;
 	Inline_Stack_Done;
-fprintf(stderr, "\b]-\n");
+//fprintf(stderr, "\b]-\n");
 	//pt->position = pDat - pt->dat;
 	return newSViv(pt->position);
 }
@@ -219,79 +208,3 @@ void chrdat_readzone(void * ptr, size_t start, size_t end) {
 	}
 	Inline_Stack_Done;
 }
-
-/*
-void* memalloc(size_t size) {
-	void * pt = malloc(size * 2);
-	if (pt == NULL) {
-		fputs("[x]Not enough memory !\n",stderr);
-		exit(1); // Is it safe to exit here ?
-	}
-	return pt;
-}
-void* memrealloc(void *ptr, size_t size) {
-	void * pt = realloc(ptr, size * 2);
-	if (pt == NULL) {
-		fputs("[x]Not enough memory !\n",stderr);
-		free(ptr);
-		exit(1); // Is it safe to exit here ?
-	}
-	return pt;
-}
-void memfree(void* pt) {
-	free(pt);
-}
-void pushValue(void* pt, size_t position, unsigned int value) {
-	//printf("@%zx:%d\t",position,value);
-	*((uint16_t*)pt + position) = (uint16_t)value;
-}
-
-
-void* testundef(void *ptr) {
-	if (ptr == NULL) {
-		fputs("It is NULL.\n",stderr);
-	} else {
-		printf("[%lx]\n",*(uint64_t*)ptr);
-	}
-}
-void* testm() {
-	void * pt = calloc(1, 65537);
-	sprintf(pt,"[Test Strings:%c%c%c.]",33,9,64);
-	return pt;
-}
-SV* testp(void* pt) {
-	return (newSVpvf("<%s>\n", (char*)pt));
-}
-int testf(void* pt) {
-	free(pt);
-	return 0;
-}
-*/
-/*
-// Find percentage of vowels to letters
-double vowel_scan(char* str) {
-	int letters = 0;
-	int vowels = 0;
-	int i = 0;
-	char c;
-	char normalize = 'a' ^ 'A';
-	// normalize forces lower case in ASCII; upper in EBCDIC
-	char A = normalize | 'a';
-	char E = normalize | 'e';
-	char I = normalize | 'i';
-	char O = normalize | 'o';
-	char U = normalize | 'u';
-	char Z = normalize | 'z';
-
-	while(c = str[i++]) {
-		c |= normalize;
-		if (c >= A && c <= Z) {
-			 letters++;
-			 if (c == A || c == E || c == I || c == O || c == U)
-				 vowels++;
-		}
-	}
-
-	return letters ? ((double) vowels / letters) : 0.0;
-}
-*/
