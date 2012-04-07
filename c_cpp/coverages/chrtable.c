2e51d8f3529a14689962c5b2a29c04af7183a471
Yuuki Galaxy <galaxy001@gmail.com>
2012-04-07 08:14:57 +0800

    modified:   chrtable.c

--- c_cpp/coverages/chrtable.c
+++ c_cpp/coverages/chrtable.c
@@ -50,24 +50,44 @@ void do_stat(bam1_t *b, const uint16_t overlap, uint8_t **ChrDat) {
 char tmpChr[255];
 int do_contig(const uint8_t mindep, const struct myData * Data, const char * const outfile) {
     sprintf(tmpChr,"_%u.contig.gz",mindep);
+    gzFile *fp = gzopen(strlinker(outfile,tmpChr), "wb9");
+#ifdef DEBUG
+    sprintf(tmpChr,"_%u.kdepth.gz",mindep);
     gzFile *fpd = gzopen(strlinker(outfile,tmpChr), "wb9");
-    gzprintf(fpd,"#minDepth = %u\n",mindep);
+#endif
+    gzprintf(fp,"#minDepth = %u\n",mindep);
     int_fast8_t inContig=0;
+    int32_t lastStart=0;
     for (int32_t i=0; i < Data->n_targets; ++i) {
+        gzprintf(fp,"#-------%s--------\n",Data->target_name[i]);
+#ifdef DEBUG
+        gzprintf(fpd,">%s\n",Data->target_name[i]);
+#endif
         for (int32_t p=0; p < Data->target_len[i]; ++p) {
             if ( (Data->ChrDat[i][p] >= mindep) ) {
                 if (!inContig) {
-                    gzprintf(fpd,"%s\t%i\t",Data->target_name[i],p);
+                    gzprintf(fp,"%s\t%i\t",Data->target_name[i],p+1);
                     inContig=1;
+                    lastStart = p;
                 }
             } else {
                 if (inContig) {
-                    gzprintf(fpd,"%i\n",p);
+                    gzprintf(fp,"%i\t%i\n",p+1,p-lastStart+1);
                     inContig=0;
                 }
             }
+#ifdef DEBUG
+            gzprintf(fpd,"%i:%u ",p,Data->ChrDat[i][p]);
+            if (p % 50 == 0) gzprintf(fpd,"\n");
+#endif
         }
+#ifdef DEBUG
+        gzprintf(fpd,"\n");
+#endif
     }
+    gzclose(fp);
+#ifdef DEBUG
     gzclose(fpd);
+#endif
     return 0;
 }
