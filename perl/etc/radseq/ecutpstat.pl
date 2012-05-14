68af64d9028f900c1db62ddff0b780c1352ec701
Simba Galaxy <galaxy001@gmail.com>
2012-05-14 20:13:40 +0800

    modified:   ecutpstat.pl

--- perl/etc/radseq/ecutpstat.pl
+++ perl/etc/radseq/ecutpstat.pl
@@ -16,7 +16,7 @@ my $EcutAt=5;
 
 open O,'>',$out or die "Error opening $out : $!\n";
 print O "# Ref: [$fa]\n#Enzyme: [$Eseq], Cut after $EcutAt\n\n#ChrID\tCut\n";
-my ($CountAll,%Count);
+my ($CountAll,%Count,%Len);
 
 open I,'<',$fa or die "Error opening $fa : $!\n";
 while (<I>) {
@@ -31,6 +31,8 @@ while (<I>) {
 	$/="\n";
 	#print STDERR "\b\b\b   \t",length $genome,".\n";
 
+	$Count{$seqname} = 0;
+	$Len{$seqname} = length $genome;
 	while ( $genome =~ m/$Eseq/g ) {
 		# push @ret, [ $-[0], $+[0] ];
 		print O join("\t",$seqname,$-[0]+$EcutAt),"\n";	# starts from 0, so OK to use directly with $EcutAt for "cut after".
@@ -42,9 +44,12 @@ while (<I>) {
 }
 close I;
 
-print O "\n#Cut stat:\n";
+my @notCut;
+print O "\n#Cut stat:\n#ChrID\tLen\tCut\tAvg_distance\n";
 for (sort keys %Count) {
-	print O "# $_\t$Count{$_}\n"
+	print O '# ',join("\t",$_,$Len{$_},$Count{$_},$Count{$_}?int(0.5 + $Len{$_}/(1+$Count{$_})):'.'),"\n";
+	push @notCut,$_ unless $Count{$_};
 }
-print O "#Total: $CountAll";
+print O "#Total: $CountAll\n#notCut: ",scalar @notCut,"\n";
+print O "#notCut:[",join(',',@notCut),"]\n" if @notCut;
 close O;
