a30aa6034b49a3fdea3405b1017fa030918088e8
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-19 20:45:10 +0800

    modified:   checkbam.sh

--- tools/etc/checkbam.sh
+++ tools/etc/checkbam.sh
@@ -10,20 +10,22 @@ if [ x"$2" = x"" ]; then
 else
    TOFIND="$2"
 fi
+TOFIND=`readlink -nf ${TOFIND}`
 
 date > $1
 echo "From [${TOFIND}] to [$1]:"
 echo "Find in [${TOFIND}]" >> $1
 
-find ${TOFIND} -name '*.bam' | while read a;
+find -L ${TOFIND} -name '*.bam' | while read a;
  do echo -n "Checking [$a] ...";
     RES=`gzip -t "$a" 2>&1`;
     RETVAL=$?;
     if [ "${RETVAL}" -ne "0" ]; then
-       echo -e "\b\b\b\b: Error=${RETVAL}.${RES}"
+       echo -e "\b\b\b\b:Error=${RETVAL}.${RES}"
        echo "[e${RETVAL}] ${a}${RES}" >> $1
     else
-       echo -e "\b\b\b\b: OK."
+       echo -e "\b\b\b\b:OK."
+       echo "[ok] ${a}" >> $1
     fi;
  done
 
