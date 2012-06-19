913f22da6a9d6cc754652cfabea7474a9f40dc2b
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-19 20:25:21 +0800

    $? does not remains, have to store it first. 	modified:   tools/etc/checkbam.sh

--- tools/etc/checkbam.sh
+++ tools/etc/checkbam.sh
@@ -18,9 +18,10 @@ echo "Find in [${TOFIND}]" >> $1
 find ${TOFIND} -name '*.bam' | while read a;
  do echo -n "Checking [$a] ...";
     RES=`gzip -t "$a" 2>&1`;
-    if [ "$?" -ne "0" ]; then
-       echo -e "\b\b\b\b: Error=${?}.${RES}"
-       echo "[e${?}] ${a}${RES}" >> $1
+    RETVAL=$?;
+    if [ "${RETVAL}" -ne "0" ]; then
+       echo -e "\b\b\b\b: Error=${RETVAL}.${RES}"
+       echo "[e${RETVAL}] ${a}${RES}" >> $1
     else
        echo -e "\b\b\b\b: OK."
     fi;
