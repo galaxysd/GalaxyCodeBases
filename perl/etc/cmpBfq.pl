57dc18474791862dfc3a0b07965210bdf75b897f
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-25 21:51:35 +0800

    modified:   cmpBfq.pl

--- perl/etc/cmpBfq.pl
+++ perl/etc/cmpBfq.pl
@@ -21,14 +21,21 @@ my ($Count1,$Count2,$CountPairs)=(0,0,0);
 sub doCMP($$$);
 
 my ($dat1,$dat2);
-my ($maskedQ,$mlen,$rlen,$ret,%Count,%MaskLen,%Readlen);
+my ($maskedQ,$mlen,$rlen,$t,$ret,%Count,%MaskLen,%Readlen,%ReadType);
 my ($totBase,$totMaskedBP,$totMaskedReads)=(0,0,0);
+my ($totBaseC,$totMaskedBPC,$totMaskedReadsC)=(0,0,0);
 while (1) {
 	$dat1 = readfq($fha, \@aux1);	# [$name, $comment, $seq, $qual]
 	$dat2 = readfq($fhb, \@aux2);
 	if ($dat1 && $dat2) {
 		($maskedQ,$mlen) = @{doEamss($$dat1[2],$$dat1[3])};
-		#print "3-",join('|',$$dat1[0],$$dat1[1],$mlen),"\n$$dat1[2]\n$$dat1[3]\n$maskedQ\n";
+		if ($$dat1[1] =~ /(\d):([YN]):\d+:([ATCG]+)?/) {	# 1:N:0:TGACCA
+			$t = ($1 eq '2')? 1 : 0;
+			$t |= 2 if ($2 eq 'Y');
+			$t |= 4 if $3;
+			++$ReadType{$t};	# 1:Read2, 2:Casava-filtered, 3:with-index
+		}
+		#print "3-",join('|',$$dat1[0],$$dat1[1],$mlen,$t),"\n$$dat1[2]\n$$dat1[3]\n$maskedQ\n";
 		$ret = doCMP($dat1,$dat2,$maskedQ);
 		++$Count{$ret};
 		++$MaskLen{$mlen};
@@ -39,7 +46,12 @@ while (1) {
 		if ($mlen) {
 			++$totMaskedReads;
 			$totMaskedBP += $mlen;
+			unless ($t | 2) {
+				$totMaskedBPC += $mlen;
+				++$totMaskedReadsC;
+			}
 		}
+		$totBaseC += $rlen unless ($t & 2);
 	} elsif ($dat1) {
 		++$Count1;
 		print "1-",join('|',@$dat1),"\n";
@@ -59,8 +71,12 @@ my $str = "Out Pairs: $CountPairs\nFQ1 over hang: $Count1\nFQ2 over hang: $Count
 print $str;
 print LOG "From [$inA]&[$inB] to [$out.stat]\n$str";
 
-my $t = int(0.5+10*$totMaskedBP/$totMaskedReads)/10;
-$str = "\nTotal Reads, Base: $CountPairs, $totBase\nMasked Reads, Base: $totMaskedBP, $totMaskedReads\nAvg Masked Length: $t\n\n";
+$totMaskedReads = -1 unless $totMaskedReads;
+$t = int(0.5+10*$totMaskedBP/$totMaskedReads)/10;
+$str = "\nTotal Reads, Base: $CountPairs, $totBase\tMasked Reads, Base: $totMaskedBP, $totMaskedReads\tAvg Masked Length: $t\n";
+$totMaskedReadsC = -1 unless $totMaskedReadsC;
+$t = int(0.5+10*$totMaskedBPC/$totMaskedReadsC)/10;
+$str .= "After Casava-filter:\nTotal Base: $totBaseC\tMasked Reads, Base: $totMaskedBPC, $totMaskedReadsC\tAvg Masked Length: $t\n\n";
 print $str;
 print LOG $str,"@ $totBase\t$totMaskedBP\t$inA\n";
 
@@ -71,18 +87,25 @@ for my $k (sort {$a <=> $b} keys %Count) {
 print $str;
 print STAT $str;
 
+$str="\n[Read_Type] (1:Read2, 2:Casava-filtered, 4:with-index)\n";
+for my $k (sort {$a <=> $b} keys %ReadType) {
+	$str .= sprintf("%#06b\t(%#x)\t%d\n",$k,$k,$ReadType{$k});
+}
+print $str;
+print STAT $str;
+
 $str="\n[Read_Length]\n";
 for my $k (sort {$a <=> $b} keys %Readlen) {
 	$str .= sprintf("%d\t%d\n",$k,$Readlen{$k});
 }
-print $str;
+#print $str;
 print STAT $str;
 
 $str="\n[Masked_Length]\n";
 for my $k (sort {$a <=> $b} keys %MaskLen) {
 	$str .= sprintf("%d\t%d\n",$k,$MaskLen{$k});
 }
-print $str;
+#print $str;
 print STAT $str;
 
 close LOG;
