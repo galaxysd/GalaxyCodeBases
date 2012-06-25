9f1c3c4e1b9372cffc456f447a648b0aa05fe81e
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-25 23:16:02 +0800

    modified:   cmpBfq.pl

--- perl/etc/cmpBfq.pl
+++ perl/etc/cmpBfq.pl
@@ -5,10 +5,14 @@ use Galaxy::IO::FASTAQ;
 use Galaxy::IO;
 use Galaxy::Casava::Eamss qw(doEamss);
 
+$SIG{INT} = \&stopnow;
+
 die "Usage: $0 <ori-fq> <B-masked-fq> [outprefix].(log|stat)\n" if @ARGV <2;
 my ($inA,$inB,$out)=@ARGV;
 $out=$inA unless $out;
 warn "From [$inA]&[$inB] to [$out].(log|stat)\n";
+
+open LOG,'>',"${out}.log" or die "Error opening $out.log:$!\n";
 
 my $fha=openfile($inA);
 my $fhb=openfile($inB);
@@ -33,12 +37,20 @@ while (1) {
 			$t = ($1 eq '2')? 1 : 0;
 			$t |= 2 if ($2 eq 'Y');
 			$t |= 4 if $3;
-			++$ReadType{$t};	# 1:Read2, 2:Casava-filtered, 3:with-index
+			#++$ReadType{$t};	# 1:Read2, 2:Casava-filtered, 3:with-index
 		}
 		#print "3-",join('|',$$dat1[0],$$dat1[1],$mlen,$t),"\n$$dat1[2]\n$$dat1[3]\n$maskedQ\n";
+RECMP:
 		$ret = doCMP($dat1,$dat2,$maskedQ);
+if ($ret & 1) {
+	++$ReadType{$t|16};
+	$dat2 = readfq($fhb, \@aux2);
+	goto RECMP;
+}
+# due to a mistake upstream in the masked FASTQ file ...
+		++$ReadType{$t};
 		++$Count{$ret};
-		++$MaskLen{$mlen};
+		++$MaskLen{$mlen}->[0];
 		$rlen = length $$dat1[2];
 		++$Readlen{$rlen};
 		++$CountPairs;
@@ -51,7 +63,10 @@ while (1) {
 				++$totMaskedReadsC;
 			}
 		}
-		$totBaseC += $rlen unless ($t & 2);
+		unless ($t & 2) {
+			$totBaseC += $rlen;
+			++$MaskLen{$mlen}->[1];
+		}
 	} elsif ($dat1) {
 		++$Count1;
 		print "1-",join('|',@$dat1),"\n";
@@ -65,7 +80,6 @@ while (1) {
 close $fha;
 close $fhb;
 
-open LOG,'>',"${out}.log" or die "Error opening $out.log:$!\n";
 open STAT,'>',"${out}.stat" or die "Error opening $out.stat:$!\n";
 my $str = "Out Pairs: $CountPairs\nFQ1 over hang: $Count1\nFQ2 over hang: $Count2\n";
 print $str;
@@ -73,14 +87,14 @@ print LOG "From [$inA]&[$inB] to [$out.stat]\n$str";
 
 $totMaskedReads = -1 unless $totMaskedReads;
 $t = int(0.5+10*$totMaskedBP/$totMaskedReads)/10;
-$str = "\nTotal Reads, Base: $CountPairs, $totBase\tMasked Reads, Base: $totMaskedBP, $totMaskedReads\tAvg Masked Length: $t\n";
+$str = "\nTotal Reads, Base: $CountPairs, $totBase\tMasked Reads, Base: $totMaskedReads, $totMaskedBP\tAvg Masked Length: $t\n";
 $totMaskedReadsC = -1 unless $totMaskedReadsC;
 $t = int(0.5+10*$totMaskedBPC/$totMaskedReadsC)/10;
-$str .= "After Casava-filter:\nTotal Base: $totBaseC\tMasked Reads, Base: $totMaskedBPC, $totMaskedReadsC\tAvg Masked Length: $t\n\n";
+$str .= "After Casava-filter:\nTotal Base: $totBaseC\tMasked Reads, Base: $totMaskedReadsC, $totMaskedBPC\tAvg Masked Length: $t\n\n";
 print $str;
-print LOG $str,"@ $totBase\t$totMaskedBP\t$inA\n";
+print LOG $str,"@ ",join("\t",$totBaseC,$totMaskedBPC,$totBase,$totMaskedBP,$inA),"\n";
 
-$str="[Compared] (0 or 8 is OK)\n";
+$str="[Compared] (0 or 8 is OK, diff for id,comment,seq,q,eamss)\n";
 for my $k (sort {$a <=> $b} keys %Count) {
 	$str .= sprintf("%#06b\t(%#x)\t%d\n",$k,$k,$Count{$k});
 }
@@ -101,9 +115,11 @@ for my $k (sort {$a <=> $b} keys %Readlen) {
 #print $str;
 print STAT $str;
 
-$str="\n[Masked_Length]\n";
+$str="\n[Masked_Length] (all, After_Casava-filter)\n";
 for my $k (sort {$a <=> $b} keys %MaskLen) {
-	$str .= sprintf("%d\t%d\n",$k,$MaskLen{$k});
+	$MaskLen{$k}->[0] = 0 unless $MaskLen{$k}->[0];
+	$MaskLen{$k}->[1] = 0 unless $MaskLen{$k}->[1];
+	$str .= sprintf("%d\t%d\t%d\n",$k,$MaskLen{$k}->[0],$MaskLen{$k}->[1]);
 }
 #print $str;
 print STAT $str;
@@ -119,5 +135,12 @@ sub doCMP($$$) {
 		$flag |= 1<<$i if $$dat1[$i] ne $$dat2[$i];
 	}
 	$flag |= 1<<4 if $maskedQ ne $$dat2[3];
-	return $flag;
+	return $flag;	# id,comment,seq,q,eamss
+}
+
+sub stopnow() {
+	$SIG{INT} = \&stopnow;
+	warn "\nCtrl+C detected, stop and saving ...\n";
+	print LOG "Ctrl+C detected. Stopped in mid-way.\n";
+	$dat1 = $dat2 = undef;
 }
