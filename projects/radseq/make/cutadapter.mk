8d27543060e81adfd03d06c369fa8b0a0e40e0de
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-30 05:49:03 +0800

    modified:   make/cutadapter.mk

--- projects/radseq/make/cutadapter.mk
+++ projects/radseq/make/cutadapter.mk
@@ -7,6 +7,7 @@ ADAPTER23:=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT
 ADAPTER25:=GTGACTGGAGTTCAGACGTGTGCTCTTCCGATCT
 
 CUTADAPTARG:=-e 0.08 -n 2 -m 0 -O 5
+CUTADRADARG:=-e 0.08 -n 1 -m 0 -O 5
 
 FQEXTS:=.fq.gz
 CUTADAPTCMD:=cutadapt
@@ -23,10 +24,12 @@ all: $(RADSEQFQS1:.1.fq.gz=.rad) $(COMMONFQS:.fq.gz=)
 	@echo all [$@] [$<]
 
 %.1: %.1.fq.gz
-	@echo 1 $(CUTADAPTCMD) [$@] [$<]
+	@echo "1 $(CUTADAPTCMD) -a $(ADAPTER13) -g $(ADAPTER15) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log"
 
 %.2: %.2.fq.gz
-	@echo 2 $(CUTADAPTCMD) [$@] [$<]
+	@echo "2 $(CUTADAPTCMD) -a $(ADAPTER23) -g $(ADAPTER25) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log"
 
 %.rad: %.1.fq.gz
-	@echo RAD1 $(CUTADAPTCMD) [$@] [$<]
+	@echo "RAD1 $(CUTADAPTCMD) -a $(ADAPTER13) $(CUTADRADARG) -o $(@:.rad=).cut1.gz -r $(@:.rad=).rest1.gz $< > $(@:.rad=).cut1.log"
+	@echo perl $(@:.rad=).cut1.gz $(@:.rad=).cutM.gz
+	@echo "RAD2 $(CUTADAPTCMD) -g $(ADAPTER15) $(CUTADRADARG) -o $(@:.rad=).cut2.gz -r $(@:.rad=).rest2.gz $(@:.rad=).cutM.gz > $(@:.rad=).cut2.log"
