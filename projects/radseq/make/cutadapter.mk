97f71fc222e8ab7387c2d89c1dd7726ef5b87d90
Yuuki Galaxy <galaxy001@gmail.com>
2012-06-30 07:10:31 +0800

    modified:   make/makefile

--- projects/radseq/make/cutadapter.mk
+++ projects/radseq/make/cutadapter.mk
@@ -16,23 +16,30 @@ RADSEQFQS1 = $(foreach d,$(RADSEQFQPATH),$(wildcard $(addprefix $(d)/*,.1$(FQEXT
 RADSEQFQS2 = $(foreach d,$(RADSEQFQPATH),$(wildcard $(addprefix $(d)/*,.2$(FQEXTS))))
 WGSFQS1 = $(foreach d,$(WGSFQPATH),$(wildcard $(addprefix $(d)/*,.1$(FQEXTS))))
 WGSFQS2 = $(foreach d,$(WGSFQPATH),$(wildcard $(addprefix $(d)/*,.2$(FQEXTS))))
-COMMONFQS := $(RADSEQFQS2) $(WGSFQS1) $(WGSFQS2)
+COMMONFQS := $(RADSEQFQS1) $(RADSEQFQS2) $(WGSFQS1) $(WGSFQS2)
 
 .PHONY: all clean
 
-all: $(RADSEQFQS1:.1.fq.gz=.rad) $(COMMONFQS:.fq.gz=)
+#all: $(RADSEQFQS1:.1.fq.gz=.rad) $(COMMONFQS:.fq.gz=)
+all: $(COMMONFQS:.fq.gz=)
 	@echo all [$@] [$<] [${MAKEOPTS}]
 
 %.1: %.1.fq.gz
 #	@echo "1 $(CUTADAPTCMD) -a $(ADAPTER13) -g $(ADAPTER15) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log"
 	$(CUTADAPTCMD) -a $(ADAPTER13) -g $(ADAPTER15) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log
+	touch $@
 
 %.2: %.2.fq.gz
 #	@echo "2 $(CUTADAPTCMD) -a $(ADAPTER23) -g $(ADAPTER25) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log"
 	$(CUTADAPTCMD) -a $(ADAPTER23) -g $(ADAPTER25) $(CUTADAPTARG) -o $@.cut.gz -r $@.rest.gz $< > $@.cut.log
+	touch $@
 
-%.rad: %.1.fq.gz
+#%.rad: %.1.fq.gz
 #	@echo "RAD1 $(CUTADAPTCMD) -a $(ADAPTER13) $(CUTADRADARG) -o $(@:.rad=).cut1.gz -r $(@:.rad=).rest1.gz $< > $(@:.rad=).cut1.log"
 #	@echo perl $(@:.rad=).cut1.gz $(@:.rad=).cutM.gz
 #	@echo "RAD2 $(CUTADAPTCMD) -g $(ADAPTER15) $(CUTADRADARG) -o $(@:.rad=).cut2.gz -r $(@:.rad=).rest2.gz $(@:.rad=).cutM.gz > $(@:.rad=).cut2.log"
-	$(CUTADAPTCMD) -a $(ADAPTER13) $(CUTADRADARG) -o $(@:.rad=).cut1.gz -r $(@:.rad=).rest1.gz $< > $(@:.rad=).cut1.log
+#	$(CUTADAPTCMD) -a $(ADAPTER13) $(CUTADRADARG) -o $(@:.rad=).cut1.gz -r $(@:.rad=).rest1.gz $< > $(@:.rad=).cut1.log
+
+clean:
+	-rm $(COMMONFQS:.fq.gz=) $(COMMONFQS:.fq.gz=.rest.gz) $(COMMONFQS:.fq.gz=.cut.log)
+	-rm $(COMMONFQS:.fq.gz=.cut.gz)
