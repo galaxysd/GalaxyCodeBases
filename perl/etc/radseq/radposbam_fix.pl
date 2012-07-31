0d0ab0b697e3fd24e969585c247940b5d6c22f57
Yuuki Galaxy <galaxy001@gmail.com>
2012-07-31 21:04:18 +0800

    modified:   radposbam_fix.pl

--- perl/etc/radseq/radposbam_fix.pl
+++ perl/etc/radseq/radposbam_fix.pl
@@ -5,10 +5,32 @@ Version: 1.0.0 @ 20120720
 =cut
 use strict;
 use warnings;
+use Data::Dump qw(ddx);
 
 die "Usage: $0 <ec list> <out>\n" if @ARGV<1;
 my $eclst=shift;
 my $outfs=shift;
+
+my $ReadLen = 101-5;
+my $Eseq="CTGCAG";
+my $EseqLen = length $Eseq;
+my $EcutAt=5;
+
+my $EfwdTerm5=1-$EcutAt+1;	# -3
+my $EfwdTerm3=1-$EcutAt+$ReadLen;	# 92
+my $ErevTerm3=0;	# 0
+my $ErevTerm5=$ErevTerm3-$ReadLen+1;	# -95
+my $Rfwd2Ec = -$EfwdTerm5;	# 3
+my $Rrev2Ec = -$ErevTerm3;	# 0
+
+# C|TGCAG
+my $PosLeft = -$EfwdTerm3;	# -92
+my $PosRight = -$ErevTerm5;	# 95;
+my $PosECsft = $Rfwd2Ec;	# 3;
+# - [x-$PosLeft,x+$PosECsft], Len=96
+# + [x,x+$PosRight], Len=96
+# A [x-$PosLeft,x+$PosRight], Len=95+92+1=186
+my $minDis = $PosLeft + $PosRight;
 
 open O,'>',$outfs or die "Error opening $outfs : $!\n";
 
@@ -56,9 +78,24 @@ for my $chr (@ChrOrder) {
 	}
 }
 
+my %tooNear;
 for my $chr (@ChrOrder) {
-	for my $pos (sort {$a <=> $b} keys %{$Markers{$chr}}) {
+	my @Pos = sort {$a <=> $b} keys %{$Markers{$chr}};
+	my $lastPos=$Pos[0];
+	for my $pos (@Pos) {
 		print O join("\t",$chr,$pos,@{$Markers{$chr}{$pos}->[0]}),"\n";
+		if ($pos != $lastPos and $pos-$lastPos < $minDis) {
+			$tooNear{$chr}{$pos} += $Markers{$chr}{$pos}->[0][2];
+		}
 	}
 }
 close O;
+
+ddx \%tooNear;
+
+__END__
+# radposbam_fix.pl:94: { scaffold413 => { 11969 => 1 }, scaffold574 => { 8197 => 349 } }
+scaffold413	11967	A	10425.452997535	450	18
+scaffold413	11969	-	0.801056595461641	1	1
+scaffold574	8195	+	7	7	5
+scaffold574	8197	A	10336.9968505454	349	17
