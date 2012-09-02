774d3d5aebf3dc6defcbd3c4962a849f9fbe7086
Yuuki Galaxy <galaxy001@gmail.com>
2012-09-02 17:09:50 +0800

    modified:   getsyn.pl

--- perl/etc/radseq/getsyn.pl
+++ perl/etc/radseq/getsyn.pl
@@ -14,6 +14,7 @@ die "Usage: $0 <in> <out.prefix>\n" if @ARGV<2;
 my $inf=shift;
 my $outf=shift;
 
+my $lineFit = Statistics::LineFit->new();
 my (%dat,$t,%out);
 open I,'<',$inf or die $!;
 while (<I>) {
@@ -61,14 +62,36 @@ print join("\t",'',$lenScafd,$lenChr,$t),"\n";
 		print join(",",$scoreAbs{$chr},$scoreSame{$chr},%cntStrand),"\t-----\n";
 	}
 	my ($theChr) = sort { $scoreAbs{$b} <=> $scoreAbs{$a} } keys %scoreAbs;
-	my $lineFit = Statistics::LineFit->new();
 	my $diffStrand;
 	if ($scoreSame{$theChr} > 0) {
 		$diffStrand = 0;
 	} else {
 		$diffStrand = 1;
 	}
-	print $theChr,'-' x 70,"\n";
+	my (@x,@y);
+	for my $item (@{$thePos{$theChr}}) {
+		next unless $item->[0] == $diffStrand;
+		push @y,$item->[1];	# ChrPos
+		push @x,$item->[2];	# ScaffPos
+	}
+	my $scaffOnChr;
+	my ($intercept, $slope, $r2) = (0,0,-1);
+	if (@x > 1) {
+		$lineFit->setData(\@x, \@y) or die "Invalid regression data\n";
+		($intercept, $slope) = $lineFit->coefficients();
+		defined $intercept or die "Can't fit line if x values are all equal";
+		$r2 = $lineFit->rSquared();
+		$scaffOnChr = $intercept + $slope;
+	} elsif (@x == 1) {
+		$scaffOnChr = 1 + $y[0] - $x[0];
+	} else {
+		die;
+	}
+	$scaffOnChr = int($scaffOnChr);
+	print '-' x 10, join(',',
+	$theChr,$scaffOnChr,'r2',$r2, 's',$lineFit->sigma(),'k',$slope,'b',$intercept, 'dy',(map {int($_)} $lineFit->residuals()),
+	"\n",'-' x 10,
+	'x',(map {int($_)} @x),'y',(map {int($_)} @y) ),"\n";
 }
 
 open O,'>',$outf.'.plst' or die $!;
