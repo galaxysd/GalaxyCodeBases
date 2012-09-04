a23d48d8bd21dc22fd67c96f223c7a9c79e2f771
Yuuki Galaxy <galaxy001@gmail.com>
2012-09-04 11:14:18 +0800

    modified:   getplot.pl

--- perl/etc/radseq/getplot.pl
+++ perl/etc/radseq/getplot.pl
@@ -18,6 +18,7 @@ if ($outf) {
 }
 
 my $scaffnfo = '/bak/seqdata/2012/tiger/120512_TigerRefGenome/chr.nfo';
+my $markerdat = '/share/users/huxs/work/tiger/paper/rec.npa';
 print "From [$inf] to [$inf$outf.(dat|svg)]\n";
 
 my (@ChrOrder,%ChrIDLen,%ScaffoldLen,%ChrExists);
@@ -60,13 +61,14 @@ while (<I>) {
 close I;
 print "\n",scalar keys(%ScaffoldLen)," scaffold(s) Load.\n";
 
-my (%RawDat,%Chr2Scaff);
+my (%Chr2Scaff,%Scaff2ChrID);
 open I,'<',$inf.'.plst' or die $!;
 while (<I>) {
 	next if /^#/;
 	chomp;
 	my @items = split /\t/;
 	die unless exists $ScaffoldLen{$items[3]};
+	$Scaff2ChrID{$items[3]} = $items[0];
 	my ($end,$endmin);
 	if ($items[5]) {	# k
 		$end = int( $items[1]-1 + $items[5]*($ScaffoldLen{$items[3]}->[0]) );
@@ -87,23 +89,23 @@ my %Stat;
 for my $chr (keys %Chr2Scaff) {
 	for my $i (0 .. $#{$Chr2Scaff{$chr}}) {
 		my $arr = $Chr2Scaff{$chr}->[$i];
-		++$Stat{'Total_Items'};
+		++$Stat{'Total_Scaffold'};
 		if ($arr->[5]) {	# isDiffStrand => $arr->[0] > $arr->[1], check with pre
 			if ($i) {
 				my ($pR) = sort { $b<=>$a } @{$Chr2Scaff{$chr}->[$i-1]}[0,1];
 				$pR = $Chr2Scaff{$chr}->[$i-1][0];
 				if ( $arr->[1] < $pR ) {
-print join(',',@{$Chr2Scaff{$chr}->[$i-1]}),",---\n";
-print join(',',@{$Chr2Scaff{$chr}->[$i]}),",$pR\n";
-$arr->[6] = '-'.$arr->[1];
-$arr->[7] = $arr->[4];
+#print join(',',@{$Chr2Scaff{$chr}->[$i-1]}),",---\n";
+#print join(',',@{$Chr2Scaff{$chr}->[$i]}),",$pR\n";
+#$arr->[6] = '-'.$arr->[1];
+#$arr->[7] = $arr->[4];
 					$arr->[1] = $pR + 1;
 					$arr->[4] = (1 + $arr->[1] - $arr->[0])/($ScaffoldLen{$arr->[3]}->[0]);
 					if (abs($arr->[4])<0.5) {
 						$arr->[1] = $arr->[2];
 						$arr->[4] = (1 + $arr->[1] - $arr->[0])/($ScaffoldLen{$arr->[3]}->[0]);
 						++$Stat{'Repos_Pre_Failbak'};
-$arr->[8] = 'Failbak';
+#$arr->[8] = 'Failbak';
 					} else {
 						++$Stat{'Repos_Pre'};
 					}
@@ -114,17 +116,17 @@ $arr->[8] = 'Failbak';
 				my ($pL) = sort { $a<=>$b } @{$Chr2Scaff{$chr}->[$i+1]}[0,1];
 				$pL = $Chr2Scaff{$chr}->[$i+1][0];
 				if ( $arr->[1] > $pL ) {
-print join(',',@{$Chr2Scaff{$chr}->[$i+1]}),",+++\n";
-print join(',',@{$Chr2Scaff{$chr}->[$i]}),",$pL\n";
-$arr->[6] = '+'.$arr->[1];
-$arr->[7] = $arr->[4];
+#print join(',',@{$Chr2Scaff{$chr}->[$i+1]}),",+++\n";
+#print join(',',@{$Chr2Scaff{$chr}->[$i]}),",$pL\n";
+#$arr->[6] = '+'.$arr->[1];
+#$arr->[7] = $arr->[4];
 					$arr->[1] = $pL - 1;
 					$arr->[4] = (1 + $arr->[1] - $arr->[0])/($ScaffoldLen{$arr->[3]}->[0]);
 					if (abs($arr->[4])<0.5) {
 						$arr->[1] = $arr->[2];
 						$arr->[4] = (1 + $arr->[1] - $arr->[0])/($ScaffoldLen{$arr->[3]}->[0]);
 						++$Stat{'Repos_Next_Failbak'};
-$arr->[8] = 'Failbak';
+#$arr->[8] = 'Failbak';
 					} else {
 						++$Stat{'Repos_Next'};
 					}
@@ -134,6 +136,24 @@ $arr->[8] = 'Failbak';
 	}
 }
 ddx \%Chr2Scaff;
+
+my %MarkerDat;
+open I,'<',$markerdat or die $!;
+while (<I>) {
+	next if /^#/;
+	chomp;
+	my @items = split /\t/;
+	die unless exists $ScaffoldLen{$items[1]};
+	if (exists $Scaff2ChrID{$items[1]}) {
+		push @{$MarkerDat{$items[1]}},[ $items[2],$items[9],int(0.5-100*log($items[9])/log(10))/100 ];	# pos,p,lg(p)
+		++$Stat{'Marker_Used'};
+	} else {
+		++$Stat{'Marker_Skipped'};
+	}
+}
+close I;
+ddx \%MarkerDat;
+
 ddx \%Stat;
 
 __END__
