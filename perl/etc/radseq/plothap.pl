099ae939c04c088e4ad7196933130b41fd54bba1
Yuuki Galaxy <galaxy001@gmail.com>
2012-09-16 20:23:46 +0800

    modified:   plothap.pl

--- perl/etc/radseq/plothap.pl
+++ perl/etc/radseq/plothap.pl
@@ -19,11 +19,43 @@ if ($outf) {
 	$outf = '';
 }
 
-#my $scaffnfo = '/bak/seqdata/2012/tiger/120512_TigerRefGenome/chr.nfo';
+my $scaffnfo = '/bak/seqdata/2012/tiger/120512_TigerRefGenome/chr.nfo';
 my $scaf2locuslst = '/share/users/huxs/work/tiger/20120910/tighap.lst';
 print "From [$markf][$inf] to [$inf$outf.(dat|svg)]\n";
 
 my %Stat;
+
+my @Scaffolds = ([qw(scaffold75 scaffold1458)], ['scaffold188']);
+my $t=0;
+my %scaffolds = map {map {$_ => ++$t} @$_} @Scaffolds;
+my %scaff2chr = map { $scaffolds{$_} => $_ } keys %scaffolds;
+ddx \@Scaffolds;
+ddx \%scaffolds;
+ddx \%scaff2chr;
+# plothap.pl:32: [["scaffold75", "scaffold1458"], ["scaffold188"]]
+# plothap.pl:33: { scaffold1458 => 2, scaffold188 => 3, scaffold75 => 1 }
+# plothap.pl:34: { 1 => "scaffold75", 2 => "scaffold1458", 3 => "scaffold188" }
+
+my ($maxLen,@LineLen,%ScaffoldLen)=(0);
+open I,'<',$scaffnfo or die $!;
+while (<I>) {
+	next if /^#/;
+	chomp;
+	my @items = split /\t/;
+	next unless exists $scaffolds{$items[0]};
+	$ScaffoldLen{$items[0]} = $items[1];
+}
+close I;
+print "\n",scalar keys(%ScaffoldLen)," scaffold(s) Load.\nLength:\n";
+for (@Scaffolds) {
+	my $len = 0;
+	$len += $ScaffoldLen{$_} for @$_;
+	push @LineLen,$len;
+	print "[",join(', ',@$_),"]: $len\n";
+	$maxLen = $len if $maxLen < $len;
+}
+print "MaxLen: $maxLen\n";
+#ddx \@LineLen;
 
 my %LD;
 open I,'<',$inf or die $!;
@@ -40,9 +72,33 @@ while (<I>) {
 #ddx [$x,$y];
 	$LD{$$x[0]}{$$x[1]}{$$y[0]}{$$y[1]} = [$dist,$rsq,$dp,$p,$N];
 }
-ddx \%LD;
+#ddx \%LD;
+
+my $Xrange = 500;
+my $Yrange = 320;
+my $YmaxVal = 5;
+my $ArrowLen = 20;	# 16+4
+my $axisTick = 4;
+my $OutBorder = 24;
+my $InBorder = 40;
+my $Xtotal = $Xrange + $ArrowLen + 2*$OutBorder;
+my $Yitem = $Yrange + $ArrowLen + $InBorder;
+my $FontSize = int($Xrange/40);
+my $FontFamily = 'Arial';
+
+my $perUnit = int($maxLen/10);	# 279.330936 M /10 = 27.933093 M
+my $numlevel = int(log($perUnit)/log(10));	# 7
+my $numSuflevel = int($numlevel/3);	# 2
+my $numSuf=( '', qw( K M G T P E Z Y ) )[$numSuflevel];	# M <---
+$numSuflevel = 10 ** (3*$numSuflevel);	# 1,000,000 <---
+my $roundTo = 5 * (10 ** ($numlevel-1));	# 5e6
+my $unit = $perUnit + (-$perUnit % $roundTo);	# 30 M
+my $countMax = int($maxLen/$unit) + (($maxLen%$unit)?1:0);
+print join(",",$maxLen/$numSuflevel,$perUnit/$numSuflevel,$numlevel,$numSuf,$numSuflevel,$roundTo/$numSuflevel,$unit/$numSuflevel,$countMax),"\n";
+my $BasepPx = 10*$unit/$Xrange;
+
 
 __END__
 
-
+./plothap.pl ../rec.npa 17all75.txt
 
