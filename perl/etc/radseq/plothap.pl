77391d20f6d85b152a687280a2973b6079b13e69
Yuuki Galaxy <galaxy001@gmail.com>
2012-09-18 21:24:10 +0800

    modified:   plothap.pl

--- perl/etc/radseq/plothap.pl
+++ perl/etc/radseq/plothap.pl
@@ -36,7 +36,7 @@ ddx \%scaff2chr;
 # plothap.pl:33: { scaffold1458 => 2, scaffold188 => 3, scaffold75 => 1 }
 # plothap.pl:34: { 1 => "scaffold75", 2 => "scaffold1458", 3 => "scaffold188" }
 
-my ($maxLen,@LineLen,%ScaffoldLen)=(0);
+my ($maxLen,@LineLen,%ScaffoldLen,%ScaffoldsOffect)=(0);
 open I,'<',$scaffnfo or die $!;
 while (<I>) {
 	next if /^#/;
@@ -49,13 +49,20 @@ close I;
 print "\n",scalar keys(%ScaffoldLen)," scaffold(s) Load.\nLength:\n";
 for (@Scaffolds) {
 	my $len = 0;
-	$len += $ScaffoldLen{$_} for @$_;
+	for (@$_) {
+		$ScaffoldsOffect{$_} = $len;
+		$len += $ScaffoldLen{$_};
+	}
+	#push @t,$len;	# not necessary
 	push @LineLen,$len;
 	print "[",join(', ',@$_),"]: $len\n";
 	$maxLen = $len if $maxLen < $len;
 }
 print "MaxLen: $maxLen\n";
 #ddx \@LineLen;
+ddx \%ScaffoldsOffect;
+# plothap.pl:65: [[0, 5658748, 12958355], [0, 19124984]]
+# plothap.pl:63: { scaffold1458 => 5658748, scaffold188 => 0, scaffold75 => 0 }
 
 my %LD;
 open I,'<',$inf or die $!;
@@ -75,14 +82,14 @@ while (<I>) {
 #ddx \%LD;
 
 my $Xrange = 500;
-my $Yrange = int($Xrange*sqrt(2)) + 120;
-my $YmaxVal = 5;
+my $Yrange = $Xrange/2 + 50;
+my $YmaxVal = 2;
 my $ArrowLen = 20;	# 16+4
 my $axisTick = 4;
 my $OutBorder = 24;
 my $InBorder = 40;
-my $Xtotal = $Xrange + $ArrowLen + 2*$OutBorder;
-my $Yitem = $Yrange + $ArrowLen + $InBorder;
+my $Xtotal = $Xrange + 2*$OutBorder;
+my $Yitem = $Yrange + $InBorder;
 my $FontSize = int($Xrange/40);
 my $FontFamily = 'Arial';
 
@@ -96,6 +103,8 @@ my $unit = $perUnit + (-$perUnit % $roundTo);	# 30 M
 my $countMax = int($maxLen/$unit) + (($maxLen%$unit)?1:0);
 my $BasepPx = 10*$unit/$Xrange;
 #print join(",",$BasepPx,$maxLen/$numSuflevel,$perUnit/$numSuflevel,$numlevel,$numSuf,$numSuflevel,$roundTo/$numSuflevel,$unit/$numSuflevel,$countMax),"\n";
+my $ChrCount = @Scaffolds;
+my $Ytotal = $Yitem*$ChrCount - $InBorder + 2*$OutBorder;
 
 sub getVal($) {
 	my %dat = %{$_[0]};
@@ -113,21 +122,86 @@ sub getVal($) {
 
 my %PlotLD;
 for (@Scaffolds) {
+	my %locushere = map { $scaffolds{$_} => 1 } @$_;
 	for my $scaff (@$_) {
 		my $locus1 = $scaffolds{$scaff};
 		for my $pos1 (sort {$a<=>$b} keys %{$LD{$locus1}}) {
 			for my $locus2 (keys %{$LD{$locus1}{$pos1}}) {
+				next unless exists $locushere{$locus2};
 				for my $pos2 (sort {$a<=>$b} keys %{$LD{$locus1}{$pos1}{$locus2}}) {
-					#print join(',',$scaff,$locus1,$pos1,$locus2,$pos2),"\n";
-					my ($p1,$p2) = map { int(10*$_/$BasepPx)/10 } ($pos1,$pos2);
-					my $val = $LD{$locus1}{$pos1}{$locus2}{$pos2}->[1];
-					push @{$PlotLD{$locus1}{$p1}{$locus2}{$p2}},$val;
+					my @Offects = map {$ScaffoldsOffect{$scaff2chr{$_}} } ($locus1,$locus2);
+#print join(',',$scaff,$locus1,$pos1,$locus2,$pos2,@Offects),"\n";
+					my $bp1 = $pos1 + $Offects[0];
+					my $bp2 = $pos2 + $Offects[1];
+					my ($p1,$p2) = map { int(0.5*$_/$BasepPx)*2 } ($bp1,$bp2);
+#print join(',',$scaff,$bp1,$bp2,$p1,$p2),"\n";
+					my $val = $LD{$locus1}{$pos1}{$locus2}{$pos2}->[1];	# r^2
+					next if $val eq "NaN";
+					push @{$PlotLD{$locus1}{$p1}{$p2}},$val;
 				}
 			}
 		}
 	}
 }
-ddx \%PlotLD;
+#ddx \%PlotLD;
+
+open O,'>',$inf.$outf.'.svg' or die $!;
+print O <<HEAD;
+<?xml version="1.0"?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.2" baseProfile="tiny"
+ width="${Xtotal}pt" height="${Ytotal}pt" viewBox="0 0 $Xtotal $Ytotal">
+<title>Plot $inf</title>
+  <rect x="0" y="0" width="$Xtotal" height="$Ytotal" fill="none" stroke="red" stroke-width="2" />
+  <defs>
+HEAD
+
+$t=0;
+for (@LineLen) {
+	my $len = $_ / $BasepPx;
+	print O <<CLIP;
+    <clipPath id="curveClip$t">
+      <path id="curve$t" d="M$len 0 H0 V$len z"/>
+    </clipPath>
+CLIP
+	++$t;
+}
+print O "  </defs>\n";
+$t=0;
+my $theY=$OutBorder;
+for (@LineLen) {
+	my $len = $_ / $BasepPx;
+	my $Ylen = $len/2 + 50;
+	my $halflen = $len / 2;
+	print O <<DEF2;
+  <g transform="translate($OutBorder,$theY)" stroke-width="2" stroke="black" font-size="$FontSize" font-family="$FontFamily">
+  <rect x="0" y="0" width="$Xrange" height="$Ylen" fill="none" stroke="navy" stroke-width="2" />
+    <g transform="matrix(0.5,0.5,-0.5,0.5,$halflen,0)" clip-path="url(#curveClip$t)" stroke-width="0">
+      <rect x="0" y="0" width="$Xrange" height="$len" fill="none" stroke="green" stroke-width="1" />
+DEF2
+	;
+	for my $scaff (@{$Scaffolds[$t]}) {
+		my $locus1 = $scaffolds{$scaff};
+		for my $pos1 (sort {$a<=>$b} keys %{$LD{$locus1}}) {
+			for my $locus2 (keys %{$LD{$locus1}{$pos1}}) {
+				for my $pos2 (sort {$a<=>$b} keys %{$LD{$locus1}{$pos1}{$locus2}}) {
+				}
+			}
+		}
+	}
+
+	print O '    </g>
+      <g transform="translate(0,',5 + $len/2,')">
+      <line x1="0" y1="0" x2="',$len,'" y2="0" stroke-width="3"/>
+';
+	;
+	print O "      </g>\n  </g>\n";
+	++$t;
+	$theY += $Ylen + $InBorder;
+}
+print O "</svg>\n";
+close O;
+
+ddx \%Stat;
 
 __END__
 
