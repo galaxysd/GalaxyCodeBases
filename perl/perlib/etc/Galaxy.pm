f3207c1ea8743dac9f430caf167925ac8e49ac2b
Yuuki Galaxy <galaxy001@gmail.com>
2012-09-18 22:43:55 +0800

    add colormap:   /perlib/etc/Galaxy.pm

--- perl/perlib/etc/Galaxy.pm
+++ perl/perlib/etc/Galaxy.pm
@@ -2,8 +2,8 @@ package Galaxy;
 use strict;
 require Exporter;
 our @ISA   =qw(Exporter);
-our @EXPORT    =qw(commify);
-our @EXPORT_OK   =qw();
+our @EXPORT    =qw(commify colormap);
+our @EXPORT_OK   =qw(colormap);
 our $VERSION   = v1.0.0;
 
 sub commify {	# http://www.perlmonks.org/?node_id=653
@@ -13,3 +13,34 @@ sub commify {	# http://www.perlmonks.org/?node_id=653
 	$input = reverse $input;
 	return $input;
 }
+
+sub colormap {	# http://cresspahl.blogspot.com/2012/03/expanded-control-of-octaves-colormap.html
+	my ($value) = @_;
+	my @MR = ([0,0],[0.02,0.3],[0.3,1],[1,1]);
+	my @MG = ([0,0],[0.3,0],[0.7,1],[1,1]);
+	my @MB = ([0,0],[0.7,0],[1,1]);
+	my $r = int( 0.5 + 255*_getColorMap($value,\@MR) );
+	my $g = int( 0.5 + 255*_getColorMap($value,\@MG) );
+	my $b = int( 0.5 + 255*_getColorMap($value,\@MB) );
+	my @color = map { sprintf "%02x",$_; } ($r,$g,$b);
+	return '#'.join('',@color);
+}
+sub _getColorMap {
+	my ($v,$matrix) = @_;
+	my ($oldvalue,$oldout);
+	for (@$matrix) {
+		my ($value,$out) = @$_;
+		if ($value<=$v) {
+			$oldvalue = $value;
+			$oldout = $out;
+			return $out if $v == $value;
+		} else {
+			my $k = ($out - $oldout)/($value - $oldvalue);
+			my $b = $out - $k * $value;
+			return $k*$v + $b;
+		}
+	}
+}
+
+
+1;
