b3481cb1fc4604e5cbb8ed08c42ea8e81b3bd2a8
Yuuki Galaxy <galaxy001@gmail.com>
2013-10-18 21:41:39 +0800

    new file:   anocrep.pl

---
+++ perl/etc/crep/anocrep.pl
@@ -0,0 +1,94 @@
+#!/bin/env perl
+use strict;
+use warnings;
+use IO::Unread qw(unread);
+use Data::Dump qw(ddx);
+
+die "Usage: $0 <anno_file> <input>\n" if @ARGV < 2;
+my ($anno,$inf)=@ARGV;
+
+my (%ANNO,%ANNOcnt,%LOCdesc,%LOC2AFF);
+
+open I,'<',$anno or die;
+while (<I>) {
+	chomp;
+	my @dat = split /\t/;
+	next unless exists $dat[1];
+	next if $dat[1] eq 'NONE|NONE|NONE';
+	my $id = shift @dat;
+	my $sum=0;
+	my @tids;
+	for (@dat) {
+		my ($tid,$ratio,$desc) = split /\|/;
+		$ratio =~ s#/11$## or die "[$ratio]";
+		$sum += $ratio;
+		if (exists $LOCdesc{$tid} and $LOCdesc{$tid} ne $desc) {
+			die "[x]$_\n$LOCdesc{$tid}\n";
+		}
+		$LOCdesc{$tid} = $desc;
+		push @tids,[$tid,$ratio];
+	}
+	$ANNO{$id} = \@tids;
+	$ANNOcnt{$id} = $sum;
+}
+close I;
+#ddx \%ANNO;
+#   "Os.10031.1.S1_at"          => [["LOC_Os05g35140", 11], ["LOC_Os07g12230", 1]],
+
+for my $aid (keys %ANNO) {
+	my @items = @{$ANNO{$aid}};
+	for (@items) {
+		$LOC2AFF{$_->[0]} = [[],0] unless exists $LOC2AFF{$_->[0]};
+=pod
+		my @dat = @{ $LOC2AFF{$_->[0]} };
+		push @{ $dat[0] },$_->[0];
+		$dat[1] += $_->[1];
+		$LOC2AFF{$_->[0]} = \@dat;
+=cut
+		push @{ $LOC2AFF{$_->[0]}->[0] },[$aid,$_->[1]];
+		$LOC2AFF{$_->[0]}->[1] += $_->[1];
+	}
+}
+#ddx \%LOC2AFF;
+#   LOC_Os05g35140 => [[["Os.10031.1.S1_at", 11]], 11],
+#   LOC_Os07g12230 => [
+#                       [
+#                         ["OsAffx.22438.1.S1_x_at", 11],
+#                         ["Os.10031.1.S1_at", 1],
+#                         ["OsAffx.22438.1.S1_at", 11],
+#                         ["OsAffx.8676.1.S1_at", 4],
+#                       ],
+#                       27,
+#                     ],
+
+for my $tid (keys %LOC2AFF) {
+	my @dat = @{$LOC2AFF{$tid}->[0]};
+	my $sum = $LOC2AFF{$tid}->[1];
+	for (@dat) {
+		$_->[2] = $_->[1] / $sum;
+	}
+	$LOC2AFF{$tid} = \@dat;
+}
+#ddx \%LOC2AFF;
+#   LOC_Os05g35140 => [["Os.10031.1.S1_at", 11, 1]],
+#   LOC_Os07g12230 => [
+#                       ["OsAffx.22438.1.S1_x_at", 11, 0.407407407407407],
+#                       ["Os.10031.1.S1_at", 1, 0.037037037037037],
+#                       ["OsAffx.22438.1.S1_at", 11, 0.407407407407407],
+#                       ["OsAffx.8676.1.S1_at", 4, 0.148148148148148],
+#                     ],
+
+my %outLOC;
+open I,'<',$inf or die;
+while (<I>) {
+	chomp;
+	my @dat = split /\t/;
+	my $tid = shift @dat;
+	if (exists $ANNO{$tid}) {
+		#$outLOC{}
+	}
+}
+close I;
+
+__END__
+perl anocrep.pl crep_anno_merged.tsv crep_all_tsv_new.txt.up2
