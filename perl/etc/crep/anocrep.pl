7b6ddabab8ca34389f1653a7c5654c63915ad95b
Yuuki Galaxy <galaxy001@gmail.com>
2013-10-19 22:02:51 +0800

    done:   anocrep.pl

--- perl/etc/crep/anocrep.pl
+++ perl/etc/crep/anocrep.pl
@@ -81,26 +81,67 @@ for my $tid (keys %LOC2AFF) {
 #                       "OsAffx.22438.1.S1_x_at" => [11, 0.407407407407407],
 #                       "OsAffx.8676.1.S1_at"    => [4, 0.148148148148148],
 #                     },
+
 #ddx \%AFF2LOC;
 #   "Os.10031.1.S1_at"          => [
 #                                    ["LOC_Os05g35140", 11, 1],
 #                                    ["LOC_Os07g12230", 1, 0.037037037037037],
 #                                  ],
 
-die;
 my %outLOC;
 open I,'<',$inf or die;
 while (<I>) {
 	chomp;
 	my @dat = split /\t/;
-	my $aid = shift @dat;
+	my $aid = $dat[0];
 	if (exists $AFF2LOC{$aid}) {
 		for my $i ( @{ $AFF2LOC{$aid} } ) {
-			$outLOC{ $i->[0] } = [] unless exists $outLOC{ $i->[0] };
+			$outLOC{ $i->[0] } = [0,[]] unless exists $outLOC{ $i->[0] };
+			push @{ $outLOC{$i->[0]}->[1] },[@dat];
+			#push @{ $outLOC{$i->[0]}->[1] },\@dat;
+			$outLOC{$i->[0]}->[0] += $dat[1] * $i->[2];
 		}
 	}
 }
 close I;
+#ddx \%outLOC;
+#   LOC_Os01g02300 => [
+#                       2.99555110455193,
+#                       [
+#                         [
+#                           "Os.27046.1.S1_at",
+#                           2.48051948051948,
+#                           101.87,
+#                           41.07,
+#                           "66|189.6|50",
+#                           "49.5|59.1|14.6",
+#                         ],
+#                         [
+#                           "Os.27046.2.S1_x_at",
+#                           3.46376167185416,
+#                           259.67,
+#                           74.97,
+#                           "88.3|399.4|291.3",
+#                           "13.9|167|44",
+#                         ],
+#                       ],
+#                     ],
+#   LOC_Os01g02300 => {
+#                       "Os.27046.1.S1_at"   => [10, 0.476190476190476],
+#                       "Os.27046.2.S1_x_at" => [11, 0.523809523809524],
+#                     },
+
+open O,'>',$inf.'.out' or die;
+for my $tid (sort { $outLOC{$b}->[0] <=> $outLOC{$a}->[0] } keys %outLOC) {
+	my @items = @{ $outLOC{$tid} };
+	my @affprobes;
+	for ( @{ $items[1] } ) {
+		$$_[1] = int(0.5+1000*$$_[1])/1000;
+		push @affprobes,join('~',@{$LOC2AFF{$tid}{$$_[0]}},@$_);
+	}
+	print O join("\t",$tid,(scalar @affprobes),$items[0],$LOCdesc{$tid},join("\t",@affprobes) ),"\n";
+}
+close O;
 
 __END__
 perl anocrep.pl crep_anno_merged.tsv crep_all_tsv_new.txt.up2
