f61bb98a19f54d5496592cd5118135e616249931
Yuuki Galaxy <galaxy001@gmail.com>
2014-04-13 21:29:55 +0800

    modified:   merge_ricexpro_anno.pl

--- perl/etc/crep/merge_ricexpro_anno.pl
+++ perl/etc/crep/merge_ricexpro_anno.pl
@@ -57,7 +57,7 @@ RXP_1009 => 'BRs', #Shoot gene expression profile in response to brassinosteroid
 RXP_1010 => 'CK' #Shoot gene expression profile in response to cytokinin
 );
 
-my (@FeatureOrder, @ExpOrder);
+my (@FeatureOrder, @ExpOrder, @FeatNameOrder, @reps, %rep, @FinalOrder);
 # FeatureNum    RXP_1006        RXP_1009        RXP_1010
 ## 0 hr (Cy3)|0 hr (Cy5)|1 hr (Cy3)|1 hr (Cy5)|3 hr (Cy3)|3 hr (Cy5)|6 hr (Cy3)|6 hr (Cy5)|12 hr (Cy3)|12 hr (Cy5)
 while (<A>) {
@@ -67,7 +67,11 @@ while (<A>) {
 			chomp;
 			@FeatureOrder = split /\t/;
 			shift @FeatureOrder;
-			print "$_, $RXP2Name{$_}\n" for @FeatureOrder;
+			for (@FeatureOrder) {
+				my $tmp = $RXP2Name{$_};
+				push @FeatNameOrder,$tmp;
+				print "$_, $tmp\n";
+			}
 		} elsif ( /^## (.+)$/ ) {
 			$_ = $1;
 			chomp;
@@ -82,19 +86,137 @@ while (<A>) {
 		next;
 	}
 	my ( $acc,@dat ) = split /\t/;
-	$cyDat{$acc} = \@dat;
+	my %DAT;
+	for my $FNO ( @FeatNameOrder ) {
+		my $tmp = shift @dat;
+		my @rep = split /\,/,$tmp;
+		for my $repline ( @rep ) {
+			my @repdat = split /\|/,$repline;
+			my $repid = shift @repdat;
+			next if $repid eq 'mean';
+			++$rep{$repid};
+			$DAT{$FNO}{$repid} = \@repdat;
+		}
+	}
+	$cyDat{$acc} = \%DAT;
 }
 close A;
+@reps = sort keys %rep;
+for my $FNO ( @FeatNameOrder ) {
+	for my $RO ( @reps ) {
+		for my $EO ( @ExpOrder ) {
+			my $t = $RO;
+			$t =~ s/^rep/r/;
+			push @FinalOrder,"$FNO$EO$t";
+		}
+	}
+}
+for my $acc ( keys %cyDat ) {
+	my %cyhash = %{$cyDat{$acc}};
+	my @item;
+	for my $FNO ( @FeatNameOrder ) {
+		for my $RO ( @reps ) {
+			for my $EO ( 0 .. $#ExpOrder ) {
+				push @item,$cyhash{$FNO}{$RO}->[$EO];
+			}
+		}
+	}
+	$cyDat{$acc} = \@item;
+}
 #ddx \%cyDat;
+print "@FinalOrder\n";
+print join(',',@{$cyDat{'45209'}}),"\n";
 #   45209 => [
 #              "rep1|8.0073|3.6185|8.9315|5.0357|4.6752|4.7962|83.208|12.445|44.395|6.3024,rep2|3.1927|2.6697|3.3677|4.4205|3.3884|3.1185|7.425|6.9132|5.2888|5.7744,mean|5.6|3.1441|6.1496|4.7281|4.0318|3.9574|45.317|9.6791|24.842|6.0384",
 #              "rep1|8.0073|3.6185|4.2184|3.7634|3.4171|6.2807|3.9199|4.3256|5.1421|3.9737,rep2|3.1927|2.6697|4.048|3.8231|4.0791|2.9637|3.569|4.0095|2.597|2.6025,mean|5.6|3.1441|4.1332|3.7933|3.7481|4.6222|3.7445|4.1676|3.8695|3.2881",
 #              "rep1|8.0073|3.6185|3.2538|4.5685|11.197|3.8527|4.4721|3.4333|4.8344|4.8462,rep2|3.1927|2.6697|2.7079|3.9205|7.4918|3.104|3.1729|2.6352|6.9551|4.1439,mean|5.6|3.1441|2.9809|4.2445|9.3444|3.4784|3.8225|3.0343|5.8947|4.495\n",
 #            ],
+#   45209 => {
+#              ABA => {
+#                       rep1 => [
+#                                 8.0073,
+#                                 3.6185,
+#                                 8.9315,
+#                                 5.0357,
+#                                 4.6752,
+#                                 4.7962,
+#                                 83.208,
+#                                 12.445,
+#                                 44.395,
+#                                 6.3024,
+#                               ],
+#                       rep2 => [
+#                                 3.1927,
+#                                 2.6697,
+#                                 3.3677,
+#                                 4.4205,
+#                                 3.3884,
+#                                 3.1185,
+#                                 7.425,
+#                                 6.9132,
+#                                 5.2888,
+#                                 5.7744,
+#                               ],
+#                     },
+#              BRs => {
+#                       rep1 => [
+#                                 8.0073,
+#                                 3.6185,
+#                                 4.2184,
+#                                 3.7634,
+#                                 3.4171,
+#                                 6.2807,
+#                                 3.9199,
+#                                 4.3256,
+#                                 5.1421,
+#                                 3.9737,
+#                               ],
+#                       rep2 => [
+#                                 3.1927,
+#                                 2.6697,
+#                                 4.048,
+#                                 3.8231,
+#                                 4.0791,
+#                                 2.9637,
+#                                 3.569,
+#                                 4.0095,
+#                                 2.597,
+#                                 2.6025,
+#                               ],
+#                     },
+#              CK  => {
+#                       rep1 => [
+#                                 8.0073,
+#                                 3.6185,
+#                                 3.2538,
+#                                 4.5685,
+#                                 11.197,
+#                                 3.8527,
+#                                 4.4721,
+#                                 3.4333,
+#                                 4.8344,
+#                                 4.8462,
+#                               ],
+#                       rep2 => [
+#                                 3.1927,
+#                                 2.6697,
+#                                 2.7079,
+#                                 3.9205,
+#                                 7.4918,
+#                                 3.104,
+#                                 3.1729,
+#                                 2.6352,
+#                                 6.9551,
+#                                 4.1439,
+#                               ],
+#                     },
+#            },
+# 8.0073,3.6185,8.9315,5.0357,4.6752,4.7962,83.208,12.445,44.395,6.3024,3.1927,2.6697,3.3677,4.4205,3.3884,3.1185,7.425,6.9132,5.2888,5.7744,8.0073,3.6185,4.2184,3.7634,3.4171,6.2807,3.9199,4.3256,5.1421,3.9737,3.1927,2.6697,4.048,3.8231,4.0791,2.9637,3.569,4.0095,2.597,2.6025,8.0073,3.6185,3.2538,4.5685,11.197,3.8527,4.4721,3.4333,4.8344,4.8462,3.1927,2.6697,2.7079,3.9205,7.4918,3.104,3.1729,2.6352,6.9551,4.1439
 
+print O join("\t",'YORF','NAME',@FinalOrder),"\n";
 for my $loc (sort keys %Loc2Acc) {
 	for my $acc ( sort keys %{$Loc2Acc{$loc}} ) {
-		print O join("\t",$loc,$acc,@{$cyDat{$acc}});
+		print O join("\t",$loc,$acc,@{$cyDat{$acc}}),"\n";
 	}
 }
 close O;
