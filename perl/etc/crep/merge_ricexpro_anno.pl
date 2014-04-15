2175c2a742d24b7f2f8eb82bd41b99d7af8af15f
Yuuki Galaxy <galaxy001@gmail.com>
2014-04-15 14:47:34 +0800

    modified:   merge_ricexpro_anno.pl

--- perl/etc/crep/merge_ricexpro_anno.pl
+++ perl/etc/crep/merge_ricexpro_anno.pl
@@ -6,6 +6,7 @@ use Data::Dump;
 open I,'<','resLOC.anno' or die $!;
 open A,'<','resLOC.cydat' or die $!;
 open O,'>','resLOC.ricexpro' or die $!;
+open ON,'>','resLOC.rpratio' or die $!;
 
 my (%Acc2Loc,%Loc2Acc,%Acc2Accession);
 while (<I>) {
@@ -49,15 +50,15 @@ for my $loc (@LocNums) {
 
 #ddx \%Acc2Accession;	# undef exists from file !
 
-my %cyDat;
+my (%cyDat,%cyRDat);
 
 my %RXP2Name = (
 RXP_1006 => 'ABA', #Shoot gene expression profile in response to abscisic acid
 RXP_1009 => 'BRs', #Shoot gene expression profile in response to brassinosteroid
 RXP_1010 => 'CK' #Shoot gene expression profile in response to cytokinin
-);
+);	# Cy3 (mock treatment) and Cy5 (hormone treatment) => Cy5/Cy3, http://ricexpro.dna.affrc.go.jp/RXP_1006/index.php
 
-my (@FeatureOrder, @ExpOrder, @FeatNameOrder, @reps, %rep, @FinalOrder);
+my (@FeatureOrder, @ExpOrder, @FeatNameOrder, @reps, %rep, @FinalOrder, @RatioExpOrder, @RatioFinalOrder);
 # FeatureNum    RXP_1006        RXP_1009        RXP_1010
 ## 0 hr (Cy3)|0 hr (Cy5)|1 hr (Cy3)|1 hr (Cy5)|3 hr (Cy3)|3 hr (Cy5)|6 hr (Cy3)|6 hr (Cy5)|12 hr (Cy3)|12 hr (Cy5)
 while (<A>) {
@@ -79,26 +80,31 @@ while (<A>) {
 			for ( @Exps ) {
 				/^(\d+) hr \(Cy([35])\)$/ or die;
 				push @ExpOrder,"${1}h_Cy$2";
+				push @RatioExpOrder,"${1}h" if $2 eq '3';
 				#print "$_ => ${1}h_Cy$2\n";
 			}
-			print "@ExpOrder\n";
+			print "@ExpOrder -> @RatioExpOrder\n";
 		}
 		next;
 	}
 	my ( $acc,@dat ) = split /\t/;
-	my %DAT;
+	my (%DAT,%RDAT);
 	for my $FNO ( @FeatNameOrder ) {
 		my $tmp = shift @dat;
 		my @rep = split /\,/,$tmp;
 		for my $repline ( @rep ) {
 			my @repdat = split /\|/,$repline;
 			my $repid = shift @repdat;
-			next if $repid eq 'mean';
-			++$rep{$repid};
-			$DAT{$FNO}{$repid} = \@repdat;
+			if ($repid eq 'mean') {
+				$RDAT{$FNO} = \@repdat;
+			} else {
+				++$rep{$repid};
+				$DAT{$FNO}{$repid} = \@repdat;
+			}
 		}
 	}
 	$cyDat{$acc} = \%DAT;
+	$cyRDat{$acc} = \%RDAT;
 }
 close A;
 @reps = sort keys %rep;
@@ -110,22 +116,33 @@ for my $FNO ( @FeatNameOrder ) {
 			push @FinalOrder,"$FNO$EO$t";
 		}
 	}
+	for my $REO ( @RatioExpOrder ) {
+		push @RatioFinalOrder,"$FNO$REO";
+	}
 }
 for my $acc ( keys %cyDat ) {
 	my %cyhash = %{$cyDat{$acc}};
-	my @item;
+	my %cyrhash = %{$cyRDat{$acc}};
+	my (@item,@ritem);
 	for my $FNO ( @FeatNameOrder ) {
 		for my $RO ( @reps ) {
 			for my $EO ( 0 .. $#ExpOrder ) {
 				push @item,$cyhash{$FNO}{$RO}->[$EO];
 			}
 		}
+		for my $EO ( 0 .. $#RatioExpOrder ) {
+			push @ritem,$cyrhash{$FNO}->[1+2*$EO] / $cyrhash{$FNO}->[2*$EO];
+			#push @ritem,log($cyrhash{$FNO}->[1+2*$EO] / $cyrhash{$FNO}->[2*$EO])/log(2);
+		}
 	}
 	$cyDat{$acc} = \@item;
+	$cyRDat{$acc} = \@ritem;
 }
 #ddx \%cyDat;
-print "@FinalOrder\n";
+print "\n@FinalOrder\n";
 print join(',',@{$cyDat{'45209'}}),"\n";
+print "\n@RatioFinalOrder\n";
+print join(',',@{$cyRDat{'45209'}}),"\n";
 #   45209 => [
 #              "rep1|8.0073|3.6185|8.9315|5.0357|4.6752|4.7962|83.208|12.445|44.395|6.3024,rep2|3.1927|2.6697|3.3677|4.4205|3.3884|3.1185|7.425|6.9132|5.2888|5.7744,mean|5.6|3.1441|6.1496|4.7281|4.0318|3.9574|45.317|9.6791|24.842|6.0384",
 #              "rep1|8.0073|3.6185|4.2184|3.7634|3.4171|6.2807|3.9199|4.3256|5.1421|3.9737,rep2|3.1927|2.6697|4.048|3.8231|4.0791|2.9637|3.569|4.0095|2.597|2.6025,mean|5.6|3.1441|4.1332|3.7933|3.7481|4.6222|3.7445|4.1676|3.8695|3.2881",
@@ -220,5 +237,11 @@ for my $loc (sort keys %Loc2Acc) {
 	}
 }
 close O;
-
+print ON join("\t",'YORF','NAME',@RatioFinalOrder),"\n";
+for my $loc (sort keys %Loc2Acc) {
+	for my $acc ( sort keys %{$Loc2Acc{$loc}} ) {
+		print ON join("\t",$loc,$acc,@{$cyRDat{$acc}}),"\n";
+	}
+}
+close ON;
 __END__
