b813c3657c11249f2d7aeba8154b19e023a65bc0
Yuuki Galaxy <galaxy001@gmail.com>
2014-09-02 23:41:53 +0800

    modified:   xtubam/depth.sh

--- perl/etc/justonce/sperm/xtubam/depth.sh
+++ perl/etc/justonce/sperm/xtubam/depth.sh
@@ -4,4 +4,5 @@ samtools depth Blood-MDA.sort.bam Blood-MAL.sort.bam Sperm23-MDA.sort.bam Sperm2
 # zcat depths.gz|perl -lane 'BEDIN {@SUM;$CNT} next if $F[0]=~/[XYM]/i; $chr=shift @F; $pos=shift @F; for $i (0 .. $#F) { $SUM[$i]+=$F[$i]; ++$CNT } END { $_ = int(1000*$_/$CNT)/1000 for (@SUM);print "@SUM"; }'
 # zcat depths.gz|perl -lane 'BEDIN {@SUM;$CNT} next if $F[0]=~/[XYM]/i; $chr=shift @F; $pos=shift @F; for $i (0 .. $#F) { $SUM[$i]+=$F[$i]; ++$CNT } print "@SUM"; END { $_ = int(1000*$_/$CNT)/1000 for (@SUM);print "@SUM"; }'
 # zcat depths.gz|perl -lane 'BEDIN {@SUM;$CNT} next if $F[0]=~/[XYM]/i; $chr=shift @F; $pos=shift @F; for $i (0 .. $#F) { $SUM[$i]+=$F[$i]; ++$CNT } print "$chr:$pos\t@SUM" unless $CNT%1000000 ; END { $_ = int(1000*$_/$CNT)/1000 for (@SUM);print "@SUM"; }'
+# zcat depths.gz|perl -lane 'BEDIN {@SUM;$CNT} next if $F[0]=~/[XYM]/i; $chr=shift @F; $pos=shift @F; for $i (0 .. $#F) { $SUM[$i]+=$F[$i]; ++$CNT } print "$chr:$pos\t@SUM\t",$SUM[1]/(0.00000001+$SUM[0]) unless $CNT%1000000 ; END { $_ = int(1000*$_/$CNT)/1000 for (@SUM);print "@SUM"; }'
 
