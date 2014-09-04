88a58e6d11f17b37f0b9d8647640d923e004af9d
Yuuki Galaxy <galaxy001@gmail.com>
2014-09-04 22:44:08 +0800

    modified:   cntsnp3.pl

--- perl/etc/justonce/sperm/cntsnp3.pl
+++ perl/etc/justonce/sperm/cntsnp3.pl
@@ -4,8 +4,8 @@ use warnings;
 use Data::Dump;
 
 #die "Usage: $0 <blood vcf.gz> <sperm vcf.gz>\n" if @ARGV < 2;
-die "Usage: $0 <spermA vcf> <spermB vcf> <spermC vcf> <out>\n" if @ARGV < 4;
-my ($inA,$inB,$inC,$out)=@ARGV;
+die "Usage: $0 <spermA vcf> <spermB vcf> <spermC vcf>\n" if @ARGV < 3;
+my ($inA,$inB,$inC)=@ARGV;
 
 my (@FH,%SNP);
 for ($inA,$inB,$inC) {
@@ -31,6 +31,7 @@ sub readfile($) {	# 1 .. 3
 	while (<$fh>) {
 		next if /^#/;
 		my ($chr,$pos,undef,$ref,$alt,$QUAL,undef,$INFO,undef,$data) = split /\t/;
+		next if $chr =~ /[XYM]/i;
 		next if $INFO =~ /INDEL;/;
 		next if $QUAL < 20;
 		my $GQ = (split /:/,$data)[-1];
@@ -50,85 +51,34 @@ close $_ for @FH;
 
 my %Stat;
 for my $chr (keys %SNP) {
-	for my $pos (keys %{$Stat{$chr}}) {
-		my %Dat = %{$Stat{$chr}{$pos}};
+	for my $pos (keys %{$SNP{$chr}}) {
+		my %Dat = %{$SNP{$chr}{$pos}};
 		my $flag = 0;
 		for my $k (keys %Dat) {
-			$flag |= 2^$k;
+			$flag |= 2 ** $k;
 		}
-		$Stat{$chr}{$pos} = $flag;
-ddx $Stat{$chr}{$pos}; print "$flag\n";
+		++$Stat{$flag};
+#ddx $SNP{$chr}{$pos}; print "$flag\n\n";
 	}
 }
 
+print "$_\t",$Stat{$_},"\n" for sort {$a<=>$b} keys %Stat;
 
 __END__
-my (%Results,%subResults);
-for my $chr (keys %SNP) {
-	for my $pos (keys %{$SNP{$chr}}) {
-		my ($GT1,$GT2) = ( $SNP{$chr}{$pos}{'BD'}, $SNP{$chr}{$pos}{'SP'} );
-		unless ( defined $GT1 ) {
-			++$Results{$chr}{'SpermOnly'};
-			++$Results{'_All_'}{'SpermOnly'};
-			next;
-		}
-		unless ( defined $GT2 ) {
-			++$Results{$chr}{'BloodOnly'};
-			++$Results{'_All_'}{'BloodOnly'};
-			next;
-		}
-		my @gt1 = split //,$GT1;
-		my @gt2 = split //,$GT2;
-		my ($flag1,$flag2);
-		if ($gt1[0] eq $gt1[1]) { $flag1='Hom'; } else { $flag1='Het'; }
-		if ($gt2[0] eq $gt2[1]) { $flag2='Hom'; } else { $flag2='Het'; }
-#print "$gt1[0] $gt1[1] $gt2[0] $gt2[1] $flag1,$flag2\n";
-		my ($str,$subtype);
-		if ($GT1 eq $GT2) {
-			$str = 'Same';
-		} else {
-			$str = $flag1 . '-' . $flag2;
-			if ( $flag1 eq $flag2 ) {
-				if ($flag1 eq 'Hom') {
-					$subtype = 'Hom' . $gt1[1] . $gt2[1];
-				} else {
-					$subtype = 'Het' . $GT1 . $GT2;
-				}
-			}
-		}
-		++$Results{$chr}{$str};
-		++$Results{'_All_'}{$str};
-		if ( defined $subtype ) {
-			++$subResults{$chr}{$subtype};
-			++$subResults{'_All_'}{$subtype};
-		}
-	}
-}
-ddx \%subResults;
-ddx \%Results;
+perl ./cntsnp3.pl sperm23.5cf.filter sperm24.5cf.filter sperm28.5cf.filter
+2	243901
+4	268409
+6	38623
+8	253689
+10	33956
+12	35961
+14	7207
 
-for my $chr (sort keys %Results) {
-	print O "$chr\t";
-	for my $type (sort keys %{$Results{$chr}}) {
-		print O "$type: $Results{$chr}{$type}\t";
-	}
-	if (exists $subResults{$chr}) {
-		print O "|\t";
-		for my $type (sort keys %{$subResults{$chr}}) {
-			print O "$type: $subResults{$chr}{$type}\t";
-		}
-	}
-	print O "\n";
-}
-
-close IBD;
-close ISP;
-close O;
-
-__END__
-perl cntsnp.pl blood.gz sperm23.gz
-
-perl cntsnp.pl blood.mda.filter.gz sperm23.vcf.filter.gz
-
-
-
+perl ./cntsnp3.pl sperms01.5cf.filter sperms02.5cf.filter sperms03.5cf.filter
+2	676931
+4	635059
+6	60638
+8	623031
+10	65199
+12	58892
+14	36960
