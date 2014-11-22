e7a9c5a3329a1808ea11ac299a18ed1bddb7a3f9
Yuuki Galaxy <galaxy001@gmail.com>
2014-11-23 03:39:07 +0800

    modified:   gitrepo.init.sh

--- gitrepo.init.sh
+++ gitrepo.init.sh
@@ -5,6 +5,12 @@ git remote add c git@coding.net:galaxy/GalaxyCodeBase.git
 git remote -v
 git remote update
 
+cd released/pIRS
+git remote add github git@github.com:galaxy001/pirs.git
+git remote add google https://code.google.com/p/pirs/
+git remote -v
+git remote update
+
 git config --global alias.xpush '!f(){ for i in `git remote`; do git push -v $i; done; };f'
 git config --global alias.xpull '!f(){ for i in `git remote`; do git pull -v $i; done; };f'
 
