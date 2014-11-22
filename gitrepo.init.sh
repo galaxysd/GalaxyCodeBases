c6794b9e81147c43bb807e6d5885c44cd2b5682b
Yuuki Galaxy <galaxy001@gmail.com>
2014-11-23 04:13:11 +0800

    git submodule add git@bitbucket.org:galaxy001/mydoc.git mydoc

--- gitrepo.init.sh
+++ gitrepo.init.sh
@@ -12,5 +12,7 @@ git remote -v
 git remote update
 
 git config --global alias.xpush '!f(){ for i in `git remote`; do git push -v $i; done; };f'
-git config --global alias.xpull '!f(){ for i in `git remote`; do git pull -v $i; done; };f'
+git config --global alias.xpull '!f(){ for i in `git remote`; do git pull -v $i $1; done; };f'
+
+# git submodule add git@bitbucket.org:galaxy001/mydoc.git mydoc
 
