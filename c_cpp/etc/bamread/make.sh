d6eb0625871683ce6712320771408f9cc7028f1c
Yuuki Galaxy <galaxy001@gmail.com>
2015-01-22 02:47:46 +0800

    link htslib here.

--- c_cpp/etc/bamread/make.sh
+++ c_cpp/etc/bamread/make.sh
@@ -2,5 +2,4 @@
 
 rm bamread
 
-gcc -Wfloat-equal -Wall  -pipe -march=core2 -mtune=generic -std=gnu99 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -pthread -I../../lib/htslib -O3 -Wl,-O1 -Wl,--sort-common -Wl,--enable-new-dtags -Wl,--hash-style=both -lz -o bamread bamread.c ../../lib/htslib/libhts.a
-
+gcc -Wfloat-equal -Wall  -pipe -march=core2 -mtune=generic -std=gnu99 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -pthread -I./htslib -O3 -lz -o bamread bamread.c ./htslib/libhts.a
