6a1c63c2231e00ab7fc8f2a84231efc1074feb4d
Yuuki Galaxy <galaxy001@gmail.com>
2015-01-22 02:56:09 +0800

    modified:   make.sh

--- c_cpp/etc/bamread/make.sh
+++ c_cpp/etc/bamread/make.sh
@@ -3,3 +3,6 @@
 rm bamread
 
 gcc -Wfloat-equal -Wall  -pipe -march=core2 -mtune=generic -std=gnu99 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -pthread -I./htslib -O3 -lz -o bamread bamread.c ./htslib/libhts.a
+
+# tar --exclude-vcs --exclude=htslib/test -czvhf bamread.tgz bamread.c make.sh htslib t.bam t.sam.gz truncated.bam
+
