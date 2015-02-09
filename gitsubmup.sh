cea6c1e7d05cdc9afe6f4f6fc8521fe8327fc331
Yuuki Galaxy <galaxy001@gmail.com>
2015-02-10 01:42:51 +0800

    modified:   gitsubmup.sh

--- gitsubmup.sh
+++ gitsubmup.sh
@@ -2,11 +2,10 @@
 
 git pull
 git submodule update
+git submodule foreach git pull
 git submodule foreach git checkout master
 
 git fsck
 git gc
 git submodule foreach git fsck
 git submodule foreach git gc
-
-# git submodule foreach git pull
