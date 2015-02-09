efc45035088f04d7a7d043bb743049df60ed49fd
Yuuki Galaxy <galaxy001@gmail.com>
2015-02-10 01:37:15 +0800

    new file:   gitsubmup.sh

---
+++ gitsubmup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git pull
+git submodule update
+git submodule foreach git checkout master
+
+git fsck
+git gc
+git submodule foreach git fsck
+git submodule foreach git gc
+
+# git submodule foreach git pull
