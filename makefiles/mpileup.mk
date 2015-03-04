d5ec75fb26d9eaee4f2c7dfd8df927dd8357ccf6
Yuuki Galaxy <galaxy001@gmail.com>
2015-03-04 19:54:26 +0800

    modified:   mpileup.mk Usage: make -j42 > t.log 2>&1 &

--- makefiles/mpileup.mk
+++ makefiles/mpileup.mk
@@ -1,10 +1,14 @@
 OUT := test.bcf
 CMD := samtools mpileup -g -d 1000 -t DP,DPR,DV,DP4,SP -f /bak/seqdata/genomes/TigerRefGenome/P_tigris.scaffold.fa
-BAMS := pti096_clean_aln_pe_rmdup.bam pti183_clean_aln_pe_rmdup.bam pti301_clean_aln_pe_rmdup.bam pti332_clean_aln_pe_rmdup.bam
-CHRS := scaffold1000 scaffold979 scaffold982
+
+#BAMS := pti096_clean_aln_pe_rmdup.bam pti183_clean_aln_pe_rmdup.bam pti301_clean_aln_pe_rmdup.bam pti332_clean_aln_pe_rmdup.bam
+BAMS := $(wildcard *.bam)
+#CHRS := scaffold1000 scaffold979 scaffold982
+CHRS := $(shell samtools view -H $(firstword $(BAMS)) | sed -n 's/^@SQ\tSN:\([a-zA-Z0-9_\-]*\).*/\1/p')
 
 BYCHR = $(addsuffix .bcf,$(addprefix bychr/_,$(CHRS)))
 BAIS = $(addsuffix .bai,$(BAMS))
+#GATKBAIS = $(BAMS:.bam=.bai)
 
 define newline
 
@@ -13,9 +17,16 @@ endef
 NOOP :=
 space = $(NOOP) $(NOOP)
 
-.PHONY: all clean cleanall
+.PHONY: all clean cleanall list
 
-all: $(OUT) $(BAIS) bcfbychr.lst
+all: list $(OUT) bcfbychr.lst
+
+list:
+	@echo -e '$(subst $(space),\n,$(BAMS))' >_bams.lst
+	@echo -e '$(subst $(space),\n,$(CHRS))' >_chrs.lst
+	@echo -e '$(subst $(space),\n,$(BYCHR))' >_bcfbychr.lst
+
+bai: $(BAIS)
 
 $(OUT): $(BYCHR) bcfbychr.lst
 	bcftools concat -f bcfbychr.lst -o $(OUT)
@@ -24,14 +35,13 @@ bychr/:
 	mkdir bychr
 
 bcfbychr.lst: $(BYCHR)
-	@echo dbg2 [$^] [$|]
+	@echo dbg2 [$@] [$*] [$^] [$|]
 	echo -e '$(subst $(space),\n,$(BYCHR))' >bcfbychr.lst
 
 %.bai: $*
 	samtools index $*
 
-bychr/_%.bcf: $(BAMS) bychr/ $(BAIS)
-	@echo dbg1 [$@] [$*]
+bychr/_%.bcf: $(BAMS) | bychr/
 	$(CMD) -r $* $(BAMS) >$@
 
 clean:
