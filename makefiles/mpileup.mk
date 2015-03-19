fda05f9aad14191d140849d4ca1decde1084435e
Yuuki Galaxy <galaxy001@gmail.com>
2015-03-20 02:58:14 +0800

    add sort:   mpileup.mk

--- makefiles/mpileup.mk
+++ makefiles/mpileup.mk
@@ -1,10 +1,12 @@
-OUT := test.bcf
+OUTMAIN := mpileup
+OUT = $(addsuffix $(shell date +%Y%m%d%Z%H%M%S).bcf,$(OUTMAIN)_)
+
 CMD := samtools mpileup -g -d 1000 -t DP,DPR,DV,DP4,SP -f /bak/seqdata/genomes/Felis_catus_80_masked/Felis_catus80_chr.fa
 
 #BAMS := pti096_clean_aln_pe_rmdup.bam pti183_clean_aln_pe_rmdup.bam pti301_clean_aln_pe_rmdup.bam pti332_clean_aln_pe_rmdup.bam
-BAMS := $(wildcard *.bam)
+BAMS := $(sort $(wildcard *.bam))
 #CHRS := scaffold1000 scaffold979 scaffold982
-RAWCHRS := $(shell samtools view -H $(firstword $(BAMS)) | sed -n 's/^@SQ\tSN:\([^\t]*\).*/\1/p')
+RAWCHRS := $(sort $(shell samtools view -H $(firstword $(BAMS)) | sed -n 's/^@SQ\tSN:\([^\t]*\).*/\1/p'))
 
 p+ = $(subst |,+,$1)
 +p = $(subst +,|,$1)
@@ -29,6 +31,7 @@ all: list $(OUT) bcfbychr.lst
 list:
 	@echo -e '$(subst $(space),\n,$(BAMS))' >_bams.lst
 	@echo -e '$(subst $(space),\n,$(CHRS))' >_chrs.lst
+	@echo -e '\n$(subst $(space),\n,$(RAWCHRS))' >>_chrs.lst
 	@echo -e '$(subst $(space),\n,$(BYCHR))' >_bcfbychr.lst
 
 bai: $(BAIS)
