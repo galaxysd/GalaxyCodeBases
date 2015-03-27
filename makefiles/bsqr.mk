469cff723979d1c044d9b975a6bca8a72e009179
Yuuki Galaxy <galaxy001@gmail.com>
2015-03-27 17:02:07 +0800

    modified:   bsqr.mk

--- makefiles/bsqr.mk
+++ makefiles/bsqr.mk
@@ -3,14 +3,14 @@ BAMF = $(patsubst %.bam,%,$(BAMS))
 OUT1 = $(addsuffix .recal.tbl,$(BAMF))
 OUT3 = $(addsuffix .post.tbl,$(BAMF))
 OUT4 = $(addsuffix .cmp.pdf,$(BAMF))
-OUT2 = $(addsuffix .recal.bam,$(BAMF))
+OUT2 = $(addsuffix .recalbam,$(BAMF))
 
 REF := /bak/seqdata/genomes/Felis_catus_80_masked/Felis_catus80_chr.fa
 
 .PHONY: all
 
 all: $(OUT1) $(OUT2) $(OUT3) $(OUT4)
-	echo $(BAMS)
+	echo [$(BAMS)] -> [$(OUT2)]
 
 %.recal.tbl: %.bam
 	gatk -T BaseRecalibrator -nct 8 -mte -R $(REF) -I $(*).bam -knownSites filtered2.vcf -o $@ 2>$@.log
@@ -21,5 +21,5 @@ all: $(OUT1) $(OUT2) $(OUT3) $(OUT4)
 %.cmp.pdf: %.recal.tbl %.post.tbl
 	gatk -T AnalyzeCovariates -R $(REF) -before $(@:.cmp.pdf=.recal.tbl) -after $(@:.cmp.pdf=.post.tbl) -plots $@ 2>$@.log
 
-%.recal.bam: %.recal.tbl %.bam
-	gatk -T PrintReads -R $(REF) -I $(*).bam -BQSR $(@:.recal.bam=.recal.tbl) -o $@ 2>$@.log
+%.recalbam: %.recal.tbl %.bam
+	gatk -T PrintReads -R $(REF) -I $(*).bam -BQSR $(@:.recalbam=.recal.tbl) -o $@ 2>$@.log
