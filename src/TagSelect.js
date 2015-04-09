20bec90bc4bb3a50a05382ad64687b16c3e0ba13
Don McCurdy <don.r.mccurdy@gmail.com>
2015-04-09 12:09:27 -0700

    Skip validation if no discrete options are specified.

--- src/TagSelect.js
+++ src/TagSelect.js
@@ -15,7 +15,7 @@ var DEFAULT_ATTRIBUTES = {
 	},
 	FIELD_TPL = ''
 		+	'<% _.forEach(selected, function (fd) { %>'
-		+		'<span data-fd="<%- fd %>" class="tagselect-fd<%= options[fd] ? \'\' : \' invalid\' %>">'
+		+		'<span data-fd="<%- fd %>" class="tagselect-fd<%= options[fd] || !shouldValidate ? \'\' : \' invalid\' %>">'
 		+			'<%- fd %>'
 		+		'</span>'
 		+		'<span class="tagselect-fd-separator">, </span>'
@@ -26,6 +26,7 @@ var DEFAULT_ATTRIBUTES = {
 
 function TagSelect (el, options) {
 	el = el.jquery ? el[0] : el;
+	options = options || {};
 	this.el = el;
 	this.autocomplete = null;
 	this.selected = options.selected || [];
@@ -46,6 +47,7 @@ function TagSelect (el, options) {
 _.merge(TagSelect.prototype, {
 
 	render: function () {
+		this.shouldValidate = !!_.size(this.options);
 		this.el.innerHTML = this.trim(this.template(this));
 	},
 
@@ -97,6 +99,8 @@ _.merge(TagSelect.prototype, {
 	},
 
 	validate: function () {
+		if (!_.size(this.options)) return true;
+
 		for (var i = 0, fd; (fd = this.selected[i]); i++) {
 			if (!this.options[fd]) {
 				return false;
