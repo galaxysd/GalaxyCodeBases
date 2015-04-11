6f29f3591645f0a1228034b1461de5be93b0c8b8
Yuuki Galaxy <galaxy001@gmail.com>
2015-04-11 22:47:51 +0800

    new file:   bash/rmgitsubmodule.sh 	deleted:    c_cpp/{gnulib,pspp}

---
+++ bash/rmgitsubmodule.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# http://stackoverflow.com/questions/1260748/how-do-i-remove-a-git-submodule
+
+if [ x"$1" = x"" ]; then
+	echo "Usage: $0 <git submodule name>"
+	exit 1
+else
+	echo "Removing submodule [$1] ..."
+fi
+
+git submodule deinit $1    
+git rm $1
+
+# Note: [asubmodule] (no trailing slash)
+# or, if you want to leave it in your working tree
+#git rm --cached $1
+
+rm -vrf .git/modules/$1
--- gitsubup.sh
+++ gitsubup.sh
@@ -15,3 +15,4 @@ git submodule
 
 git subtree pull --prefix=c_cpp/lib/cstring git://github.com/cloudwu/cstring.git master
 git subtree pull --prefix=c_cpp/lib/htslib git://github.com/samtools/htslib.git master
+git subtree pull --squash --prefix=c_cpp/lib/gnulib git://git.savannah.gnu.org/gnulib.git
