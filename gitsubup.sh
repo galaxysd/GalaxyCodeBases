add3b9359124e6c2360202dca8c02319f512e112
Yuuki Galaxy <galaxy001@gmail.com>
2015-04-11 16:11:15 +0800

    modified:   .gitmodules 	deleted:    c_cpp/lib/htslib 	modified:   gitsubup.sh

--- gitsubup.sh
+++ gitsubup.sh
@@ -13,4 +13,5 @@ git submodule foreach git gc
 git submodule foreach git checkout master
 git submodule
 
-git subtree pull --prefix=c_cpp/lib/cstring  git://github.com/cloudwu/cstring.git master
+git subtree pull --prefix=c_cpp/lib/cstring git://github.com/cloudwu/cstring.git master
+git subtree pull --prefix=c_cpp/lib/htslib git://github.com/samtools/htslib.git master
