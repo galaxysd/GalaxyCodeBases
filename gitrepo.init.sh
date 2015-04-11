f18b8f9068764539aca2366e60913a6750060f1d
Yuuki Galaxy <galaxy001@gmail.com>
2015-04-12 00:32:23 +0800

    modified:   .gitmodules

--- gitrepo.init.sh
+++ gitrepo.init.sh
@@ -2,12 +2,15 @@
 
 git remote add g git@gitlab.com:galaxycodebases/main.git
 git remote add c git@coding.net:galaxy/GalaxyCodeBase.git
+git remote add github git@github.com:galaxysd/GalaxyCodeBases.git
+# git push -u github master
 git remote -v
 git remote update
 
 cd released/pIRS
-git remote add github git@github.com:galaxy001/pirs.git
-git remote add google https://code.google.com/p/pirs/
+git remote add sf ssh://galaxy001@git.code.sf.net/p/pirsim/code
+# git remote add github git@github.com:galaxy001/pirs.git
+# git remote add google https://code.google.com/p/pirs/
 git remote -v
 git remote update
 
@@ -15,4 +18,3 @@ git config --global alias.xpush '!f(){ for i in `git remote`; do git push -v $i;
 git config --global alias.xpull '!f(){ for i in `git remote`; do git pull -v $i $1; done; };f'
 
 # git submodule add git@bitbucket.org:galaxy001/mydoc.git mydoc
-
