49866c6b6bf7f35c5c339e17ce4dcb34abec3bf8
Yuuki Galaxy <galaxy001@gmail.com>
2015-04-12 21:30:28 +0800

    modified:   gitsubtreeupd.sh

--- gitsubtreeupd.sh
+++ gitsubtreeupd.sh
@@ -7,5 +7,6 @@ git subtree pull --prefix=c_cpp/lib/cstring git://github.com/cloudwu/cstring.git
 git subtree pull --prefix=c_cpp/lib/htslib git://github.com/samtools/htslib.git master
 # git subtree pull --squash --prefix=c_cpp/lib/gnulib git://git.savannah.gnu.org/gnulib.git master 由于服务器“no common commits”，每次更新都得下载全部历史，所以直接gc会变6G的.git目录。
 # git subtree pull --squash --prefix=c_cpp/lib/gnulib git://github.com/coreutils/gnulib.git master 仍然“no common commits”，可能本地必须有上个关键commit，吧？
+git subtree pull --prefix=c_cpp/fromOthers/PEAR git://github.com/xflouris/PEAR.git master
 git fsck
 git gc --aggressive --prune=now
