593075608f2b3a93f0a084a8ea567d4b02cff850
Yuuki Galaxy <galaxy001@gmail.com>
2015-07-04 22:56:24 +0800

    modified:   lib/BSuitLib.pm

--- perl/bsIntegration/lib/BSuitLib.pm
+++ perl/bsIntegration/lib/BSuitLib.pm
@@ -1,8 +1,13 @@
 package main;
+use File::Path;
 
 sub do_pre() {
 	#my $Config = $_[0];
 	ddx \$Config;
+	my $RootPath = $Config->{'Output'}->{'WorkDir'};
+	$RootPath =~ s/[\/\\]+$//g;
+	warn "[!] WorkDir: [$RootPath]\n";
+	File::Path::make_path("$RootPath/Ref",{verbose => 0,mode => 0755});
 }
 
 
