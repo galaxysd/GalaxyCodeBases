3108aa069e825ef212037a2bf729a157c97837a5
Yuuki Galaxy <galaxy001@gmail.com>
2015-07-05 03:17:03 +0800

    modified:   lib/BSuitLib.pm

--- perl/bsIntegration/lib/BSuitLib.pm
+++ perl/bsIntegration/lib/BSuitLib.pm
@@ -1,5 +1,7 @@
 package main;
 use File::Path;
+use File::Basename;
+use Digest::SHA; # qw(sha1_base64);
 
 sub do_pre() {
 	#my $Config = $_[0];
@@ -8,6 +10,10 @@ sub do_pre() {
 	$RootPath =~ s/[\/\\]+$//g;
 	warn "[!] WorkDir: [$RootPath]\n";
 	File::Path::make_path("$RootPath/Ref",{verbose => 0,mode => 0755});
+	my $HostRefName = basename($Config->{'RefFiles'}->{'HostRef'});
+	my $VirusRefName = basename($Config->{'RefFiles'}->{'VirusRef'});
+	my $RefFilesSHA = Digest::SHA::sha1_base64("$HostRefName,$VirusRefName");
+warn "[$HostRefName,$VirusRefName] -> [$RefFilesSHA]\n";
 	if ( -f "$RootPath/Ref/Ref.ini" ) {
 		my $RefConfig = Galaxy::IO::INI->new();
 		$Config->read("$RootPath/Ref/Ref.ini");
