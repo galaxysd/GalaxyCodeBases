6805a64abb141e50ad0a791031b111d0d8d0da33
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-02 23:00:35 +0800

    modified:   simVirusInserts.pl

--- perl/bsim/simVirusInserts.pl
+++ perl/bsim/simVirusInserts.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 use Data::Dump qw(ddx);
+use Cwd 'abs_path';
 
 use FindBin 1.51 qw($RealBin);
 use lib "$RealBin";
@@ -26,6 +27,20 @@ my $VirLen = length $Virstr;
 close $Virfh;
 $Virstr .= $Virstr;	# circle
 
+open INI,'>',$outp.'.ini' or die $!;
+print INI <<CONTENT;
+[RefFiles]
+HostRef=/bak/seqdata/genomes/HomoGRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+VirusRef=/share/users/huxs/work/bsvir/HBV.AJ507799.2.fa
+
+[Output]
+WorkDir=/share/users/huxs/work/bsvir/bsI
+ProjectID=simVir
+
+CONTENT
+
+my %fqFiles;
+
 my %Para = (
 	PEinsertLen => 200,
 	SeqReadLen => 90,
@@ -36,36 +51,55 @@ my %Para = (
 	OutPrefix => $outp . '_m13FG',
 );
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 200;
 $Para{VirFrag} = 45;
 $Para{OutPrefix} = $outp . '_m2D';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 420;
 $Para{VirFrag} = 120;
 $Para{OutPrefix} = $outp . '_m458AE';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 200;
 $Para{VirFrag} = 120;
 $Para{OutPrefix} = $outp . '_m9';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 150;
 $Para{VirFrag} = 120;
 $Para{OutPrefix} = $outp . '_m6';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 150;
 $Para{VirFrag} = 45;
 $Para{OutPrefix} = $outp . '_m7C';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
 
 $Para{PEinsertLen} = 100;
 $Para{VirFrag} = 45;
 $Para{OutPrefix} = $outp . '_mB';
 dosim($Refstr,$Virstr,\%Para);
+$fqFiles{$Para{OutPrefix}} = $Para{PEinsertLen};
+
+my @fps = sort keys %fqFiles;
+print INI "[DataFiles]\n";
+for my $i (0 .. $#fps) {
+	print INI "F${i}.1=",abs_path($fps[$i].'.1.fq'),"\n";
+	print INI "F${i}.2=",abs_path($fps[$i].'.2.fq'),"\n";
+}
+print INI "\n[InsertSizes]\n";
+for my $i (0 .. $#fps) {
+	print INI "F${i}=",$fqFiles{$fps[$i]},"\nF${i}.SD=1\n";
+}
+close INI;
 
 __END__
 ./simVirusInserts.pl hs_ref_GRCh38.p2_chr18.mfa.gz HBV.AJ507799.2.fa.gz simout
