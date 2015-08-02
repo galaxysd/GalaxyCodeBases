8a57dcf56454d4fbd2671d9e67441906d9e9b1d4
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-02 17:12:30 +0800

    new file:   perl/bsim/simVirusInserts.pl

---
+++ perl/bsim/simVirusInserts.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dump qw(ddx);
+
+use FindBin 1.51 qw($RealBin);
+use lib "$RealBin";
+use simVirusInserts;
+
+die $0;
---
+++ perl/bsim/simVirusInserts.pm
@@ -0,0 +1,4 @@
+package main;
+
+
+1;
