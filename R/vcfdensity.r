d83a551d304b6045cd2935eb8efbc7ebdd457a36
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-07 01:37:19 +0800

    modified:   vcfdensity.r

--- R/vcfdensity.r
+++ R/vcfdensity.r
@@ -1,27 +1,40 @@
 #!/usr/bin/env littler
 
 if (!interactive()) {
-	if (exists("argv")) {
-		if (!is.null(argv) && length(argv)>0) {
-			print(argv)
+	if (is.null(argv) | length(argv)<3) {
+		cat("Usage: ./vcfdensity.r <WinSize> <input.vcf.gz> <output.prefix>\n")
+		q(status=1)
+	} else {
+		WinSize <- as.integer(argv[1])
+		if (is.na(WinSize)) WinSize <- 0L
+		InVCFgz <- trimws(argv[2],'both')
+		OutP <- trimws(argv[3],'both')
+		cat("[!] WinSize=[",WinSize,"], [",InVCFgz,"]->[",OutP,"]\n",sep='')
+		if (!file.exists(InVCFgz)) {
+			cat("[x] File not found:[", InVCFgz, "] !\n",sep='')
+			q(status=-1)
+		}
+		if ( WinSize < 1L ) {
+			cat("[x] WinSize=[", WinSize, "] is smaller than [1] !\n",sep='')
+			q(status=-1)
 		}
 	}
 }
 
 library('data.table')
-library('zoo')
-tab5rows <- read.table(pipe("gzip -dc vcf.gz | cut -f1,2"),sep="\t",nrows=5)
+suppressPackageStartupMessages(library('zoo'))
+tab5rows <- read.table(pipe(paste0("gzip -dc ",InVCFgz," | cut -f1,2")),sep="\t",nrows=5)
 classes <- sapply(tab5rows, class)
 #tabAll <- read.table(pipe("zcat vcf.gz | cut -f1,2|head -300"),sep="\t", colClasses = classes,col.names=c('Chr','Pos'))
-tabAll <- fread("gzip -dc vcf.gz|grep -ve '^#' |head -n500000000",header=F,stringsAsFactors=T,sep="\t",autostart=100,select=c(1,2), colClasses=classes, data.table=T,verbose=T)
+tabAll <- fread(paste0("gzip -dc ",InVCFgz,"|grep -ve '^#' |head -n500000000"),header=F,stringsAsFactors=T,sep="\t",autostart=100,select=c(1,2), colClasses=classes, data.table=T,verbose=F)
 #tabAll <- fread("cat t.vcf|grep -ve '^#' |head -n500000000",header=F,verbose=T,sep="\t",autostart=100,select=c(1,2),stringsAsFactors=T, colClasses=classes,data.table=T)
 
-print(tabAll)
+#print(tabAll)
 
 Poses <- split(tabAll$V2,tabAll$V1)
-print(Poses[2])
+#print(Poses[2])
 
-WinSize <- 1000
+#WinSize <- 1000
 
 resAll <- integer(0)
 for (p in Poses) {
@@ -33,11 +46,11 @@ for (p in Poses) {
 	res0 <- rollapply(chrdat, WinSize, sum, by = WinSize)
 	resAll <- append(resAll,res0)
 }
-res <- hist(resAll,plot=F)
+#res <- hist(resAll,plot=F)
 tbl <- table(resAll)
 
-print(resAll)
-print(res)
+#print(resAll)
+#print(res)
 print(tbl)
 
 
