46c637cdd3f23e45f01c937a86cb386734b84055
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-07 00:30:05 +0800

    modified:  R/vcfdensity.r, mydoc

--- R/vcfdensity.r
+++ R/vcfdensity.r
@@ -1,32 +1,48 @@
 #!/usr/bin/env littler
 
+if (!interactive()) {
+	if (exists("argv")) {
+		if (!is.null(argv) && length(argv)>0) {
+			print(argv)
+		}
+	}
+}
+
 library('data.table')
 library('zoo')
-tab5rows <- read.table(pipe("zcat vcf.gz | cut -f1,2"),sep="\t",nrows=5)
+tab5rows <- read.table(pipe("gzip -dc vcf.gz | cut -f1,2"),sep="\t",nrows=5)
 classes <- sapply(tab5rows, class)
 #tabAll <- read.table(pipe("zcat vcf.gz | cut -f1,2|head -300"),sep="\t", colClasses = classes,col.names=c('Chr','Pos'))
-#tabAll <- fread("zcat vcf.gz|grep -vP '^#' |head -n500000000",header=F,verbose=T,sep="\t",autostart=100,select=c(1,2),stringsAsFactors=T, colClasses=classes,data.table=T)
-tabAll <- fread("cat t.vcf|grep -vP '^#' |head -n500000000",header=F,verbose=T,sep="\t",autostart=100,select=c(1,2),stringsAsFactors=T, colClasses=classes,data.table=T)
+tabAll <- fread("gzip -dc vcf.gz|grep -ve '^#' |head -n500000000",header=F,stringsAsFactors=T,sep="\t",autostart=100,select=c(1,2), colClasses=classes, data.table=T,verbose=T)
+#tabAll <- fread("cat t.vcf|grep -ve '^#' |head -n500000000",header=F,verbose=T,sep="\t",autostart=100,select=c(1,2),stringsAsFactors=T, colClasses=classes,data.table=T)
 
 print(tabAll)
 
 Poses <- split(tabAll$V2,tabAll$V1)
 print(Poses[2])
 
-t=sapply(Poses,mean)
-print(t)
+WinSize <- 1000
 
-WinSize <- 10
+resAll <- integer(0)
+for (p in Poses) {
+	chrdat <- integer(0)
+	for (i in p) chrdat[i] <- 1L
+	thelen <- length(chrdat)
+	length(chrdat) <- ceiling(thelen/WinSize)*WinSize
+	chrdat[is.na(chrdat)] <- 0L
+	res0 <- rollapply(chrdat, WinSize, sum, by = WinSize)
+	resAll <- append(resAll,res0)
+}
+res <- hist(resAll,plot=F)
+tbl <- table(resAll)
 
-a <- c()
-a[1]=1
-a[10]=1
-a[20]=1
-a[56]=1
-a[58]=1
-a[59]=1
-a[67]=1
-a[is.na(a)] <- 0
-t=rollapply(a, WinSize, sum, by = WinSize)
-print(a)
-print(t)
+print(resAll)
+print(res)
+print(tbl)
+
+
+
+
+
+#t=sapply(Poses,mean)
+
