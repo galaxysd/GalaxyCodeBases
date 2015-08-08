f71d6b81ce58efa8adf4b7f2af0e6c7c435721af
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-08 15:13:30 +0800

    Parallel done:   vcfdensity.r From http://stackoverflow.com/questions/19082794/speed-up-data-table-group-by-using-multiple-cores-and-parallel-programming

--- R/vcfdensity.r
+++ R/vcfdensity.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env littler
 
-#library('parallel')
-#NumofCore <- detectCores(logical = TRUE)
+library('parallel')
+NumofCore <- detectCores(logical = TRUE)
 
 if (!interactive()) {
 	if (is.null(argv) | length(argv)<3) {
@@ -12,7 +12,7 @@ if (!interactive()) {
 		if (is.na(WinSize)) WinSize <- 0L
 		InVCFgz <- trimws(argv[2],'both')
 		OutP <- trimws(argv[3],'both')
-		cat("[!] WinSize=[",WinSize,"], [",InVCFgz,"]->[",OutP,"].*\n",sep='')
+		cat("[!] WinSize=[",WinSize,"], [",InVCFgz,"]->[",OutP,"].*, Core:[",NumofCore,"]\n",sep='')
 		if (!file.exists(InVCFgz)) {
 			cat("[x] File not found:[", InVCFgz, "] !\n",sep='')
 			q(status=-1)
@@ -22,6 +22,11 @@ if (!interactive()) {
 			q(status=-1)
 		}
 	}
+} else {	# for `source("vcfdensity.r")`
+	InVCFgz <- 't.vcf'
+	WinSize <- 100L
+	OutP <- 'test'
+	cat("[!] WinSize=[",WinSize,"], [",InVCFgz,"]->[",OutP,"].*, Core:[",NumofCore,"]\n",sep='')
 }
 
 library('data.table')
@@ -45,9 +50,18 @@ options(datatable.verbose=T)
 #classes <- sapply(tab5rows, class)
 classes <- c(V1="factor",V2="integer")
 #tabAll <- read.table(pipe("zcat vcf.gz | cut -f1,2|head -300"),sep="\t", colClasses = classes,col.names=c('Chr','Pos'))
-tabAll <- fread(paste0("gzip -dc ",InVCFgz,"|awk '!/^#|\tINDEL;/'|cut -f1,2"),header=F,stringsAsFactors=T,sep="\t",autostart=100,select=c(1,2), colClasses=classes, data.table=T)
+
+if (grepl("\\.gz$",InVCFgz,ignore.case=T)) {
+	fileIn <- paste0("gzip -dc ",InVCFgz,"|awk '!/^#|\tINDEL;/'|cut -f1,2")
 # grep -ve '^#' 也可，但 MacOS X 下没有 grep -vP '\tINDEL'
 # Piped file will be in `/dev/shm/file46583ba3b517` tempory, thus add `|cut -f1,2` to reduce its footprint.
+} else if (grepl("\\.vcf$",InVCFgz,ignore.case=T)) {
+	fileIn <- InVCFgz
+} else {
+	fileIn <- paste0("bcftools view -H -vsnps ",InVCFgz,"|cut -f1,2")
+}
+
+tabAll <- fread(fileIn, header=F,stringsAsFactors=T,sep="\t",autostart=100,select=c(1,2), colClasses=classes, data.table=T)
 setnames(tabAll,1,'Chr')
 setnames(tabAll,2,'Pos')
 print(head(tabAll))
@@ -64,8 +78,19 @@ dorolling <- function(x, rollwin) {
 	res0 <- wapply(chrdat, rollwin, FUN = sum,na.rm = TRUE)
 	return(res0)
 }
-resArr <- tabAll[, .(Density=dorolling(Pos,rollwin=WinSize)), by=Chr]
+#resArr <- tabAll[, dorolling(Pos,rollwin=WinSize), by=Chr]
+#resAll <- unlist(resArr$V1,use.names=F)
+
+#resArr <- tabAll[, .(Density=dorolling(Pos,rollwin=WinSize)), by=Chr]
 # lapply optimization changed j from 'lapply(.SD, dorolling, rollwin = WinSize)' to 'list(dorolling(Pos, rollwin = WinSize))'
+
+# http://stackoverflow.com/questions/19082794/speed-up-data-table-group-by-using-multiple-cores-and-parallel-programming
+resArr <- mclapply(unique(tabAll[["Chr"]]),
+	function(x) {
+		tabAll[.(x),.(Density=dorolling(Pos,rollwin=WinSize)), by=Chr]
+	}, mc.cores=NumofCore)
+resArr <- rbindlist(resArr)
+
 resAll <- unlist(resArr$Density,use.names=F)
 cat("[!] Stat done.\n")
 
