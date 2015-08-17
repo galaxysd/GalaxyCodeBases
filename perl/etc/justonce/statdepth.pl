61f8ba081c49c5a3946d09c003b5cf87a7a28b2f
Yuuki Galaxy <galaxy001@gmail.com>
2015-08-17 16:25:20 +0800

    new file:   statdepth.pl

---
+++ perl/etc/justonce/statdepth.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dump qw(ddx);
+
+die "Usage: $0 <depth file>\n" if @ARGV <1;
+
+my $in = shift;
+
+open I,'<',$in or die;
+my (%Depdat,%Ranges);
+while (<I>) {
+	chomp;
+	my ($chr,$pos,$depth) = split /\t/;
+	$Depdat{$chr}{$pos} = $depth;
+}
+close I;
+for my $chr (sort keys %Depdat) {
+	my @poses = sort {$a <=> $b} keys %{$Depdat{$chr}};
+	my ($st,$ed);
+	while (@poses) {
+		$st = shift @poses;
+		$ed = shift @poses;
+		while ($poses[0] - $ed == 1) {
+			$ed = shift @poses;
+		}
+		push @{$Ranges{$chr}},[$st,$ed];
+	}
+}
+ddx \%Ranges;
