6c5005a44f0c9e28960e0ee3420387137f7da47b
Yuuki Galaxy <galaxy001@gmail.com>
2015-09-10 16:44:24 +0800

    new file:   bsIntegration/src/methlyAln/bin/t.pl

---
+++ perl/bsIntegration/src/methlyAln/bin/t.pl
@@ -0,0 +1,18 @@
+#!/bin/env perl
+use strict;
+use warnings;
+
+use IPC::Open2;
+my  $pid = open2( \*READER, \*WRITER, "./alnmethly" );
+    WRITER->autoflush(); # default here, actually
+my $got = "";
+my $input = " ";
+
+print WRITER "a,gtttaacccgg\n,gttCggttCg\nxxx\n";
+
+while(<READER>) {
+	chomp;
+	if (/^Path(\d): (.+)$/) {
+		print "[$_] [$1] [$2]\n";
+	}
+}
