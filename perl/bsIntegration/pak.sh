23463d03e56bf237657bcad496812bafd7c02658
Yuuki Galaxy <galaxy001@gmail.com>
2015-09-14 23:19:16 +0800

    modified:   pak.sh

--- perl/bsIntegration/pak.sh
+++ perl/bsIntegration/pak.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm ~/bsIntegration.tgz
-tar --one-file-system --exclude bsvir --exclude pak.sh --exclude "*.0*" --exclude ProteinogenicAA --exclude Casava --exclude bsI --exclude "t.*" --exclude "*.ini" -czvLf ~/bsIntegration.tgz .
+tar --one-file-system --exclude bsvir --exclude pak.sh --exclude "*.0*" --exclude ProteinogenicAA --exclude Casava --exclude bsI --exclude "t.*" --exclude "*.ini" -czvhf ~/bsIntegration.tgz .
 ls -l ~/bsIntegration.tgz
