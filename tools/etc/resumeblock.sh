dbd0407cd0f3865b3818439d015a5f1ed7e130a2
Yuuki Galaxy <galaxy001@gmail.com>
2015-09-20 21:36:18 +0800

    new file:   resumeblock.sh

---
+++ tools/etc/resumeblock.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source_disk=/dev/disk0s4
+host='huxs@lab.luo-lab.org'
+host_image=/share/users/huxs/t/disk0s4.img
+
+bytes_transferred() {
+  if ! ssh -p2211 "$host" "test -e '$host_image'"; then
+    echo 0
+    return
+  fi
+  ssh -p2211 "$host" "stat -c '%s' '$host_image'"
+}
+bytes_total() {
+  #echo $(( $(blockdev --getsz $source_disk) * 512 ))
+  echo `diskutil info $source_disk|grep 'Total Size'|awk '{print $5}'|sed 's/(//'`
+}
+
+echo "$(bytes_total) to $(bytes_transferred)"
+
+while (( $(bytes_transferred) < $(bytes_total) )); do
+  ( dd bs=1 skip=$(bytes_transferred) count=0 2>/dev/null && cat ) < $source_disk | ssh -p2211 "$host" "cat >> '$host_image'"
+done
+
