757a721c8bed456e5cadfa21fbcb9560aa2d64ad
Yuuki Galaxy <galaxy001@gmail.com>
2015-11-15 21:44:28 +0800

    modified:   gitsubtreeupd.sh

--- gitsubtreeupd.sh
+++ gitsubtreeupd.sh
@@ -6,13 +6,18 @@ git diff .
 # `git subtree add` when it does not exist.
 git subtree pull --prefix=c_cpp/lib/cstring git://github.com/cloudwu/cstring.git master
 git subtree pull --prefix=c_cpp/lib/htslib git://github.com/samtools/htslib.git master
+
 # 已被和谐 git subtree pull --prefix=etc/gfwlist2pac git://github.com/clowwindy/gfwlist2pac.git master
+git subtree pull --prefix=etc/genpac https://github.com/JinnLynn/genpac.git master
 git subtree pull --squash --prefix=etc/autoproxy-gfwlist git://github.com/calfzhou/autoproxy-gfwlist.git master
+
 git subtree pull --prefix=c_cpp/lib/klib git://github.com/attractivechaos/klib.git master
 git subtree pull --prefix=tools/bioawk git://github.com/lh3/bioawk.git master
 git subtree pull --prefix=tools/lh3misc https://github.com/lh3/misc.git master
+
 git subtree pull --prefix=pubdoc/Markdown-Syntax-CN https://gitcafe.com/riku/Markdown-Syntax-CN.git master
 git subtree pull --prefix=etc/whichpm https://github.com/mklement0/whichpm.git master
+
 git fsck
 git gc --aggressive --prune=now
 
