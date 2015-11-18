818541f6f4fd8873e850ee5799b58eca24d3525b
Yuuki Galaxy <galaxy001@gmail.com>
2015-11-19 00:30:08 +0800

    deleted:    etc/gfwlist2pac for moving.

--- etc/gfwlist2pac/test/log2test.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import re
-
-with open('1000.log', 'rb') as f:
-    for line in f:
-        line = line.strip()
-        m = re.findall('connecting (.*?):', line)
-        if m:
-            print(m[0])
