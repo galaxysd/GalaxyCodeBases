54b15f73e51af44a5aca5f69b0801e6d9d3c07c2
Yuuki Galaxy <galaxy001@gmail.com>
2016-02-20 01:17:18 +0800

    modified:   perl/bsIntegration/src/install.sh

--- perl/bsIntegration/src/install.sh
+++ perl/bsIntegration/src/install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ ! -d "./idba" ]; then
+	./download.sh
+fi
+
 cd bwa; make
 cp -av bwa ../../bin/
 cd ..
@@ -8,18 +12,35 @@ cd samtools; make
 cp -av samtools ../../bin/
 cd ..
 
+uname_S=`uname -s 2>/dev/null || echo not`
+
 cd idba
 aclocal
 autoconf
 automake --add-missing
-./configure
+if [[ "$uname_S" == "Darwin" ]]; then
+	gcc5_S=`which g++-5 || echo not`
+	if [[ "$gcc5_S" == "not" ]]; then
+		brew install --without-multilib gcc
+	fi
+	CXX=g++-5 ./configure
+else
+	./configure
+fi
 #make clean
 make -j
 cp -av bin/idba_hybrid ../../bin/
 cd ..
 
-cd methlyAln/bin; cmake ..
-cp -av alnmethly ../../../bin/
+if [[ "$uname_S" == "Darwin" ]]; then
+	cd methlyAln/src; make
+	cp -av ../bin/alnmethly ../../../bin/
+else
+	cd methlyAln/bin; cmake ..
+	cp -av alnmethly ../../../bin/
+fi
 cd ../..
 
 ls -l ../bin/
+
+echo "[!]To clean: rm -fr idba bwa samtools htslib"
