d7756d6fb310f28f6e32a6eff4212fb421cdce40
Yuuki Galaxy <galaxy001@gmail.com>
2016-02-27 01:47:41 +0800

    modified:   a1script.r, a1funs.r

--- R/utoronto_radford_csc120/a1funs.r
+++ R/utoronto_radford_csc120/a1funs.r
@@ -1,9 +1,5 @@
 # source("a1funs.r")
 
-find_pairings <- function () {
-	;
-}
-
 distance <- function (pa,pb) {
 	differ <- pa-pb
 	diss <- sum(differ*differ)
@@ -20,14 +16,90 @@ total_distance <- function (mpoints,lpairs) {
 	dis_summer
 }
 
-plot_pairs <- function (mpoints,lpairs) {
+plot_pairs <- function (mpoints,lpairs,mytitle='',plot=FALSE, wait=TRUE) {
+	if (plot == FALSE) return
+	if (wait == TRUE) readline(prompt="Press [enter] to continue")
 	plot(mpoints,type='p',xlab="x",ylab='y',pch = 19)
 	for (edge in lpairs) {
 		xyxy <- mpoints[edge[1:2],]
 		lines(xyxy[,1],xyxy[,2])
 	}
+	title(mytitle)
 }
 
 random_pairings <- function (mpoints) {
-	
+	n <- nrow(mpoints)
+	n <- as.integer(n/2)*2
+	smp <- sample(n)
+	foo <- matrix(smp,ncol=2)
+	lapply(1:nrow(foo), function(i) foo[i,])
+	# lapply(seq_len(ncol(x)), function(i) x[,i])
 }
+
+improve_pairings <- function (mpoints,inpairs,cnt=0,plot=FALSE) {
+	lpairs <- inpairs
+	if (cnt==0) cnt <- length(inpairs)
+	i <- 1
+	while (i < cnt) {
+		edgeO <- lpairs[c(i,i+1)]
+		abxy <- unlist(edgeO)
+		#xxyy <- simplify2array(edgeO)
+		axby <- list(c(abxy[c(1,3)]),abxy[c(2,4)])
+		aybx <- list(c(abxy[c(1,4)]),abxy[c(2,3)])
+		disO <- total_distance(mpoints,edgeO)
+		disaxby <- total_distance(mpoints,axby)
+		disaybx <- total_distance(mpoints,aybx)
+		if (disaxby < disO) {
+			lpairs[i] <- axby[1]
+			lpairs[i+1] <- axby[2]
+		}
+		if (disaybx < disO) {
+			lpairs[i] <- aybx[1]
+			lpairs[i+1] <- aybx[2]
+			if (disaxby < disaybx) {
+				lpairs[i] <- axby[1]
+				lpairs[i+1] <- axby[2]
+			}
+		}
+		#plot_pairs(mpoints,lpairs,paste('Time(s)',(i+1)/2),plot)
+		i <- i + 2
+	}
+	lpairs
+}
+
+find_pairings <- function (mpoints, tries=1, plot=FALSE) {
+	pairs0 <- random_pairings(mpoints)
+	plot_pairs(mpoints,pairs0,'Init.',plot)
+	new_pairs <- improve_pairings(mpoints,pairs0,tries,plot)
+	plot_pairs(mpoints,new_pairs,paste('Time(s)',tries),plot)
+}
+
+find_pairingsY <- function (mpoints, tries=1, plot=FALSE) {
+	pairs0 <- random_pairings(mpoints)
+	plot_pairs(mpoints,pairs0,'Init.',plot)
+	lastpairs <- pairs0
+	for (i in 1:tries) {
+		new_pairs <- improve_pairings(mpoints,lastpairs)
+		plot_pairs(mpoints,new_pairs,paste('Time(s)',i),plot)
+		lastpairs <- new_pairs
+	}
+}
+
+find_pairingsX <- function (mpoints, tries=1, plot=FALSE) {
+	pairs0 <- random_pairings(mpoints)
+	plot_pairs(mpoints,pairs0,'Init.',plot)
+	cnt <- length(pairs0)
+	if (tries > cnt) tries <- cnt
+	if (tries < 1) tries <- 1
+	need_id <- sample(cnt)[1:tries]
+	need_imp <- pairs0[need_id]
+	had_imp <- improve_pairings(mpoints,need_imp)
+	pairs1 <- pairs0
+	for (id in need_id) {
+		t <- 1
+		pairs1[id] <- had_imp[t]
+		t <- t + 1
+	}
+	plot_pairs(mpoints,pairs1,paste('Tries(s)',tries),plot)
+}
+
--- R/utoronto_radford_csc120/a1script.r
+++ R/utoronto_radford_csc120/a1script.r
@@ -4,3 +4,11 @@ source("a1funs.r")
 
 #a1data <- as.matrix(read.table("http://www.cs.utoronto.ca/~radford/csc120/a1data.txt"))
 a1data <- as.matrix(read.table('a1data.txt'))
+
+set.seed(1)
+find_pairings(a1data,1,TRUE)
+find_pairings(a1data,10,TRUE)
+
+set.seed(999612806)
+find_pairings(a1data,1,TRUE)
+find_pairings(a1data,10,TRUE)
