8753f4991d0ef85f4d3ef69008dd5f2cad8d228c
Yuuki Galaxy <galaxy001@gmail.com>
2016-04-10 19:30:43 +0800

    modified:   ffp.pl

--- perl/etc/justonce/tmp/ffp.pl
+++ perl/etc/justonce/tmp/ffp.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use threads;
 
-die "Usage: $0 <TS file> <Out mkv> [Split=15]\n" if @ARGV <2;
+die "Usage: $0 <TS file> <Out>.mkv [Split=15]\n" if @ARGV <2;
 
 my ($fin,$fout,$nsplit)=@ARGV;
 $nsplit = 15 unless defined $nsplit;
@@ -21,17 +21,17 @@ for my $j (1 .. $nsplit) {
 	#push @jobs,[$Port1,$Port2,$OutTag];
 	$ffarmIDs .= "[$OutTag]";
 	$PreOut .= " -map \'[$OutTag]\' -f yuv4mpegpipe tcp://localhost:$Port1?listen";
-	push @ffarms,"$FFMPEG_BIN -i tcp://localhost:$Port1 -vf owdenoise=8:0.309:0,fps=\'$fps1001n/1001\' -f yuv4mpegpipe tcp://localhost:$Port2?listen";
+	push @ffarms,"$FFMPEG_BIN -i tcp://localhost:$Port1 -vf owdenoise=8:0.309:0,fps=\'$fps1001n/1001\' -f yuv4mpegpipe tcp://localhost:$Port2?listen 2>&1 >$fout.p$j.log";
 	$foutIn .= " -i tcp://localhost:$Port2";
 }
 
 sub doffarm() {
-	#system($_);
+	#system("AV_LOG_FORCE_COLOR=1 $cmd");
 	print "[s]do [$_]\n";
 }
 
-my $cmd = "$FFMPEG_BIN -threads 16 -i $fin -lavfi \"bwdif=send_field:tff,mcdeint=fast:tff:10,scale=w=iw:h=ih/2,drawtext=\'fontfile=arial.ttf:text=%{n}_%{pict_type}_%{pts}:fontcolor=Aqua:fontsize=32\',select=n=4:e=\'mod(n,$nsplit)+1\'$ffarmIDs\"$PreOut";
-#system($cmd);
+my $cmd = "$FFMPEG_BIN -threads 16 -i $fin -lavfi \"bwdif=send_field:tff,mcdeint=fast:tff:10,scale=w=iw:h=ih/2,drawtext=\'fontfile=arial.ttf:text=%{n}_%{pict_type}_%{pts}:fontcolor=Aqua:fontsize=32\',select=n=4:e=\'mod(n,$nsplit)+1\'$ffarmIDs\"$PreOut 2>&1 >$fout.pre.log";
+#system("AV_LOG_FORCE_COLOR=1 $cmd");
 print "[m]do [$cmd]\n";
 
 sleep 1;
@@ -43,8 +43,9 @@ foreach (@ffarms) {
 
 sleep 3;
 
-$cmd = "$FFMPEG_BIN $foutIn -lavfi [0:v][1:v][2:v][3:v]interleave=4,fps=\'60000/1001\' -pix_fmt yuv420p10le -vcodec libx264 -preset placebo -x264opts \'crf=23:vbv-maxrate=15000:vbv-bufsize=15000:threads=32:colormatrix=bt709:colorprim=bt709:transfer=bt709:psnr=1:ssim=1\' -vframes 1600 -f matroska -y $fout";
-#system($cmd);
+#$cmd = "$FFMPEG_BIN $foutIn -lavfi [0:v][1:v][2:v][3:v]interleave=4,fps=\'60000/1001\' -pix_fmt yuv420p10le -vcodec libx264 -preset placebo -x264opts \'crf=23:vbv-maxrate=15000:vbv-bufsize=15000:threads=32:colormatrix=bt709:colorprim=bt709:transfer=bt709:psnr=1:ssim=1\' -vframes 1600 -f matroska -y $fout.mkv 2>&1 >$fout.cmp.log";
+$cmd = "$FFMPEG_BIN $foutIn -lavfi [0:v][1:v][2:v][3:v]interleave=4,fps=\'60000/1001\' -pix_fmt yuv420p10le -strict -1 -y -f yuv4mpegpipe - | mpv -";
+#system("AV_LOG_FORCE_COLOR=1 $cmd");
 print "[m]do [$cmd]\n";
 
 my %count;
