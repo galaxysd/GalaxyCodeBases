2d37fdba1d9e7197a10bbfb67037b36b90fc2a69
Yuuki Galaxy <galaxy001@gmail.com>
2016-10-10 15:35:24 +0800

    modified:   perl/bsvf/lib/BSuitInc.pm

--- perl/bsvf/lib/BSuitInc.pm
+++ perl/bsvf/lib/BSuitInc.pm
@@ -676,7 +676,8 @@ if ($DEBGUHERE) {
 						#$tlen = $tl;
 						print STDERR ']';
 						#next;	# CNS部分，不容有失，这个偶发问题暂时跳过
-						$t = -$readlen;	# 修改后Fk150_m2D的TP下降，是故，冒烟吧。
+						#$t = -$readlen;	# 修改后Fk150_m2D的TP下降，是故，冒烟吧。
+						no warnings 'substr';
 					}
 					$vseq = substr $i->[9],$t,$tlen;
 					$vqual = substr $i->[10],$t,$tlen;
@@ -691,7 +692,8 @@ if ($DEBGUHERE) {
 						#ddx $i; print "--->Off:$offset, Start:$t, Len:$tlen\n";
 						print STDERR '[';
 						#next;	# 不想倒着算了
-						$t = $readlen-1;
+						#$t = $readlen-1;
+						no warnings 'substr';
 					}
 					$vseq = substr $i->[9],$t,$tlen;
 					$vqual = substr $i->[10],$t,$tlen;
