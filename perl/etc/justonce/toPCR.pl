1d2b0dd57d4132b75174ee8d7947fdc93a080121
Yuuki Galaxy <galaxy001@gmail.com>
2017-06-13 15:48:48 +0800

    modified:   perl/etc/justonce/toPCR.pl, perl/perlib/etc/Galaxy/IO/FASTA.pm

--- perl/etc/justonce/toPCR.pl
+++ perl/etc/justonce/toPCR.pl
@@ -1,17 +1,39 @@
 #!/usr/bin/env perl
 =pod
 Author: Hu Xuesong @ BGI <huxuesong@genomics.org.cn>
-Version: 1.0.0 @ 20120309
+Version: 1.0.0 @ 20170612
 =cut
 use strict;
 use warnings;
 use Galaxy::IO;
-use Galaxy::IO::FASTA;
+use Galaxy::IO::FASTA qw(FastaReadNextA);
+
+open O,'>','toPCR.p3' or die "Error opening [toPCR.p3]: $!\n";
+print O <<'HEAD';
+PRIMER_MIN_SIZE=19
+PRIMER_MAX_SIZE=25
+PRIMER_THERMODYNAMIC_PARAMETERS_PATH=/usr/local/opt/primer3/share/primer3/primer3_config/
+PRIMER_PRODUCT_SIZE_RANGE=30-401
+PRIMER_NUM_RETURN=1
+=
+HEAD
 
 my $in = openfile('toPCR.fa.gz');
-while (my $ret = FastaReadNext($in)) {
-	my ($seqname,$genome) = @$ret;
-	print $seqname,"\n";
+while (my $ret = FastaReadNextA($in)) {
+	my ($seqname,$genome,$seqdesc) = @$ret;
+	print $seqname," | $seqdesc\n";
+	$seqdesc = (split / /,$seqdesc)[0];
+	$seqdesc = (split /:/,$seqdesc)[1];
+	my ($begin,$end) = split /-/,$seqdesc;
+	my $thePos = (split /_/,$seqname)[1];
+	$begin -= $thePos;
+	$end -= $thePos;
+	print "$thePos, $begin,$end\n";
 }
 
 close $in;
+close O;
+print "Run [primer3_core < toPCR.p3] now.\n";
+__END__
+primer3_core -format_output < toPCR.p3.test
+primer3_core < toPCR.p3.test
--- perl/perlib/etc/Galaxy/IO/FASTA.pm
+++ perl/perlib/etc/Galaxy/IO/FASTA.pm
@@ -4,7 +4,7 @@ use warnings;
 require Exporter;
 our @ISA   =qw(Exporter);
 our @EXPORT    =qw(FastaReadNext);
-our @EXPORT_OK   =qw(FastaReadNext);
+our @EXPORT_OK   =qw(FastaReadNext FastaReadNextA);
 our $VERSION   = v1.0.0;
 
 sub FastaReadNext($) {
@@ -20,6 +20,25 @@ sub FastaReadNext($) {
 		$genome=~s/\s//g;
 		$/="\n";
 		return [$seqname,$genome];
+	}
+	$genome='';
+	return 0;
+}
+
+sub FastaReadNextA($) {
+	my $fh = $_[0];
+	my ($seqname,$genome,$seqdesc);
+	while (<$fh>) {
+		s/^>//;
+		/^(\S+) ?(.*)?/ or next;
+		$seqname = $1;
+		$seqdesc = $2?$2:'';
+		$/=">";
+		$genome=<$fh>;
+		chomp $genome;
+		$genome=~s/\s//g;
+		$/="\n";
+		return [$seqname,$genome,$seqdesc];
 	}
 	$genome='';
 	return 0;
