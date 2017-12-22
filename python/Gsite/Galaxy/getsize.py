5d2a72cd0fcc1d487668a6d4ccc24c6f9b5cd54f
Yuuki Galaxy <galaxy001@gmail.com>
2017-12-22 14:46:03 +0800

    new file:   python/Gsite/Galaxy/getsize.py

---
+++ python/Gsite/Galaxy/getsize.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import sys
+
+# https://goshippo.com/blog/measure-real-size-any-python-object/
+# https://gist.github.com/bosswissam/a369b7a31d9dcab46b4a034be7d263b2#file-pysize-py
+def getsize(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([getsize(i, seen) for i in obj])
+    return size
+'''
+aa = [[],['a'],['a','b'],[1], [123], [123,456]]
+for a in aa:
+    print("%s size=%d" %(a, getsize(a)))
+
+#output
+[] size=64
+['a'] size=122
+['a', 'b'] size=180
+[1] size=100
+[123] size=100
+[123, 456] size=136
+'''
+
+# https://code.activestate.com/recipes/577504/
+#from __future__ import print_function
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+'''
+##### Example call #####
+
+if __name__ == '__main__':
+    d = dict(a=1, b=2, c=3, d=[4,5,6,7], e='a string of chars')
+    print(total_size(d, verbose=True))
+'''
+
+if __name__ == '__main__':
+    print('Use `getsize` or `asizeof` from "pympler".\n')
+    aa = [[],['a'],['a','b'],[1], [123], [123,456]]
+    for a in aa:
+        print("%s size=%d" %(a, getsize(a)))
+    print('------\nAnother example for old version of asizeof:\n')
+    d = dict(a=1, b=2, c=3, d=[4,5,6,7], e='a string of chars')
+    print(total_size(d, verbose=True))
