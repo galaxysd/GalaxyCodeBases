47da5986e48c4f08bc56c600edde2011b2ea2900
Yuuki Galaxy <galaxy001@gmail.com>
2018-07-05 10:05:12 +0800

    modified:   perl/etc/justonce/tmp/microSNPdiff.pl

--- perl/etc/justonce/tmp/microSNPdiff.pl
+++ perl/etc/justonce/tmp/microSNPdiff.pl
@@ -9,6 +9,8 @@ my $Out1 = 'micro.consisted.tsv';
 my $Out2 = 'micro.same.tsv';
 my $Out3 = 'micro.GT.tsv';
 my $Out4 = 'micro.diff.tsv';
+my $Out5 = 'micro.Tsame.tsv';
+my $Out6 = 'micro.Tdiff.tsv';
 
 my (%Samples,@Samples,@UIDs,%ID2Sample,%SampleCnts);
 open I,'<',$SampleList or die $?;
@@ -23,7 +25,7 @@ while (<I>) {
 close I;
 @UIDs = sort keys %Samples;
 
-my (%SameCntSum,%SameGTdat,%SameCnt);
+my (%SameCntSum,%SameGTdat,%SameCnt,%TopLoc);
 open O,'>',$Out1 or die $?;
 open P,'>',$Out3 or die $?;
 print O join("\t",'Chr','Pos','Consisted',@UIDs),"\n";
@@ -58,6 +60,9 @@ while (<I>) {
 	++$SameCntSum{$SameCnt{$Loc}{'\t'}};
 	print O join("\t",$Chr,$Pos,$SameCnt{$Loc}{'\t'},(map {$SameCnt{$Loc}{$_}} @UIDs)),"\n";
 	print P join("\t",$Chr,$Pos,$SameCnt{$Loc}{'\t'},(map {$SameGTdat{$Loc}{$_}} @UIDs)),"\n";
+	if ($SameCnt{$Loc}{'\t'} == scalar @UIDs) {
+		++$TopLoc{$Loc};
+	}
 }
 close I;
 close P;
@@ -71,15 +76,19 @@ close O;
 #my %SameDiffSum;
 open O,'>',$Out2 or die $?;
 open E,'>',$Out4 or die $?;
+open X,'>',$Out5 or die $?;
+open Y,'>',$Out6 or die $?;
 print O join("\t",'=',@UIDs),"\n";
 print E join("\t",'=',@UIDs),"\n";
 for my $i (0 .. $#UIDs) {
 	my $uidi = $UIDs[$i];
 	print O $uidi;
 	print E $uidi;
+	print X $uidi;
+	print Y $uidi;
 	for my $j (0 .. $i) {
 		my $uidj = $UIDs[$j];
-		my ($cntij,$cntijE) = (0,0);
+		my ($cntij,$cntijE,$Tcntij,$TcntijE) = (0,0,0,0);
 		for my $Loc (keys %SameGTdat) {
 			my $GTi = $SameGTdat{$Loc}{$uidi};
 			my $GTj = $SameGTdat{$Loc}{$uidj};
@@ -91,17 +100,30 @@ for my $i (0 .. $#UIDs) {
 			} else {
 				++$cntijE;
 			}
+			if ($TopLoc{$Loc}) {
+				if ($GTi eq $GTj) {
+					++$Tcntij;
+				} else {
+					++$TcntijE;
+				}
+			}
 		}
 		#++$SameDiffSum{$cntij};
 		print O "\t$cntij";
 		print E "\t$cntijE";
+		print X "\t$Tcntij";
+		print Y "\t$TcntijE";
 	}
 	print O "\n";
 	print E "\n";
+	print X "\n";
+	print Y "\n";
 }
 
 close O;
 close E;
+close X;
+close Y;
 
 #ddx \%SameDiffSum;
 __END__
