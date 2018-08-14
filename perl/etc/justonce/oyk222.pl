1ac1de2d76478e9eb51087de2a717205c3dd473b
Yuuki Galaxy <galaxy001@gmail.com>
2018-08-14 14:51:47 +0800

    new file:   perl/etc/justonce/oyk222.pl

---
+++ perl/etc/justonce/oyk222.pl
@@ -0,0 +1,164 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use lib '.';
+
+use Data::Dump qw(ddx);
+use Text::NSP::Measures::2D::Fisher::twotailed;
+use FGI::GetCPI;
+#use Math::BigFloat;
+
+our @Bases;
+sub deBayes($) {
+	my $p = $_[0];
+	my %Dep;
+	for my $i (1 .. $#$p) {
+		$Dep{$i-1} = $p->[$i];
+	}
+	my @dKeys = sort { $Dep{$b} <=> $Dep{$a} } keys %Dep;
+	if ( $Dep{$dKeys[1]} * 49 > $Dep{$dKeys[0]} ) {	# 2%
+		my @rKeys = sort {$a<=>$b} @dKeys[0,1];
+		my $gt = join('/',$Bases[$rKeys[0]],$Bases[$rKeys[1]]);
+		$p->[0] = $gt;
+	}
+}
+
+sub getBolsheviks(@) {
+	my @dat = map { [split /[;,]/,$_] } @_;
+	deBayes($_) for @dat;
+	#ddx \@dat;
+
+	my (%GT);
+	for (@dat) {
+		++$GT{$_->[0]};
+	}
+	my $Bolsheviks = (sort {$GT{$b} <=> $GT{$a}} keys %GT)[0];
+	my @GTdep;
+	for (@dat) {
+		next if $_->[0] ne $Bolsheviks;
+		for my $i (1 .. $#$_) {
+			$GTdep[$i-1] += $_->[$i];
+		}
+	}
+	my @GTs = split /[\/|]/,$Bolsheviks;
+	my $Hom = 0;
+	$Hom = 1 if $GTs[0] eq $GTs[1];
+	#ddx $Bolsheviks,$Hom,\@GTdep,@dat if (keys %GT)>1;
+	return [$Bolsheviks,$Hom,\@GTdep];
+}
+
+sub getequal(@) {
+        my @dat = map { [split /[;,]/,$_] } @_;
+#ddx \@dat;
+        deBayes($_) for @dat;
+        my (%GT);
+        for (@dat) {
+                ++$GT{$_->[0]};
+        }
+	for (values %GT) {
+		return 1 if $_ == 2;
+	}
+	return 0;
+        #ddx $Bolsheviks,$Hom,\@GTdep,@dat if (keys %GT)>1;
+        #        return [$Bolsheviks,$Hom,\@GTdep];
+        #        }
+        #
+}
+
+my $mother=shift;
+my $father=shift;
+my $child=shift;
+open FM,'<',$mother or die "[x]Mom: $!\n";
+open FF,'<',$father or die "[x]Dad: $!\n";
+open FC,'<',$child or die "[x]Child: $!\n";
+
+my ($logcpi,$lFC,$lFF,$lFM)=(0);
+print "# Order: M,F,C\n";
+
+while (<FM>) {
+	chomp;
+	chomp($lFC = <FC>);
+	chomp($lFF = <FF>);
+	my @datM = split /\t/;
+	my @datF = split /\t/,$lFF;
+	my @datC = split /\t/,$lFC;
+	#my ($chr,undef,$bases,$qual,@data) = split /\t/;
+	next if $datM[3] < 100;
+	next if $datF[3] < 100;
+	next if $datC[3] < 100;
+	die if $datM[0] ne $datC[0];
+	my @tM = splice @datM,4;
+	my @tF = splice @datF,4;
+	my @tC = splice @datC,4;
+	@Bases = split /,/,$datM[2];
+	next if "@tM @tF @tC" =~ /\./;
+	#print "@tM\n@datM\n";
+	my $retM = getBolsheviks(@tM);
+	next unless $retM->[1];
+	my $retF = getBolsheviks(@tF);
+	#ddx $retM,$retF;
+	my $xx = getequal(@tM);
+	my $yy = getequal(@tF);
+	my $zz = getequal (@tC);
+	my $t=$xx*$yy*$zz;
+	next unless $t;
+	my @sdatC = map { [split /[;,]/,$_] } @tC;
+	my @GTdepC;
+	for (@sdatC) {
+		for my $i (1 .. $#$_) {
+			$GTdepC[$i-1] += $_->[$i];
+		}
+	}
+	my %CntM;
+	my @GTM = @{$retM->[2]};
+	for my $i (0 .. $#GTM) {
+		$CntM{$i} = $GTM[$i];
+	}
+	my ($x,$y) = sort { $CntM{$b} <=> $CntM{$a} } keys %CntM;
+	#ddx [$x,$y,$Bases[$x],$Bases[$y]],\@sdatC,\@GTdepC;
+	my $n11 = $retM->[2]->[$x];
+	my $n12 = $retM->[2]->[$y];
+	my $n21 = $GTdepC[$x];
+	my $n22 = $GTdepC[$y];
+	next if ($n21+$n22) < 1000;	# skip
+	my $GTtC;
+	my $twotailedFisher = -1;
+	$GTtC = join('/',$Bases[$x],$Bases[$x]);
+	my $Cdep = $n21 + $n22;
+	#if ($n22 * 199 < $n21) {	# <0.5% = 1:200
+	if ($n22/$Cdep < 0.02) {	# minnor < 2%, skip ; depth<10
+		next;	# skip
+	} else {
+		my $n1p = $n11 + $n12;
+		my $np1 = $n11 + $n21;
+		my $npp = $n1p + $n21 + $n22;
+		$twotailedFisher = calculateStatistic(
+			n11 => $n11,
+			n1p => $n1p,
+			np1 => $np1,
+			npp => $npp,
+		);
+		if( (my $errorCode = getErrorCode()) ) {
+			die $errorCode, " - ", getErrorMessage();
+		} else {
+			my ($m,$n) = sort {$a<=>$b} ($x,$y);
+			$GTtC = join('/',$Bases[$m],$Bases[$n]);# if $twotailedFisher < 0.05 or $n22 * 49 >= $n21;	# (f0.05 and 0.5%~2%) or >2%
+		}
+	}
+	my $retC = getBolsheviks(@tC);
+	my $resM = join(';',$retM->[0],join(',',@{$retM->[2]}));
+	my $resF = join(';',$retF->[0],join(',',@{$retF->[2]}));
+	my $resC = join(';',$GTtC,join(',',@GTdepC),$twotailedFisher,
+						$retC->[0],join(',',@{$retC->[2]})
+					);
+	my $cret = getcpi(@datM,$resM,$resF,$resC);
+	#ddx $cret;
+	$logcpi += log($cret->[0]);
+	print join("\t",@datM,$resM,$resF,$resC,@$cret,$logcpi/log(10)),"\n";
+}
+
+close FM; close FF; close FC;
+
+print "# CPI: ",exp($logcpi),"\n";
+
