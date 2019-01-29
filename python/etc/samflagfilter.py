b274ef2d9b27664dd74c131704463707acd0c09f
Yuuki Galaxy <galaxy001@gmail.com>
2019-01-29 19:19:26 +0800

    modified:   samflagfilter.py

--- python/etc/samflagfilter.py
+++ python/etc/samflagfilter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pprint
 
 def main():
     import sys
@@ -9,6 +10,18 @@ def main():
         print('[x]No arguments given!',file=sys.stderr,flush=True)
         exit(0)
     print('Reading',sys.argv[1])
+    inBAMname = sys.argv[1]
+    import pysam
+    samfile = pysam.AlignmentFile(inBAMname, "rb")
+    pairedreads = pysam.AlignmentFile("allpaired.bam", "wb", template=samfile)
+    for read in samfile:
+         if read.is_paired:
+                 #pairedreads.write(read)
+                 print (str(read))
+                 pprint.pprint(read)
+
+    pairedreads.close()
+    samfile.close()
 
 if __name__ == "__main__":
     main()
