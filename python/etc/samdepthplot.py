492aefd3ca1e03e0e831f62bb291727a1714eaef
Yuuki Galaxy <galaxy001@gmail.com>
2019-02-01 16:19:52 +0800

    modified:   samdepthplot.py

--- python/etc/samdepthplot.py
+++ python/etc/samdepthplot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env pypy3
 import sys
 
-SamplesList = ('D3B-Crick', 'D3B-Watson', 'Normal-Crick', 'Normal-Watson', 'D3B-WGS', 'Normal-WGS')
+SamplesList = ('D3B_Crick', 'D3B_Watson', 'Normal_Crick', 'Normal_Watson', 'D3B_WGS', 'Normal_WGS')
 from collections import defaultdict
 DepthCnt = dict.fromkeys(SamplesList, defaultdict(int))
+#nDepthCnt = defaultdict(lambda: defaultdict(int))
 
 def main():
     if len(sys.argv) < 3 :
@@ -28,15 +29,28 @@ def inStat(inDepthFile,verbose):
         tsvin = csv.DictReader(tsvin, delimiter='\t', fieldnames=('ChrID','Pos')+SamplesList )
         headers = tsvin.fieldnames
         print(headers)
-        for row in tsvin:
-            #print(', '.join(row[col] for col in SamplesList))
-            RecordCnt += 1
-            for k in SamplesList:
-                if int(row[k]) > MaxDepth:
-                    MaxDepth = int(row[k])
-                DepthCnt[k][int(row[k])] += 1
-            #print(MaxDepth,DepthCnt['Normal-WGS'])
+        try:
+            for row in tsvin:
+                #print(', '.join(row[col] for col in SamplesList))
+                RecordCnt += 1
+                for k in SamplesList:
+                    if int(row[k]) > MaxDepth:
+                        MaxDepth = int(row[k])
+                    DepthCnt[k][int(row[k])] += 1
+                    #nDepthCnt[int(row[k])][k] += 1
+                #print(MaxDepth,DepthCnt)
+        except KeyboardInterrupt:
+            print('\n[!]Ctrl+C pressed.',file=sys.stderr,flush=True)
+            pass
+        print('[!]Lines Read:[{}], MaxDepth is [{}].'.format(RecordCnt,MaxDepth),file=sys.stderr,flush=True)
 
 
 if __name__ == "__main__":
-    main()
+    main()  # time python3 ./samdepthplot.py t.tsv.gz 1
+    # get_size only works under Python, not PyPy.
+    mylibfile = "../lib/pysize/pysize.py"
+    with open(mylibfile) as f:
+        code = compile(f.read(), mylibfile, 'exec')
+        exec(code, globals(), locals())
+    print(get_size(DepthCnt))   # smaller
+    #print(get_size(nDepthCnt))
