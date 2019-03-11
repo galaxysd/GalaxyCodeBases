6742bdd5043f435552d48a52428765dfd419b220
Yuuki Galaxy <galaxy001@gmail.com>
2019-03-11 18:51:11 +0800

    modified:   samdepthstat.pool.py

--- python/etc/samdepthstat.pool.py
+++ python/etc/samdepthstat.pool.py
@@ -30,6 +30,9 @@ def main():
     outFile = sys.argv[2]
     print('From:[{}], To:[{}].\nVerbose: [{}].'.format(inDepthFile,outFile,verbose),file=sys.stderr,flush=True)
     RecordCnt,MaxDepth,cDepthCnt,cDepthStat = CallStat(inDepthFile)
+    if RecordCnt == 0:
+        RecordCnt = 1
+        print('[!]Empty Record !',file=sys.stderr,flush=True)
     for k in SamplesList:
         cDepthStat[k][2] = cDepthStat[k][0] / RecordCnt # E(X)
         cDepthStat[k][3] = cDepthStat[k][1] / RecordCnt # E(X^2)
@@ -50,6 +53,7 @@ def main():
 def CallStat(inDepthFile):
     import gzip
     import itertools
+    import functools
     RecordCnt = 0
     MaxDepth = 0
     cDepthCnt = {key:Counter() for key in SamplesList}
@@ -58,10 +62,16 @@ def CallStat(inDepthFile):
     manager = Manager()
     lines_queue = manager.Queue()
     stater_pool = Pool(Nworkers)
-    TASKS = itertools.repeat((lines_queue,SamplesList),Nworkers)
+    #TASKS = itertools.repeat((lines_queue,SamplesList),Nworkers)
+    QUEUES = itertools.repeat(lines_queue,Nworkers)
+    myStator = functools.partial(iStator,inSamplesList=SamplesList)
+    #fStator = functools.partial(iStator,inQueue=lines_queue,inSamplesList=SamplesList)
     #ApplyResult = [stater_pool.apply_async(iStator,x) for x in TASKS]
     #MapResult = stater_pool.map_async(iStator,TASKS,1)
-    AsyncResult = stater_pool.imap_unordered(iStator,TASKS,1)
+    #AsyncResult = stater_pool.imap_unordered(iStator,TASKS,1)
+    #MapResult = stater_pool.map_async(myStator,QUEUES,1)
+    AsyncResult = stater_pool.imap_unordered(myStator,QUEUES,1)
+    #ApplyResult = [stater_pool.apply_async(fStator) for x in range(Nworkers)]
     try:
         with gzip.open(inDepthFile, 'rt') as tsvfin:
             while True:
@@ -70,11 +80,13 @@ def CallStat(inDepthFile):
                 if not lines:
                     for i in range(Nworkers):
                         lines_queue.put(b'\n\n')
+                    stater_pool.close()
                     break
     except KeyboardInterrupt:
         print('\n[!]Ctrl+C pressed.',file=sys.stderr,flush=True)
         for i in range(Nworkers):
             lines_queue.put(b'\n\n')
+        stater_pool.close()
         pass
     #for results in ApplyResult:
         #(iRecordCnt,iMaxDepth,icDepthCnt,icDepthStat) = results.get()
@@ -89,9 +101,9 @@ def CallStat(inDepthFile):
             cDepthStat[k][1] += icDepthStat[k][1]
     return RecordCnt,MaxDepth,cDepthCnt,cDepthStat
 
-#def iStator(inQueue,inSamplesList):
-def iStator(args):
-    (inQueue,inSamplesList) = args
+def iStator(inQueue,inSamplesList):
+#def iStator(args):
+    #(inQueue,inSamplesList) = args
     import csv
     # Looking up things in global scope takes longer then looking up stuff in local scope. <https://stackoverflow.com/a/54645851/159695>
     cDepthCnt = {key:Counter() for key in inSamplesList}
