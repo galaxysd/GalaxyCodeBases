79144e132c1751cce7a1aaf1c97d2a66eaa95fc7
Yuuki Galaxy <galaxy001@gmail.com>
2019-03-12 15:22:00 +0800

    modified:   primer.pl

--- perl/etc/justonce/primer.pl
+++ perl/etc/justonce/primer.pl
@@ -13,8 +13,8 @@ print O <<'HEAD';
 PRIMER_MIN_SIZE=19
 PRIMER_MAX_SIZE=25
 PRIMER_THERMODYNAMIC_PARAMETERS_PATH=/usr/local/opt/primer3/share/primer3/primer3_config/
-PRIMER_PRODUCT_SIZE_RANGE=30-221
-PRIMER_NUM_RETURN=3
+PRIMER_PRODUCT_SIZE_RANGE=300-500
+PRIMER_NUM_RETURN=1
 =
 HEAD
 
@@ -23,15 +23,15 @@ while (my $ret = FastaReadNextA($in)) {
 	my ($seqname,$genome,$seqdesc) = @$ret;
 	#print $seqname," | $seqdesc\n";
 	my ($chr,$begin,$end) = split /[:-]/,$seqname;
-	my $thePos = $begin +150;
+	my $thePos = $begin +300;
 	my $seqlen = length $genome;
 	#print "--- $chr,$begin,$end\n";
 	print O <<"	ITEM";
 SEQUENCE_ID=${seqname}_$thePos
 SEQUENCE_TEMPLATE=$genome
-SEQUENCE_TARGET=151,1
-SEQUENCE_INTERNAL_EXCLUDED_REGION=151,1
-SEQUENCE_PRIMER_PAIR_OK_REGION_LIST=101,50,, ; ,,151,50
+SEQUENCE_TARGET=301,1
+SEQUENCE_INTERNAL_EXCLUDED_REGION=301,1
+SEQUENCE_PRIMER_PAIR_OK_REGION_LIST=251,50,, ; ,,301,50
 =
 	ITEM
 }
@@ -44,3 +44,6 @@ primer3_core -format_output < toPCR.p3.test
 primer3_core < toPCR.p3.test
 
 其中一端的引物距离目标位点少于50bp
+
+perl -lane 'print "$F[0]:",$F[1]-300,"-",$F[1]+300' uGRCh37SNPs.txt >uGRCh37SNPs.reg
+samtools faidx Homo_sapiens_assembly19.fasta -r uGRCh37SNPs.reg >u.fa &
