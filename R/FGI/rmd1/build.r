8ff2ce9d79cfbdb456828f4c1b00eb6abf4a6d7c
Yuuki Galaxy <galaxy001@gmail.com>
2020-06-08 15:17:19 +0800

    new file:   FGI/rmd1/build.r

---
+++ R/FGI/rmd1/build.r
@@ -0,0 +1,7 @@
+#!/usr/bin/env littler
+#
+# For macOS, `ln -s /usr/local/lib/R/site-library/littler/bin/r /usr/local/bin/littler`
+# For Linux, `sudo ln -s /usr/lib/R/site-library/littler/bin/r /usr/local/bin/littler`
+
+rmarkdown::render('20200517rep.Rmd',output_format='all',output_dir='./out')
+rmarkdown::render('20200608rep.Rmd',output_format='all',output_dir='./out')
