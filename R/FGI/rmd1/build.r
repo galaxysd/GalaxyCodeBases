a70509ca329b57cf4411d202ef6e3547e470f6ef
Yuuki Galaxy <galaxy001@gmail.com>
2020-06-09 13:55:27 +0800

    modified:   20200608rep.Rmd

--- R/FGI/rmd1/build.r
+++ R/FGI/rmd1/build.r
@@ -3,5 +3,5 @@
 # For macOS, `ln -s /usr/local/lib/R/site-library/littler/bin/r /usr/local/bin/littler`
 # For Linux, `sudo ln -s /usr/lib/R/site-library/littler/bin/r /usr/local/bin/littler`
 
-rmarkdown::render('20200517rep.Rmd',output_format='all',output_dir='./out')
+#rmarkdown::render('20200517rep.Rmd',output_format='all',output_dir='./out')
 rmarkdown::render('20200608rep.Rmd',output_format='all',output_dir='./out')
