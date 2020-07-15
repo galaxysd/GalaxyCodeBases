686cc36645e0bf65a36d26eef870620698c003ec
Yuuki Galaxy <galaxy001@gmail.com>
2020-07-15 19:28:17 +0800

    modified:   vcf2tped.pl

--- python/mixture/vcf2tped.pl
+++ python/mixture/vcf2tped.pl
@@ -18,8 +18,13 @@ while(<SID>) {
 close SID;
 ddx \@SampleIDs;
 
+my $theKiller = 'T3C';
+my $theVictim = 'T10C';
+my @addedSID = qw(Rsin Rdip);
+my @AllSID = (@SampleIDs,@addedSID);
+
 open O,'>',"$outp.tfam" or die "[$outp.tfam]:$!\n";
-for (@SampleIDs) {
+for (@AllSID) {
 	my $FID = 'T';
 	my $IID = $_;
 	print O join(' ',$FID,$IID,0,0,0,0),"\n";
@@ -27,7 +32,58 @@ for (@SampleIDs) {
 }
 close O;
 
+my %COUNTING;
+sub doCnt($) {
+	my $s = $_[0];
+	++$COUNTING{$s};
+	print " $s";
+}
+sub mergeGT($) {
+	my $hashref = $_[0];
+	my @GTs = sort keys %{$hashref};
+	if (scalar @GTs ==1) {
+		push @GTs,@GTs;
+	}
+	return join(' ',@GTs);
+}
 open O,'>',"$outp.tped" or die "[$outp.tped]:$!\n";
-
+open(IN,"-|",$cmd) or die "Error opening [$filename]: $!\n";
+while (<IN>) {
+	chomp;
+	my ($Chrom,$Pos,$RefAlt,$Qual,@GTs) = split /\t/,$_;
+	next if $Chrom =~ /_/;
+	print "$_\n";
+	my (%GTs,%GTstr,%GTped);
+	@GTstr{@SampleIDs} = @GTs;
+	#ddx \%GTstr;
+	for my $k (keys %GTstr) {
+		my ($sGT,$sAD,$sGQ) = split /:/,$GTstr{$k};
+		my @aGT = split /[|\/]/,$sGT;
+		my @aAD = split /\,/,$sAD;
+		my %counter;
+		for (@aGT) {
+			++$counter{$_};
+		}
+		#my @result = sort(keys %counter);
+		$GTs{$k} = \%counter;
+	}
+	$GTs{$addedSID[0]} = {%{$GTs{'mixed'}}};
+	$GTs{$addedSID[1]} = {%{$GTs{'mixed'}}};
+	for (keys %{$GTs{$theVictim}}) {
+		if (exists $GTs{$addedSID[0]}{$_}) {
+			delete $GTs{$addedSID[0]}{$_};
+		}
+	}
+	for my $k (keys %GTs) {
+		$GTped{$k} = mergeGT($GTs{$k});
+	}
+	if (length($GTped{$addedSID[0]})==0) {
+		doCnt('0same');
+		next;
+	}
+	ddx \%GTs;
+	ddx \%GTped;
+}
+close IN;
 # https://www.cog-genomics.org/plink/1.9/formats#tped
 close O;
