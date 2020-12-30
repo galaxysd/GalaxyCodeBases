53000b7a608004976cdfcb962485c32bf95c5315
Yuuki Galaxy <galaxy001@gmail.com>
2020-12-30 10:48:11 +0800

    modified:   fstr.pl

--- perl/etc/justonce/fm2/pipe/fstr.pl
+++ perl/etc/justonce/fm2/pipe/fstr.pl
@@ -6,9 +6,10 @@ use warnings;
 use Data::Dump qw(ddx);
 
 my $s1HomoLevel = 0.8;
-my $s2HomoLevel = 0.3;
-my $s2HeteLevel = 0.2;
-mu $minAllelDepth = 100;
+my $s2HomoLevela = 0.75;
+my $s2HeteLevela = 0.3;
+my $s2HeteLevelb = 0.2;
+my $minAllelDepth = 100;
 
 my %StrDat;
 while(<>) {
@@ -18,5 +19,31 @@ while(<>) {
 	#print join("\t",$strID,$strAllele,$depth),"\n";
 	$StrDat{$strID}{$strAllele} = $depth;
 }
+#ddx \%StrDat;
 
-ddx \%StrDat;
+sub SumArryItr { my $agg = 0; $agg += $_ for @_;  return $agg }
+
+for my $k (keys %StrDat) {
+	my %oneSTR = %{$StrDat{$k}};
+	#ddx \%oneSTR;
+	my @Allels = sort {$oneSTR{$b} <=> $oneSTR{$a}} keys %oneSTR;
+	#ddx \@Allels;
+	my $Sum = SumArryItr(values %oneSTR);
+	#print $Sum,"\n---\n";
+	my @GT;
+	if ($oneSTR{$Allels[0]} <= 100) {
+		@GT = ('NA');
+	} elsif ($oneSTR{$Allels[0]} >= $Sum * $s1HomoLevel ) {
+		@GT = ($Allels[0]);
+	} elsif ( ($oneSTR{$Allels[0]} >= $Sum * $s2HomoLevela) and ($oneSTR{$Allels[1]} < $Sum * $s2HeteLevelb) ) {
+		@GT = ($Allels[0]);
+	} elsif ( ($oneSTR{$Allels[0]} >= $Sum * $s2HeteLevela) and ($oneSTR{$Allels[1]} >= $Sum * $s2HeteLevelb) ) {
+		@GT = ($Allels[0],$Allels[1]);
+	} else {
+		@GT = ('Unknown');
+	}
+	ddx \@GT;
+}
+
+
+
