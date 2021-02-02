9b18b824f909805dc8348203ce1e6b54484ff5c7
Yuuki Galaxy <galaxy001@gmail.com>
2021-02-02 16:47:28 +0800

    modified:   cutsnp.pl

--- perl/etc/justonce/tmp/cutsnp.pl
+++ perl/etc/justonce/tmp/cutsnp.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Text::CSV;
-use Data::Dump qw(ddx);
+#use Text::CSV;
+#use Data::Dump qw(ddx);
 
 my $dbSNPa = 'snp151.lst.h';
 
@@ -15,7 +15,7 @@ while(<IN>) {
 	$rsIN{$rsid} = [$chr,$Ref,$Alt,$pGlobal,$pAsia];
 }
 close IN; 
-ddx \%rsIN;
+#ddx \%rsIN;
 
 my %rsDB;
 open R,'<',$dbSNPa or die "Error opening [$dbSNPa]: $!\n";
@@ -24,8 +24,20 @@ while(<R>) {
 	my ($chr,$pos,$rsid,$ref,$tgt) = split /\s+/;
 	if (exists $rsIN{$rsid}) {
 		my @dat = @{$rsIN{$rsid}};
-		$rsDB{$rsid} = [$chr,$pos,$ref,$tgt,@dat[-2,-1]];
+		pop @dat unless defined $dat[-1];
+		if (exists $rsDB{$rsid}) {
+			push @{$rsDB{$rsid}},[$chr,$pos,$ref,$tgt,@dat[-2,-1]];
+		} else {
+			$rsDB{$rsid} = [[$chr,$pos,$ref,$tgt,@dat[-2,-1]]];
+		}
 	}
 }
 close R;
-ddx \%rsDB;
+#ddx \%rsDB;
+
+for my $rsid (sort keys %rsDB) {
+	my @dat = @{$rsDB{$rsid}};
+	for my $n (0 .. $#dat) {
+		print join("\t",$rsid,$n,@{$dat[$n]}),"\n";
+	}
+}
