03f877aa28a01cc63499ff58b26404239cc5dbe4
Yuuki Galaxy <galaxy001@gmail.com>
2021-03-23 17:56:08 +0800

    Fix `FASTQC_DIR`. 	modified:   QC/tasks/fastqc.wdl

--- etc/gatk-wdl/fm2gatk/_edit.sh
+++ etc/gatk-wdl/fm2gatk/_edit.sh
@@ -3,3 +3,5 @@
 find . -type f -name '*.wdl' -exec grep -H 'docker: dockerImage' {} \;
 find . -type f -name '*.wdl' -exec sed -i -e 's/docker: dockerImage/#docker: dockerImage/g' {} \;
 
+#pip3 install biowdl-input-converter
+#pip3 install chunked-scatter
