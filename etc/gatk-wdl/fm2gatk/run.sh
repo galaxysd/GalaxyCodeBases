de4177b01cc29d28ae12efa266f0fd3d62fb8f55
Yuuki Galaxy <galaxy001@gmail.com>
2021-03-23 16:34:26 +0800

    new file:   fm2.json

---
+++ etc/gatk-wdl/fm2gatk/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# biowdl-input-converter fm2.csv >fm2.samples.json
+
+echo Begin: `date`
+
+JAVA_OPTS="-Dbackend.providers.Local.config.concurrent-job-limit=16" cromwell run -o cromwell_options_no_user.json -i fm2.json germline.wdl >fm2.log
+
+echo End: `date`
