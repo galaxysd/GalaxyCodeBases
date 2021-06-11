fcb165d96dd0f9c054a229579b591280c000437f
Yuuki Galaxy <galaxy001@gmail.com>
2021-06-11 16:17:26 +0800

    modified:   bplot.R

--- R/FGI/fm2/bplot.R
+++ R/FGI/fm2/bplot.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+#library(Cairo)
 
-if (1) {
-	indat <- read.delim('e5.snp.txt')
+if (0) {
+	indat <- read.delim('e5.snp.txt', encoding="UTF-8")
 	for (ncol in 1:nrow(indat)) {
-		for (nrow in 3:ncol(indat)) {
+		for (nrow in 5:ncol(indat)) {
 			tmp <- unlist(strsplit(indat[ncol,nrow],','))
 			indat[ncol,nrow] <- tmp[length(tmp)]
 		}
@@ -11,12 +12,12 @@ if (1) {
 	write.csv(indat,'e5.depth.csv')
 }
 
-snpdat <- read.csv('e5.depth.csv')
+snpdat <- read.csv('e5.depth.csv', encoding="UTF-8")
 plotdat <- pivot_longer(snpdat, cols = starts_with("rs"), names_to = "SNPs",values_to = "Depth")
 
 PlotPointsize <- 12
-pdf('boxType.pdf', pointsize = PlotPointsize)
-ggplot(plotdat, aes(Type, Depth)) + geom_boxplot(colour = rainbow(length(unique(plotdat$Type))))
+cairo_pdf('boxType.pdf', pointsize = PlotPointsize)	# Not working on macOS, either cairo_pdf or CairoPDF.
+ggplot(plotdat, aes(TypeCN, Depth)) + geom_boxplot(colour = rainbow(length(unique(plotdat$Type))))
 dev.off()
 
 pdf('boxSample.pdf', pointsize = PlotPointsize, width=32, height=16)
