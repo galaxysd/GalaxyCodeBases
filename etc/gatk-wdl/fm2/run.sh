4b99cf06ec191b502bbf373bfa6eeddfee89b0db
Yuuki Galaxy <galaxy001@gmail.com>
2021-06-15 10:29:48 +0800

    modified:   bin/snplst.txt

--- etc/gatk-wdl/fm2/run.sh
+++ etc/gatk-wdl/fm2/run.sh
@@ -3,6 +3,6 @@
 
 echo Begin: `date`
 
-JAVA_OPTS="-Dbackend.providers.Local.config.concurrent-job-limit=16" cromwell run -o cromwell_options_no_user.json -i fm2.json fm2.wdl >fm2.log
+JAVA_OPTS="-Dbackend.providers.Local.config.concurrent-job-limit=16" cromwell run -o cromwell_options_no_user.json -i fm2wg.json fm2.wdl >fm2.log
 
 echo End: `date`
