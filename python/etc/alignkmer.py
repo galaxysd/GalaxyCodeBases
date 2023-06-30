687e2b5773ade2c93ecb6e50a096ac761238628b
Yuuki Galaxy <galaxy001@gmail.com>
2023-06-30 17:26:23 +0800

    modified:   alignkmer.py

--- python/etc/alignkmer.py
+++ python/etc/alignkmer.py
@@ -1,47 +1,67 @@
 #!/usr/bin/env python3
 
-from skbio.alignment import StripedSmithWaterman
-from skbio import DNA
+import sys
 
-Adapters = {
-    'adapter1': 'CAAGCAGAAGACGGCATACGAGATTCTTTCCCTACACGACGCTCTTCCGATCT',
-    'adapter2': 'CCCGTTCGCAACATGTCTGGCGTCATATCTTGTGACTACAGCACCCTCGACTCTCGCAGACTTTCACCAGTCCATGAT',
-    'adapter3': 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCACGTGTAGATCTCGGTGGTCGCCGTATCATT'
-}
+def buildQuery():
+    from skbio import DNA
+    from skbio.alignment import StripedSmithWaterman
+    Adapters = {
+        'adapter1': 'CAAGCAGAAGACGGCATACGAGATTCTTTCCCTACACGACGCTCTTCCGATCT',
+        'adapter2': 'CCCGTTCGCAACATGTCTGGCGTCATATCTTGTGACTACAGCACCCTCGACTCTCGCAGACTTTCACCAGTCCATGAT',
+        'adapter3': 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCACGTGTAGATCTCGGTGGTCGCCGTATCATT'
+    }
+    Querys={}
+    for qid in Adapters.keys():
+        Seq = Adapters[qid]
+        rcSeq = str(DNA(Seq).reverse_complement())
+        Query = StripedSmithWaterman(Seq)
+        rcQuery = StripedSmithWaterman(rcSeq)
+        Querys[qid] = (Query,rcQuery)
+    return Querys
 
-Seq = 'ACATAGTCTGGCGTCATTCTTGTGTACA'
-Query = StripedSmithWaterman(Seq)
+def aliOneSeq(QuerySSWs,targetSeq):
+    Alignments = {}
+    Scores = {}
+    for qid in QuerySSWs.keys():
+        (Query,rcQuery) = QuerySSWs[qid]
+        Alignment = Query(targetSeq)
+        rcAlignment = rcQuery(targetSeq)
+        strand = '*'
+        if rcAlignment == None or Alignment.optimal_alignment_score > rcAlignment.optimal_alignment_score:
+            resAli = Alignment
+            strand = '+'
+        else:
+            resAli = rcAlignment
+            strand = '-'
+        Alignments[qid] = (resAli,strand)
+        Scores[qid] = resAli.optimal_alignment_score
+    maxScores = [key for key, value in Scores.items() if value == max(Scores.values())]
+    maxAlignments = {k: Alignments[k] for k in maxScores}
+    return maxAlignments
 
-rcSeq = str(DNA(Seq).reverse_complement())
-rcQuery = StripedSmithWaterman(rcSeq)
+def main():
+    if len(sys.argv) < 3 :
+        print('Usage:',sys.argv[0],'<kmc_dump.output_file> <min count>',file=sys.stderr,flush=True);
+        exit(0);
 
-Alignments = {}
-Scores = {}
-for tid in Adapters.keys():
-    target_sequence = Adapters[tid]
-    Alignment = Query(target_sequence)
-    rcAlignment = rcQuery(target_sequence)
-    strand = '*'
-    if rcAlignment == None or Alignment.optimal_alignment_score > rcAlignment.optimal_alignment_score:
-        resAli = Alignment
-        strand = '+'
-    else:
-        resAli = rcAlignment
-        strand = '-'
-    Alignments[tid] = (resAli,strand)
-    Scores[tid] = resAli.optimal_alignment_score
-maxScores = [key for key, value in Scores.items() if value == max(Scores.values())]
+    QuerySSWs = buildQuery()
+    targetSeq = 'ACATAGTCTGGCGTCATTCTTGTGTACA'
+    maxAlignments = aliOneSeq(QuerySSWs,targetSeq)
 
-for tid in maxScores:
-    ali = Alignments[tid]
-    prnStr = ' '.join((
-        tid, ali[1], ali[0].cigar,
-        str(ali[0].optimal_alignment_score),
-        str(ali[0].suboptimal_alignment_score)
-    ))
-    print(prnStr)
-    prnStr = ' -> '.join((
-        ali[0].aligned_query_sequence,ali[0].aligned_target_sequence
-    ))
-    print(prnStr)
-    print()
+    for tid in maxAlignments.keys():
+        ali = maxAlignments[tid]
+        prnStr = ' '.join((
+            tid, ali[1], ali[0].cigar,
+            str(ali[0].optimal_alignment_score),
+            str(ali[0].suboptimal_alignment_score)
+        ))
+        print(prnStr)
+        prnStr = ' -> '.join((
+            ali[0].aligned_target_sequence,
+            ali[0].aligned_query_sequence
+        ))
+        print(prnStr)
+        print()
+
+if __name__ == "__main__":
+    main()
