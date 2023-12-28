c057167a0883418fa2ccd1fa26d17f58370dda5d
Yuuki Galaxy <galaxy001@gmail.com>
2023-12-28 18:44:14 +0800

    modified:   fig1.py

--- python/salus/cmplatform/fig1.py
+++ python/salus/cmplatform/fig1.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
 
-import importlib.metadata
-from packaging import version
-pkgname = "squidpy"
-min_ver = "1.2.3"
-got_ver = importlib.metadata.version(pkgname)
-if version.parse(got_ver) < version.parse(min_ver):
-    raise importlib.VersionConflict(f"{pkgname}>={min_ver} is needed, but found {pkgname}=={got_ver}")
-
-import matplotlib; matplotlib.use("module://mplcairo.base")
-from matplotlib import pyplot as plt
-import mplcairo
-
-import numpy as np
-import pandas as pd
-
-import anndata as ad
-import scanpy as sc
-import squidpy as sq
-import seaborn as sns
-
 import sys
 import os
 from typing import NamedTuple
@@ -43,7 +23,7 @@ SamplesDict = {
         'prefix'   : '/share/result/spatial/data/BoAo_sp',
         'suffixOut': dict.fromkeys(PlatformTuple,"outs"),
         'suffixMtx': 'filtered_feature_bc_matrix',
-        'platform': {PlatformTuple[0]:'illumina', PlatformTuple[1]: 'salus'},
+        'platforms': {PlatformTuple[0]:'illumina', PlatformTuple[1]: 'salus'},
         'pattern': ('prefix', 'platformV', 'sid', 'suffixOutV', 'suffixMtx')
     },
     'human': {
@@ -58,68 +38,93 @@ SamplesDict = {
         'pattern': ('prefix', 'platformV', 'suffixOutV', 'suffixMtx')
     }
 }
-##################
-if len(sys.argv) > 1:
-    thisID = sys.argv[1]
-    if thisID not in SamplesDict:
-        print(f"sid can only be {SamplesDict.keys()}", file=sys.stderr)
-        exit(1)
-else:
-    thisID = 'mbrain'
-##################
-print(sys.argv, file=sys.stderr)
-print(thisID, file=sys.stderr)
 
-class scDatItem(NamedTuple):
-    name: str
-    bgRaw: tuple[int,int]
-    bgFlt: tuple[int,int]
-    annDat: ad.AnnData
+def importModules() -> None:
+    import importlib.metadata
+    from packaging import version
+    pkgname = "squidpy"
+    min_ver = "1.2.3"
+    got_ver = importlib.metadata.version(pkgname)
+    if version.parse(got_ver) < version.parse(min_ver):
+        raise importlib.VersionConflict(f"{pkgname}>={min_ver} is needed, but found {pkgname}=={got_ver}")
 
-    def __repr__(self) -> str:
-        return f'<scDatItem:{self.name}, Raw_BC*Gene={self.bgRaw[0]}x{self.bgRaw[1]}, NonZero_BC*Gene={self.bgFlt[0]}x{self.bgFlt[1]}, ann={self.annDat.n_obs}x{self.annDat.n_vars}>'
+    import matplotlib; matplotlib.use("module://mplcairo.base")
+    from matplotlib import pyplot as plt
+    import mplcairo
 
-scDat = []
-nfoDict = SamplesDict[thisID]
-for platform in PlatformTuple:
-    nfoDict['platformV']  = nfoDict['platforms'][platform]
-    nfoDict['suffixOutV'] = nfoDict['suffixOut'][platform]
-    mtxPath = os.path.join( *[nfoDict[v] for v in nfoDict['pattern']] )
-    #print(mtxPath)
-    adata=sc.read_10x_mtx(mtxPath, var_names='gene_symbols', make_unique=True, gex_only=True)
-    adata.var_names_make_unique()  # this is necessary if using `var_names='gene_symbols'` in `sc.read_10x_mtx`
-    nnRaw = (adata.n_obs,adata.n_vars)
-    adata.var['mt'] = adata.var_names.str.startswith('MT-') | adata.var_names.str.startswith('mt-')
-    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=True, inplace=True)
-    sc.pp.filter_cells(adata, min_genes=1)
-    sc.pp.filter_genes(adata, min_cells=1)
-    nnFlt = (adata.n_obs,adata.n_vars)
-    scDat.append(scDatItem(platform,nnRaw,nnFlt,adata))
+    import numpy as np
+    import pandas as pd
 
-print("\n".join(str(scDat)))
+    import anndata as ad
+    import scanpy as sc
+    import squidpy as sq
+    import seaborn as sns
 
-with pd.option_context("mode.copy_on_write", True):
-    obsmbi = scDat[0].annDat.obs[['n_genes_by_counts', 'total_counts']].copy(deep=False)
-    obsmbs = scDat[1].annDat.obs[['n_genes_by_counts', 'total_counts']].copy(deep=False)
-    p1df = pd.concat([obsmbi.assign(Platform=scDat[0].name), obsmbs.assign(Platform=scDat[1].name)], ignore_index=True).replace([np.inf, -np.inf, 0], np.nan).dropna()
-    p2df = obsmbi.join(obsmbs,lsuffix='_'+scDat[0].name,rsuffix='_'+scDat[1].name,how='inner').replace([np.inf, -np.inf, 0], np.nan).dropna()
+    class scDatItem(NamedTuple):
+        name: str
+        bgRaw: tuple[int,int]
+        bgFlt: tuple[int,int]
+        annDat: ad.AnnData
 
-custom_params = {"axes.spines.right": False, "axes.spines.top": False}
-sns.set_theme(style="ticks", rc=custom_params, font="STIX Two Text")
-figA=sns.JointGrid(data=p1df, x="total_counts", y="n_genes_by_counts", hue='Platform', dropna=True)
-#figA.plot(sns.scatterplot, sns.histplot, alpha=.7, edgecolor=".2", linewidth=.5)
-figA.plot_joint(sns.scatterplot, s=12.7, alpha=.6)
-figA.plot_marginals(sns.histplot, kde=True, alpha=.618)
-figA.figure.suptitle(f"Gene to UMI plot - {nfoDict['sub']}")
-figA.set_axis_labels(xlabel='UMIs per Barcode', ylabel='Genes per Barcode')
-figA.savefig(f"1D_{nfoDict['sid']}.pdf", transparent=True, dpi=300, metadata={'Title': 'Gene to UMI plot', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+        def __repr__(self) -> str:
+            return f'<scDatItem:{self.name}, Raw_BC*Gene={self.bgRaw[0]}x{self.bgRaw[1]}, NonZero_BC*Gene={self.bgFlt[0]}x{self.bgFlt[1]}, ann={self.annDat.n_obs}x{self.annDat.n_vars}>'
 
-figB=sns.JointGrid(data=p2df, x="total_counts_Illumina", y="total_counts_Salus", dropna=True)
-figB.plot_joint(sns.scatterplot, s=12.7, alpha=.6)
-figB.plot_marginals(sns.histplot, kde=True, alpha=.618)
-figB.figure.suptitle(f"UMI per Barcode Counts Comparing - {nfoDict['sub']}")
-figB.set_axis_labels(xlabel='UMI Counts from Illumina', ylabel='UMI Counts from Salus')
-figB.savefig(f"1E_{nfoDict['sid']}.pdf", transparent=True, dpi=300, metadata={'Title': 'UMI per Barcode Counts Comparing', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+def main() -> None:
+    scDat = []
+    nfoDict = SamplesDict[thisID]
+    for platform in PlatformTuple:
+        nfoDict['platformV']  = nfoDict['platforms'][platform]
+        nfoDict['suffixOutV'] = nfoDict['suffixOut'][platform]
+        mtxPath = os.path.join( *[nfoDict[v] for v in nfoDict['pattern']] )
+        #print(mtxPath)
+        adata=sc.read_10x_mtx(mtxPath, var_names='gene_symbols', make_unique=True, gex_only=True)
+        adata.var_names_make_unique()  # this is necessary if using `var_names='gene_symbols'` in `sc.read_10x_mtx`
+        nnRaw = (adata.n_obs,adata.n_vars)
+        adata.var['mt'] = adata.var_names.str.startswith('MT-') | adata.var_names.str.startswith('mt-')
+        sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=True, inplace=True)
+        sc.pp.filter_cells(adata, min_genes=1)
+        sc.pp.filter_genes(adata, min_cells=1)
+        nnFlt = (adata.n_obs,adata.n_vars)
+        scDat.append(scDatItem(platform,nnRaw,nnFlt,adata))
+
+    print("\n".join(map(str,scDat)))
+
+    with pd.option_context("mode.copy_on_write", True):
+        obsmbi = scDat[0].annDat.obs[['n_genes_by_counts', 'total_counts']].copy(deep=False)
+        obsmbs = scDat[1].annDat.obs[['n_genes_by_counts', 'total_counts']].copy(deep=False)
+        p1df = pd.concat([obsmbi.assign(Platform=scDat[0].name), obsmbs.assign(Platform=scDat[1].name)], ignore_index=True).replace([np.inf, -np.inf, 0], np.nan).dropna()
+        p2df = obsmbi.join(obsmbs,lsuffix='_'+scDat[0].name,rsuffix='_'+scDat[1].name,how='inner').replace([np.inf, -np.inf, 0], np.nan).dropna()
+
+    custom_params = {"axes.spines.right": False, "axes.spines.top": False}
+    sns.set_theme(style="ticks", rc=custom_params, font="STIX Two Text")
+    figA=sns.JointGrid(data=p1df, x="total_counts", y="n_genes_by_counts", hue='Platform', dropna=True)
+    #figA.plot(sns.scatterplot, sns.histplot, alpha=.7, edgecolor=".2", linewidth=.5)
+    figA.plot_joint(sns.scatterplot, s=12.7, alpha=.6)
+    figA.plot_marginals(sns.histplot, kde=True, alpha=.618)
+    figA.figure.suptitle(f"Gene to UMI plot - {nfoDict['sub']}")
+    figA.set_axis_labels(xlabel='UMIs per Barcode', ylabel='Genes per Barcode')
+    figA.savefig(f"1D_{nfoDict['sid']}.pdf", transparent=True, dpi=300, metadata={'Title': 'Gene to UMI plot', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+
+    figB=sns.JointGrid(data=p2df, x="total_counts_Illumina", y="total_counts_Salus", dropna=True)
+    figB.plot_joint(sns.scatterplot, s=12.7, alpha=.6)
+    figB.plot_marginals(sns.histplot, kde=True, alpha=.618)
+    figB.figure.suptitle(f"UMI per Barcode Counts Comparing - {nfoDict['sub']}")
+    figB.set_axis_labels(xlabel='UMI Counts from Illumina', ylabel='UMI Counts from Salus')
+    figB.savefig(f"1E_{nfoDict['sid']}.pdf", transparent=True, dpi=300, metadata={'Title': 'UMI per Barcode Counts Comparing', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        thisID = sys.argv[1]
+        if thisID not in SamplesDict:
+            print(f"[x]sid can only be {SamplesDict.keys()}", file=sys.stderr)
+            exit(1)
+    else:
+        thisID = 'mbrain'
+    print(sys.argv, file=sys.stderr)
+    print(f"[i]{thisID}")
+    sys.stdout.flush()
+    importModules()
+    main()
 
 '''
 x1 = np.random.randn(1000)
