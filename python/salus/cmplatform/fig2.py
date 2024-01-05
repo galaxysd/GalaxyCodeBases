cbf8aa1541d1914a18091aa2048fd69f31f8105c
Yuuki Galaxy <galaxy001@gmail.com>
2024-01-05 19:28:33 +0800

    modified:   fig2.py

--- python/salus/cmplatform/fig2.py
+++ python/salus/cmplatform/fig2.py
@@ -44,7 +44,7 @@ def main(thisID) -> None:
     sc.pp.filter_cells(adata, min_counts=round(c995))   # sqrt_inv_total_counts < p995 按照样品均值的标准差考虑。写作round(c995)。
     sc.pp.filter_genes(adata, min_cells=1)  # adata.var[adata.var['n_cells']<2].sort_values(by='sqrt_inv_total_counts') 有800个，就不过滤了。
     print(f"[i]Filtered: {adata.raw.shape} -> {adata.shape}", file=sys.stderr)
-
+    plt.close('all')
     #adata.layers["prnorm"] = adata.X.copy()
     #sc.experimental.pp.normalize_pearson_residuals(adata,layer='prnorm')
     sc.pp.normalize_total(adata,target_sum=1e6, key_added='CPMnormFactor')
@@ -64,18 +64,19 @@ def main(thisID) -> None:
     ax1.set_xlabel("X-label for axis 1")
     '''
     print("[i]Begin fig E. 2Ca", file=sys.stderr)
-    plt.figure(3)
+    plt.figure()
     ax=sc.pl.pca(adata, color='Platform', show=False, title=f"PCA - {nfoDict['sub']}", annotate_var_explained=True)
     plt.savefig(f"2C_PCA_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 'PCA', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
-    plt.figure(4)
+    plt.figure()
     ax=sc.pl.umap(adata,color='Platform', show=False, title=f"UMAP - {nfoDict['sub']}")
     plt.savefig(f"2C_UMAP_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 'UMAP', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
-    plt.figure(5)
+    plt.figure()
     ax=sc.pl.tsne(adata, color='Platform', show=False, title=f"t-SNE - {nfoDict['sub']}")
     plt.savefig(f"2C_tSNE_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 't-SNE', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
-    plt.figure(6)
+    plt.figure()
     ax=sc.pl.draw_graph(adata, color='Platform', show=False, title=f"ForceAtlas2 - {nfoDict['sub']}")
     plt.savefig(f"2C_ForceAtlas2_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 'ForceAtlas2', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+    plt.close('all')
 
     fig, ax = plt.subplots()
     fig.patch.set(alpha=0)
@@ -89,6 +90,7 @@ def main(thisID) -> None:
     legend_elements = [plt.scatter([],[],linewidths=0, marker='o', label=label, color=color) for label, color in zip(newlabels, palette)]
     ax.legend(handles=legend_elements,bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.2)
     plt.savefig(f"2C_mPCA_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 'PCA', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+    plt.close('all')
 
     fig, ax = plt.subplots()
     fig.patch.set(alpha=0)
@@ -102,6 +104,8 @@ def main(thisID) -> None:
     legend_elements = [plt.scatter([],[],linewidths=0, marker='o', label=label, color=color) for label, color in zip(newlabels, palette)]
     ax.legend(handles=legend_elements,bbox_to_anchor=(1.05, 0.5), loc='center left', borderaxespad=0.2)
     plt.savefig(f"2C_mtSNE_{nfoDict['sid']}.pdf", bbox_extra_artists=(ax.get_legend(),), metadata={'Title': 't-SNE', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+    plt.close('all')
+
     print("[i]Begin fig E. 2Cb", file=sys.stderr)
     fig, axes = plt.subplots(1, 2, figsize=(12, 6))
     plt.subplots_adjust(wspace=0.1)
@@ -116,15 +120,18 @@ def main(thisID) -> None:
     axB.set_xlabel('leiden Cluster NO.')
     axB.set_ylabel('Cluster Size')
     plt.savefig(f"2C_leidenHist_{nfoDict['sid']}.pdf", metadata={'Title': 'Cluster Size Histogram', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+    plt.close('all')
+
     print("[i]Begin fig E. 2D", file=sys.stderr)
+    plt.switch_backend(matplotlib.backends.backend_pdf)
     import pymn
     adata.obs['cell.cluster'] = adata.obs['leiden'].astype(str)
     adata.obs['study_id'] = adata.obs['Platform'].astype(str)
     pymn.variableGenes(adata, study_col='Platform')
     pymn.MetaNeighborUS(adata, study_col='study_id', ct_col='cell.cluster', fast_version=True)
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(10,10))
     plt.title(f"MetaNeighborUS - {nfoDict['sub']}")
-    cm=pymn.plotMetaNeighborUS(adata, figsize=(10, 10), cmap='coolwarm',show=False)
+    cm=pymn.plotMetaNeighborUS(adata, cmap='coolwarm',show=False)
     cm.savefig(f"2D_MetaNeighborUS_{nfoDict['sid']}.pdf", metadata={'Title': 'MetaNeighborUS', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
     pymn.topHits(adata, threshold=0)
     mndf = adata.uns['MetaNeighborUS_topHits']
@@ -135,6 +142,7 @@ def main(thisID) -> None:
     axC = sns.barplot(pndf,x='ClusterID',y='Mean_AUROC')
     axC.set_xlabel('leiden Cluster NO.')
     plt.savefig(f"2D_AUROC_{nfoDict['sid']}.pdf", metadata={'Title': 'AUROC', 'Subject': f"{nfoDict['sub']} Data", 'Author': 'HU Xuesong'})
+    plt.close('all')
     #adata = None
 
 if __name__ == "__main__":
